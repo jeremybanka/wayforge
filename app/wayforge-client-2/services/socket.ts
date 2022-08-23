77ae2565bf74613ac69946f1793af26eaf514208
Jeremy Banka <hello@jeremybanka.com>
2022-08-23 01:04:27 -0700

    ➖ clean deps

--- app/wayforge-client-2/services/socket.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react"
-
-import { io } from "socket.io-client"
-
-export const socket = io(`http://localhost:3333/`)
-export const SocketContext = createContext(socket)
