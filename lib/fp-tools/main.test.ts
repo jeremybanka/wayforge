653fe9239e89d0129064fdd1e4e435505cd26861
Jeremy Banka <hello@jeremybanka.com>
2022-08-29 12:03:04 -0700

    🧹 tidy up dependencies and folder names

---
+++ lib/fp-tools/main.test.ts
@@ -0,0 +1,16 @@
+import { wrapInto, clampInto } from "."
+
+describe(`wrapInto`, () => {
+  it(`should wrap a number into a range`, () => {
+    expect(wrapInto([0, 10])(-21)).toBe(9)
+    expect(wrapInto([0, 10])(11)).toBe(1)
+    expect(wrapInto([0, 10])(5)).toBe(5)
+  })
+})
+describe(`clampInto`, () => {
+  it(`should clamp a number into a range`, () => {
+    expect(clampInto([0, 10])(-21)).toBe(0)
+    expect(clampInto([0, 10])(11)).toBe(10)
+    expect(clampInto([0, 10])(5)).toBe(5)
+  })
+})
