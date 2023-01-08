a03ca9f333c5c6f6fe8fe0749c8523fd7292b5bb
Jeremy Banka <hello@jeremybanka.com>
2023-01-08 13:11:55 -0800

    🚚 simplify testing/linting

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@
 import { resolve } from "path"
 
 import { defineConfig } from "vite"
+import { configDefaults } from "vitest/config"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: { globals: true },
+  test: {
+    globals: true,
+    exclude: [...configDefaults.exclude, `app`],
+  },
   resolve: {
     alias: {
-      "~": resolve(__dirname, `..`),
+      "~": resolve(__dirname, `.`),
     },
   },
 })
