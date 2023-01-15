9cf69c2a113c1010fd5f975ec9cb61204965539c
Jeremy Banka <hello@jeremybanka.com>
2023-01-14 17:37:17 -0800

    ➕ changesets, ci improvmements

---
+++ lib/vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+
+import { resolve } from "path"
+
+import { defineConfig } from "vite"
+import { configDefaults } from "vitest/config"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    exclude: [...configDefaults.exclude, `app`],
+  },
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, `..`),
+    },
+  },
+})
