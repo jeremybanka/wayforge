8946d66f36926dbbfc07a9c29f0c382dbebdc7e9
Jeremy Banka <hello@jeremybanka.com>
2023-03-25 15:28:37 -0700

    ✨ Dictionary class (cached two-way string Record)

--- packages/anvl/src/object/mapObject.ts
+++ packages/anvl/src/object/mapObject.ts
@@ -1,14 +1,18 @@
+import { pipe } from "fp-ts/lib/function"
+
+import { entriesToRecord, recordToEntries } from "./entries"
+import { map } from "../array"
+
 export const mapObject = <K extends keyof any, I, O>(
   obj: Record<K, I>,
   fn: (val: I, key: K) => O
-): Record<K, O> => {
-  const newObj = {} as Record<K, O>
-  const entries = Object.entries(obj) as [K, I][]
-  entries.forEach(([key, val]) => {
-    newObj[key] = fn(val, key)
-  })
-  return newObj
-}
+): Record<K, O> =>
+  pipe(
+    obj,
+    recordToEntries,
+    map(([key, val]) => [key, fn(val, key)] as const),
+    entriesToRecord
+  )
 
 export const mob =
   <K extends keyof any, I, O>(fn: (val: I, key: K) => O) =>
