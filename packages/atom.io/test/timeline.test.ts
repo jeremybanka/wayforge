b57bace662295c410f282e7e342c2d5febb9d26d
Jeremy Banka <hello@jeremybanka.com>
2023-04-23 23:54:03 -0700

    ✅ test timelines

--- packages/atom.io/test/timeline.test.ts
+++ packages/atom.io/test/timeline.test.ts
@@ -4,13 +4,15 @@ import * as UTIL from "./-util"
 import {
   __INTERNAL__,
   atom,
+  getState,
   runTransaction,
+  selector,
   setLogLevel,
   setState,
   transaction,
   useLogger,
 } from "../src"
-import { timeline, undo } from "../src/timeline"
+import { redo, timeline, undo } from "../src/timeline"
 
 const loggers = [UTIL.silence, console] as const
 const choose = 1
@@ -41,6 +43,14 @@ describe(`timeline`, () => {
       key: `c`,
       default: 0,
     })
+
+    const product_abc = selector({
+      key: `product of a, b, & c`,
+      get: ({ get }) => {
+        return get(a) * get(b) * get(c)
+      },
+    })
+
     const tl_abc = timeline({
       key: `a, b, & c`,
       atoms: [a, b, c],
@@ -62,23 +72,57 @@ describe(`timeline`, () => {
       },
     })
 
+    const expectation0 = () => {
+      expect(getState(a)).toBe(0)
+      expect(getState(b)).toBe(0)
+      expect(getState(c)).toBe(0)
+      expect(getState(product_abc)).toBe(0)
+    }
+    expectation0()
+
     setState(a, 1)
+    const expectation1 = () => {
+      expect(getState(a)).toBe(1)
+      expect(getState(b)).toBe(0)
+      expect(getState(c)).toBe(0)
+      expect(getState(product_abc)).toBe(0)
+    }
+    expectation1()
+
     runTransaction(tx_ab)()
+    const expectation2 = () => {
+      expect(getState(a)).toBe(2)
+      expect(getState(b)).toBe(1)
+      expect(getState(c)).toBe(0)
+      expect(getState(product_abc)).toBe(0)
+    }
+    expectation2()
+
     runTransaction(tx_bc)(2)
+    const expectation3 = () => {
+      expect(getState(a)).toBe(2)
+      expect(getState(b)).toBe(3)
+      expect(getState(c)).toBe(2)
+    }
+    expectation3()
+
     undo(tl_abc)
+    expectation2()
 
-    console.log(
-      `at`,
-      __INTERNAL__.IMPLICIT.STORE.timelineStore.get(tl_abc.key).at
-    )
-    console.log(
-      `history`,
-      __INTERNAL__.IMPLICIT.STORE.timelineStore.get(tl_abc.key).history
-    )
+    redo(tl_abc)
+    expectation3()
 
-    // t.next()
-    // t.prev()
-    // t.next()
-    // t.next()
+    undo(tl_abc)
+    undo(tl_abc)
+    expectation1()
+
+    undo(tl_abc)
+    expectation0()
+
+    const timelineData = __INTERNAL__.IMPLICIT.STORE.timelineStore.get(
+      tl_abc.key
+    )
+    expect(timelineData.at).toBe(0)
+    expect(timelineData.history.length).toBe(3)
   })
 })
