97ed31964fdcb95f62e41bdaddd7a7f95bd968e2
Jeremy Banka <hello@jeremybanka.com>
2023-06-18 16:03:22 -0700

    ✨ receive transaction returns cleanup fn

--- packages/atom.io/src/realtime/hook-composition/receive-transaction.ts
+++ packages/atom.io/src/realtime/hook-composition/receive-transaction.ts
@@ -7,11 +7,13 @@ import type { ServerConfig } from "."
 export const useReceiveTransaction = ({ socket, store }: ServerConfig) => {
   return function receiveTransaction<ƒ extends ƒn>(
     tx: AtomIO.TransactionToken<ƒ>
-  ): void {
-    socket.on(
-      `tx:${tx.key}`,
-      (update: AtomIO.__INTERNAL__.TransactionUpdate<ƒ>) =>
-        AtomIO.runTransaction<ƒ>(tx, store)(...update.params)
-    )
+  ): () => void {
+    const fillTransactionRequest = (
+      update: AtomIO.__INTERNAL__.TransactionUpdate<ƒ>
+    ) => AtomIO.runTransaction<ƒ>(tx, store)(...update.params)
+
+    socket.on(`tx:${tx.key}`, fillTransactionRequest)
+
+    return () => socket.off(`tx:${tx.key}`, fillTransactionRequest)
   }
 }
