962101f2218a1a59daddbedfa24767df5265d104
Jeremy Banka <hello@jeremybanka.com>
2023-07-12 22:53:04 -0700

    ➖ drop dependencies

--- apps/web/shrine/vite.config.ts
+++ apps/web/shrine/vite.config.ts
@@ -6,16 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths()],
-
-  esbuild: {
-    define: {
-      this: `window`,
-    },
-  },
-
   test: {
     globals: true,
-    environment: `happy-dom`,
-    css: false,
   },
 })
