0c5a5d7064420bbae3953c540c78795f559b6f96
Jeremy Banka <hello@jeremybanka.com>
2023-07-13 02:29:05 -0700

    🎨 formatting

--- packages/atom.io/src/realtime/hook-composition/expose-timeline.ts
+++ packages/atom.io/src/realtime/hook-composition/expose-timeline.ts
@@ -6,55 +6,55 @@ import type { ServerConfig } from "."
 import type { TimelineTransactionUpdate } from "../../internal"
 
 export type TransactionRequest = {
-  key: string
-  params: unknown[]
-  transactionId: string
+	key: string
+	params: unknown[]
+	transactionId: string
 }
 
 export const useExposeTimeline__UNSTABLE = ({
-  socket,
-  store,
+	socket,
+	store,
 }: ServerConfig): ((tl: AtomIO.TimelineToken) => () => void) => {
-  const timestampsOfTransactionsState = AtomIO.__INTERNAL__.atom__INTERNAL(
-    {
-      key: `timestampsOfTransactions`,
-      default: new Join<null, `transactionId`, `timestamp`>({
-        relationType: `1:1`,
-      })
-        .from(`transactionId`)
-        .to(`timestamp`),
-    },
-    undefined,
-    store
-  )
-  return function exposeTimeline(tl: AtomIO.TimelineToken): () => void {
-    const handleTransactionRequest = (update: TransactionRequest) => {
-      AtomIO.runTransaction(
-        { key: update.key, type: `transaction` },
-        store
-      )(...update.params)
-    }
+	const timestampsOfTransactionsState = AtomIO.__INTERNAL__.atom__INTERNAL(
+		{
+			key: `timestampsOfTransactions`,
+			default: new Join<null, `transactionId`, `timestamp`>({
+				relationType: `1:1`,
+			})
+				.from(`transactionId`)
+				.to(`timestamp`),
+		},
+		undefined,
+		store,
+	)
+	return function exposeTimeline(tl: AtomIO.TimelineToken): () => void {
+		const handleTransactionRequest = (update: TransactionRequest) => {
+			AtomIO.runTransaction(
+				{ key: update.key, type: `transaction` },
+				store,
+			)(...update.params)
+		}
 
-    socket.on(`txr:${tl.key}`, handleTransactionRequest)
+		socket.on(`txr:${tl.key}`, handleTransactionRequest)
 
-    const unsubscribeFromTimeline = AtomIO.subscribeToTimeline(
-      tl,
-      (update: TimelineTransactionUpdate) => {
-        // const timestamp = update.timestamp.toString()
-        // AtomIO.setState(
-        //   timestampsOfTransactionsState,
-        //   (j) => j.set(update),
-        //   store
-        // )
-        if (update.type === `transaction_update`) {
-          socket.emit(`tl:${tl.key}`, update)
-        }
-      }
-    )
+		const unsubscribeFromTimeline = AtomIO.subscribeToTimeline(
+			tl,
+			(update: TimelineTransactionUpdate) => {
+				// const timestamp = update.timestamp.toString()
+				// AtomIO.setState(
+				//   timestampsOfTransactionsState,
+				//   (j) => j.set(update),
+				//   store
+				// )
+				if (update.type === `transaction_update`) {
+					socket.emit(`tl:${tl.key}`, update)
+				}
+			},
+		)
 
-    return () => {
-      socket.off(`tlu:${tl.key}`, handleTransactionRequest)
-      unsubscribeFromTimeline()
-    }
-  }
+		return () => {
+			socket.off(`tlu:${tl.key}`, handleTransactionRequest)
+			unsubscribeFromTimeline()
+		}
+	}
 }
