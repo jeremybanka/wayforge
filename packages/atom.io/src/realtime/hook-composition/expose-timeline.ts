c5d19a2cac16d2cd3c7b9204488b86f461816b95
Jeremy Banka <hello@jeremybanka.com>
2023-07-13 01:48:09 -0700

    ✨ work in progress rollback util

---
+++ packages/atom.io/src/realtime/hook-composition/expose-timeline.ts
@@ -0,0 +1,60 @@
+import * as AtomIO from "atom.io"
+
+import { Join } from "~/packages/anvl/src/join"
+
+import type { ServerConfig } from "."
+import type { TimelineTransactionUpdate } from "../../internal"
+
+export type TransactionRequest = {
+  key: string
+  params: unknown[]
+  transactionId: string
+}
+
+export const useExposeTimeline = ({
+  socket,
+  store,
+}: ServerConfig): ((tl: AtomIO.TimelineToken) => () => void) => {
+  const timestampsOfTransactionsState = AtomIO.__INTERNAL__.atom__INTERNAL(
+    {
+      key: `timestampsOfTransactions`,
+      default: new Join<null, `transactionId`, `timestamp`>({
+        relationType: `1:1`,
+      })
+        .from(`transactionId`)
+        .to(`timestamp`),
+    },
+    undefined,
+    store
+  )
+  return function exposeTimeline(tl: AtomIO.TimelineToken): () => void {
+    const handleTransactionRequest = (update: TransactionRequest) => {
+      AtomIO.runTransaction(
+        { key: update.key, type: `transaction` },
+        store
+      )(...update.params)
+    }
+
+    socket.on(`txr:${tl.key}`, handleTransactionRequest)
+
+    const unsubscribeFromTimeline = AtomIO.subscribeToTimeline(
+      tl,
+      (update: TimelineTransactionUpdate) => {
+        // const timestamp = update.timestamp.toString()
+        // AtomIO.setState(
+        //   timestampsOfTransactionsState,
+        //   (j) => j.set(update),
+        //   store
+        // )
+        if (update.type === `transaction_update`) {
+          socket.emit(`tl:${tl.key}`, update)
+        }
+      }
+    )
+
+    return () => {
+      socket.off(`tlu:${tl.key}`, handleTransactionRequest)
+      unsubscribeFromTimeline()
+    }
+  }
+}
