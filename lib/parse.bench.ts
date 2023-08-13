76fb4b49ce7f1d87b2f13a13d2fc144fef752a13
Jeremy Banka <hello@jeremybanka.com>
2023-08-13 13:01:25 -0700

    🐛 fix bad import

--- lib/parse.bench.ts
+++ lib/parse.bench.ts
@@ -1,8 +1,8 @@
 import * as v from "vitest"
 
-v.describe(`comparing Array.split to JSON.parse`, () => {
+v.describe(`comparing String.split to JSON.parse`, () => {
 	const str = `a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y`
-	v.bench(`Array.split`, () => {
+	v.bench(`String.split`, () => {
 		str.split(`,`)
 	})
 	v.bench(`JSON.parse`, () => {
@@ -13,7 +13,7 @@ v.describe(`comparing Array.split to JSON.parse`, () => {
 const elements = `1,`.repeat(10000).slice(0, -1)
 
 v.describe(`10000 elements`, () => {
-	v.bench(`Array.split`, () => {
+	v.bench(`String.split`, () => {
 		elements.split(`,`)
 	})
 	v.bench(`JSON.parse`, () => {
--- packages/anvl/reactivity/transceiver.test.ts
+++ packages/anvl/reactivity/transceiver.test.ts
@@ -1,16 +1,16 @@
-import { SetTV } from "./transceiver"
+import { TransceiverSet } from "./transceiver"
 
 describe(`SetEX`, () => {
 	describe(`observe`, () => {
 		it(`should call the function when the set is updated`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			const fn = vitest.fn()
 			set.observe(fn)
 			set.add(1)
 			expect(fn).toHaveBeenCalledWith(`add:${JSON.stringify(1)}`)
 		})
 		it(`should return a function that unsubscribes`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			const fn = vitest.fn()
 			const unsubscribe = set.observe(fn)
 			unsubscribe()
@@ -20,18 +20,18 @@ describe(`SetEX`, () => {
 	})
 	describe(`do`, () => {
 		it(`should add a value to the set`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			set.do(`add:1`)
 			expect(set.has(1)).toBe(true)
 		})
 		it(`should clear the set`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			set.add(1)
 			set.do(`clear:[]`)
 			expect(set.size).toBe(0)
 		})
 		it(`should delete a value from the set`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			set.add(1)
 			set.do(`del:1`)
 			expect(set.has(1)).toBe(false)
@@ -39,18 +39,18 @@ describe(`SetEX`, () => {
 	})
 	describe(`undo`, () => {
 		it(`should delete a value from the set`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			set.add(1)
 			set.undo(`add:1`)
 			expect(set.has(1)).toBe(false)
 		})
 		it(`should add a value to the set`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			set.undo(`clear:[]`)
 			expect(set.size).toBe(0)
 		})
 		it(`should clear the set`, () => {
-			const set = new SetTV()
+			const set = new TransceiverSet()
 			set.add(1)
 			set.undo(`del:1`)
 			expect(set.has(1)).toBe(true)
