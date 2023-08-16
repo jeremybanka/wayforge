2517b2e7dbd3d6db64b192faed1b19ddfd0ab5d1
Jeremy Banka <hello@jeremybanka.com>
2023-08-15 19:21:10 -0700

    🛠️ reconfigure exports, export types at the root

--- packages/rel8/types/tsup.config.ts
+++ packages/rel8/types/tsup.config.ts
@@ -1,7 +1,10 @@
+import * as path from "path"
+
 import { defineConfig } from "tsup"
 
 export default defineConfig({
 	entry: [`src/index.ts`],
+	outDir: path.join(__dirname, `../dist`),
 	dts: true,
 	format: [`esm`, `cjs`],
 	splitting: false,
