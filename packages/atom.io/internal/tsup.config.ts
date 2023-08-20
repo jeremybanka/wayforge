97e07df4525d1fbab68a84871bafa2946cd768fd
Jeremy Banka <hello@jeremybanka.com>
2023-08-20 10:50:17 -0700

    🛠️  treeshake and minify

--- packages/atom.io/internal/tsup.config.ts
+++ packages/atom.io/internal/tsup.config.ts
@@ -5,8 +5,10 @@ export default defineConfig({
 	outDir: `./dist`,
 	dts: true,
 	format: [`esm`, `cjs`],
-	splitting: false,
+	splitting: true,
+	treeshake: true,
 	sourcemap: true,
+	minify: true,
 	clean: true,
 	external: [`atom.io`],
 })
