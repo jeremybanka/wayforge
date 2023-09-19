b0b5f7036ffade7de19679ade22c12ad16ff755f
Jeremy Banka <hello@jeremybanka.com>
2023-09-19 02:02:24 -0700

    ✨ isTransceiver

--- packages/anvl/reactivity/transceiver.ts
+++ packages/anvl/reactivity/transceiver.ts
@@ -6,6 +6,18 @@ export type Transceiver<Signal extends Json.Serializable> = {
 	subscribe: (key: string, fn: (update: Signal) => void) => () => void
 }
 
+export function isTransceiver(
+	value: unknown,
+): value is Transceiver<Json.Serializable> {
+	return (
+		typeof value === `object` &&
+		value !== null &&
+		`do` in value &&
+		`undo` in value &&
+		`subscribe` in value
+	)
+}
+
 export type TransceiverMode = `playback` | `record` | `transaction`
 
 export type Signal<Core extends Transceiver<any>> = Core extends Transceiver<
