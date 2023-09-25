93bbdeab20ba18a71f953f4a582354bd54163e01
Jeremy Banka <hello@jeremybanka.com>
2023-09-24 17:58:17 -0700

    🔥 remove resolution test

--- packages/atom.io/__test__/__util__/resolution.test.ts
+++
@@ -1,9 +0,0 @@
-import tsconfigPaths from "vite-tsconfig-paths"
-describe(`config resolution`, () => {
-	test(`paths`, async () => {
-		const project = `./tsconfig.json`
-		const tsconfigPathsPlugin = tsconfigPaths({
-			projects: [project],
-		})
-	})
-})
