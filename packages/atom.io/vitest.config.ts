4478b032be6f8299a60c0711b12ea0d2c721c11d
Jeremy Banka <hello@jeremybanka.com>
2023-09-24 17:58:31 -0700

    🔧 simplify vitest config

--- packages/atom.io/vitest.config.ts
+++ packages/atom.io/vitest.config.ts
@@ -1,11 +1,8 @@
-import fs from "fs"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
+
 const shouldTestDistFolder = process.env.LIB === `dist`
 const project = shouldTestDistFolder ? `./tsconfig.prod.json` : `./tsconfig.json`
-// if (shouldTestDistFolder) throw new Error(`TODO: test dist folder`)
-
-console.log(fs.readdirSync(`./`))
 
 export default defineConfig({
 	plugins: [
