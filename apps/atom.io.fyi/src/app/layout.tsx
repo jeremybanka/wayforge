c04bd3431fc702fe7d547345e334891b91df966a
Jeremy Banka <hello@jeremybanka.com>
2023-10-02 19:44:11 -0600

    💄 define global breakpoints

--- apps/atom.io.fyi/src/app/layout.tsx
+++ apps/atom.io.fyi/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import Link from "next/link"
+import "./_breakpoints.scss"
 import "./code.scss"
 import "./globals.scss"
 import scss from "./layout.module.scss"
