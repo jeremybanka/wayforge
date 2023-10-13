728638c6bdc646d835d97ccc28da3a5eb8081305
Jeremy Banka <hello@jeremybanka.com>
2023-10-13 12:26:09 -0700

    🛠️  add scripts for integrity guarantee process

---
+++ packages/atom.io/__scripts__/integrity-test.node.ts
@@ -0,0 +1,5 @@
+import defineIntegrationScope from "./define-integration-scope.node"
+import definePackageExports from "./define-package-exports.node"
+
+definePackageExports(`test`)
+defineIntegrationScope(`test`)
