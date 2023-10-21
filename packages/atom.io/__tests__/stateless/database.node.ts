7b67a34561169bc571e086f04d2e6c1a3ef2e344
Jeremy Banka <hello@jeremybanka.com>
2023-10-21 02:26:22 -0700

    ♻️ use Postgres.js instead of node-postgres

--- packages/atom.io/__tests__/stateless/database.node.ts
+++ packages/atom.io/__tests__/stateless/database.node.ts
@@ -1,10 +1,10 @@
 import path from "path"
 import fs from "fs/promises"
-import { Client } from "pg"
+import postgres from "postgres"
 
 export class DatabaseManager {
 	public dbName: string
-	private client: Client
+	private sql: ReturnType<typeof postgres>
 	private config: {
 		user: string
 		host: string
@@ -22,64 +22,64 @@ export class DatabaseManager {
 			password: `your_password`, // Set your postgres user's password
 			port: 5432,
 		}
-		this.client = new Client(this.config)
+		this.sql = postgres(this.config)
 	}
 
 	public async connect(): Promise<void> {
-		await this.client.connect()
+		// With postgres.js, the connection is lazy and will only be established once the first query is executed.
+		// Therefore, no explicit connection step is required.
 	}
 
 	public async createDatabase(): Promise<void> {
-		await this.client.query(`CREATE DATABASE ${this.dbName}`)
-		this.client.end()
+		await this.sql`CREATE DATABASE ${this.sql(this.dbName)}`
+		this.sql.end()
 
 		// Reconnect with the new database
 		this.config.database = this.dbName
-		this.client = new Client({ ...this.config, database: this.dbName })
-		await this.client.connect()
+		this.sql = postgres(this.config)
 	}
 
 	public async setupTriggersAndNotifications(): Promise<void> {
 		const triggersPath = path.join(__dirname, `triggers.sql`)
 		const triggers = await fs.readFile(triggersPath, `utf8`)
-		await this.client.query(triggers)
+		await this.sql.file(triggersPath) // Use file method to execute SQL file
 	}
 
 	public async dropDatabase(): Promise<void> {
-		await this.client.end()
+		this.sql.end()
 
-		const adminClient = new Client({
+		const adminSql = postgres({
 			...this.config,
 			database: `postgres`,
 		})
-		await adminClient.connect()
-		await adminClient.query(`DROP DATABASE ${this.dbName}`)
-		await adminClient.end()
+		await adminSql`DROP DATABASE ${this.sql(this.dbName)}`
+		adminSql.end()
 	}
 
 	public async disconnect(): Promise<void> {
-		await this.client.end()
+		this.sql.end()
 	}
 
 	public async createSampleTable(): Promise<void> {
-		await this.client.query(`
-        CREATE TABLE your_table (
-            id SERIAL PRIMARY KEY,
-            data TEXT
-        );
-    `)
+		await this.sql`
+      CREATE TABLE your_table (
+          id SERIAL PRIMARY KEY,
+          data TEXT
+      );
+    `
 	}
 
 	public async insertSampleData(): Promise<void> {
-		const res = await this.client.query(`
-				INSERT INTO your_table (data)
-				VALUES ('Hello, world!');
-		`)
+		const res = await this.sql`
+      INSERT INTO your_table (data)
+      VALUES ('Hello, world!')
+      RETURNING *;
+    `
 		console.log({ insert: res })
 	}
 
 	public async dropSampleTable(): Promise<void> {
-		const res = await this.client.query(`DROP TABLE your_table`)
+		const res = await this.sql`DROP TABLE your_table`
 		console.log({ drop: res })
 	}
 }
--- packages/atom.io/__tests__/stateless/server.node.ts
+++ packages/atom.io/__tests__/stateless/server.node.ts
@@ -1,32 +1,30 @@
 import * as http from "http"
-
-import { Client } from "pg"
+import postgres from "postgres"
 
 console.log(`Server starting...`)
 
-const DB_NAME = process.env.DB_NAME
-const DB_HOST = process.env.GITHUB_ACTION ? `postgres` : `localhost`
 const PORT = process.env.PORT || 8080
 const ORIGIN = `http://localhost:${PORT}`
+const DB_HOST = process.env.GITHUB_ACTION ? `postgres` : `localhost`
+const DB_NAME = process.env.DB_NAME
+
+if (DB_NAME === undefined) {
+	throw new Error(`DB_NAME environment variable is not set`)
+}
 
 const main = async () => {
-	// Database connection and cache invalidation setup
-	const client = new Client({
-		user: `postgres`, // Default user; adjust if needed
+	const sql = postgres({
+		user: `postgres`,
 		host: DB_HOST,
-		database: DB_NAME, // Default database to execute administrative commands
-		password: `your_password`, // Set your postgres user's password
+		database: DB_NAME,
+		password: `your_password`,
 		port: 5432,
 	})
-	await client.connect()
-	await client.query(`LISTEN table_update`)
 
-	client.on(`notification`, (message) => {
-		if (message.channel === `table_update` && message.payload) {
-			console.log(`Received notification: ${message.payload}`)
-			const [tableName, id] = message.payload.split(`,`)
-			// Invalidate cache for the given table and ID
-		}
+	sql`LISTEN table_update`
+
+	sql.listen(`notification`, (message) => {
+		console.log(`Received notification: ${message}`)
 	})
 
 	http
@@ -39,7 +37,7 @@ const main = async () => {
 				.on(`end`, async () => {
 					const authHeader = req.headers.authorization
 					try {
-						// if (authHeader !== `Bearer MY_BEARER_TOKEN`) throw 401
+						// if (authHeader !== `Bearer MY_BEARER_TOKEN`) throw 401;
 						if (typeof req.url !== `string`) throw 418
 						const url = new URL(req.url, ORIGIN)
 						console.log({ pathname: url.pathname, method: req.method })
@@ -51,12 +49,7 @@ const main = async () => {
 										res.end(`Hello from server on port ${PORT}!`)
 										break
 									case `/get`: {
-										console.log(`GET /get`)
-										// let's get all rows
-										const { rows } = await client.query(
-											`SELECT * FROM your_table`,
-										)
-										console.log({ rows })
+										const rows = await sql`SELECT * FROM your_table`
 										res.writeHead(200, {
 											"Content-Type": `application/json`,
 										})
@@ -85,4 +78,5 @@ const main = async () => {
 			console.log(`Server started on port ${PORT}`)
 		})
 }
+
 main()
