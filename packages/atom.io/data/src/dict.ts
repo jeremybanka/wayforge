41ed8fd64fbcdcd3dcef70200be67b98a16357eb
Jeremy Banka <hello@jeremybanka.com>
2023-10-24 01:17:18 -0700

    🎨 apply eslint

--- packages/atom.io/data/src/dict.ts
+++ packages/atom.io/data/src/dict.ts
@@ -1,12 +1,13 @@
-import * as AtomIO from "atom.io"
-import { IMPLICIT, Store, createSelector } from "atom.io/internal"
-import { Json, Stringified } from "atom.io/json"
+import type * as AtomIO from "atom.io"
+import type { Store } from "atom.io/internal"
+import { IMPLICIT, createSelector } from "atom.io/internal"
+import type { Json, Stringified } from "atom.io/json"
 
 export function dict<State, Key extends Json.Serializable>(
 	findState:
 		| AtomIO.AtomFamily<State, Key>
-		| AtomIO.SelectorFamily<State, Key>
-		| AtomIO.ReadonlySelectorFamily<State, Key>,
+		| AtomIO.ReadonlySelectorFamily<State, Key>
+		| AtomIO.SelectorFamily<State, Key>,
 	index:
 		| AtomIO.AtomToken<Key[]>
 		| AtomIO.ReadonlySelectorToken<Key[]>
