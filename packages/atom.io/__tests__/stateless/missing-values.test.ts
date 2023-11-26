87800d9b4e55cbd36ee0b3f1ab936bfd1825d771
Jeremy Banka <hello@jeremybanka.com>
2023-11-25 21:02:53 -0800

    🎨 better organize tests

--- packages/atom.io/__tests__/stateless/missing-values.test.ts
+++ packages/atom.io/__tests__/stateless/missing-values.test.ts
@@ -33,155 +33,160 @@ beforeEach(() => {
 })
 
 describe(`stateless data persistence strategies`, () => {
-	test(`automatic strategy`, async () => {
-		const count = atom<Loadable<number>>({
-			key: `count`,
-			default: () => {
-				return new Promise<number>((resolve) => {
-					readFile(`${tmpDir.name}/count.txt`, `utf8`, (error, data) => {
-						if (error) {
-							writeFile(`${tmpDir.name}/count.txt`, `0`, () => {
-								resolve(0)
-							})
-							return
-						}
-						resolve(parseInt(data, 10))
+	describe(`effect strategy`, () => {
+		test(`manual implementation`, async () => {
+			const count = atom<Loadable<number>>({
+				key: `count`,
+				default: () => {
+					return new Promise<number>((resolve) => {
+						readFile(`${tmpDir.name}/count.txt`, `utf8`, (error, data) => {
+							if (error) {
+								writeFile(`${tmpDir.name}/count.txt`, `0`, () => {
+									resolve(0)
+								})
+								return
+							}
+							resolve(parseInt(data, 10))
+						})
 					})
-				})
-			},
-			effects: [
-				({ onSet, setSelf }) => {
-					onSet(({ oldValue, newValue }) => {
-						if (newValue instanceof Promise) {
-							return
-						}
-						if (oldValue instanceof Promise) {
+				},
+				effects: [
+					({ onSet, setSelf }) => {
+						onSet(({ oldValue, newValue }) => {
+							if (newValue instanceof Promise) {
+								return
+							}
+							if (oldValue instanceof Promise) {
+								const unsub =
+									Internal.IMPLICIT.STORE.subject.operationStatus.subscribe(
+										`One-Shot: enqueue update to count.txt`,
+										() => {
+											unsub()
+											const promise = new Promise<number>((resolve) => {
+												oldValue.then((resolvedOldValue) => {
+													if (resolvedOldValue === newValue) {
+														return
+													}
+													writeFile(
+														`${tmpDir.name}/count.txt`,
+														newValue.toString(),
+														() => {
+															resolve(newValue)
+														},
+													)
+												})
+											})
+											setSelf(promise)
+										},
+									)
+								return
+							}
 							const unsub =
 								Internal.IMPLICIT.STORE.subject.operationStatus.subscribe(
 									`One-Shot: enqueue update to count.txt`,
 									() => {
-										unsub()
 										const promise = new Promise<number>((resolve) => {
-											oldValue.then((resolvedOldValue) => {
-												if (resolvedOldValue === newValue) {
-													return
-												}
-												writeFile(
-													`${tmpDir.name}/count.txt`,
-													newValue.toString(),
-													() => {
-														resolve(newValue)
-													},
-												)
-											})
+											unsub()
+											writeFile(
+												`${tmpDir.name}/count.txt`,
+												newValue.toString(),
+												() => {
+													resolve(newValue)
+												},
+											)
 										})
 										setSelf(promise)
 									},
 								)
-							return
-						}
-						const unsub =
-							Internal.IMPLICIT.STORE.subject.operationStatus.subscribe(
-								`One-Shot: enqueue update to count.txt`,
-								() => {
-									const promise = new Promise<number>((resolve) => {
-										unsub()
-										writeFile(
-											`${tmpDir.name}/count.txt`,
-											newValue.toString(),
-											() => {
-												resolve(newValue)
-											},
-										)
-									})
-									setSelf(promise)
-								},
-							)
+						})
+					},
+				],
+			})
+
+			expect(await getState(count)).toBe(0)
+			clearValueMap()
+			setState(count, 1)
+			clearValueMap()
+			await new Promise((resolve) => setTimeout(resolve, 5))
+			expect(await getState(count)).toBe(1)
+			clearValueMap()
+			setState(count, 2)
+			clearValueMap()
+			await new Promise((resolve) => setTimeout(resolve, 5))
+			expect(await getState(count)).toBe(2)
+		})
+	})
+	describe(`writer composition strategy`, () => {
+		test(`manual implementation`, async () => {
+			const count = atom<Loadable<number>>({
+				key: `count`,
+				default: () => {
+					return new Promise<number>((resolve) => {
+						readFile(`${tmpDir.name}/count.txt`, `utf8`, (error, data) => {
+							if (error) {
+								writeFile(`${tmpDir.name}/count.txt`, `0`, () => {
+									resolve(0)
+								})
+								return
+							}
+							resolve(parseInt(data, 10))
+						})
 					})
 				},
-			],
-		})
+			})
 
-		expect(await getState(count)).toBe(0)
-		clearValueMap()
-		setState(count, 1)
-		clearValueMap()
-		await new Promise((resolve) => setTimeout(resolve, 5))
-		expect(await getState(count)).toBe(1)
-		clearValueMap()
-		setState(count, 2)
-		clearValueMap()
-		await new Promise((resolve) => setTimeout(resolve, 5))
-		expect(await getState(count)).toBe(2)
-	})
-	test(`manual strategy`, async () => {
-		const count = atom<Loadable<number>>({
-			key: `count`,
-			default: () => {
-				return new Promise<number>((resolve) => {
-					readFile(`${tmpDir.name}/count.txt`, `utf8`, (error, data) => {
-						if (error) {
-							writeFile(`${tmpDir.name}/count.txt`, `0`, () => {
-								resolve(0)
-							})
-							return
-						}
-						resolve(parseInt(data, 10))
-					})
-				})
-			},
-		})
+			async function writeCount(newValue: number): Promise<number> {
+				await fsp.writeFile(`${tmpDir.name}/count.txt`, newValue.toString())
+				return newValue
+			}
 
-		async function writeCount(newValue: number): Promise<number> {
-			await fsp.writeFile(`${tmpDir.name}/count.txt`, newValue.toString())
-			return newValue
-		}
-
-		function updateCount(newValue: number): void {
-			if (Internal.IMPLICIT.STORE.operation.open) {
-				const unsub = Internal.IMPLICIT.STORE.subject.operationStatus.subscribe(
-					`One-Shot: enqueue update to count.txt`,
-					() => {
-						unsub()
-						const currentValue = getState(count)
-						if (currentValue instanceof Promise) {
-							currentValue.then((resolvedCurrentValue) => {
-								if (resolvedCurrentValue === newValue) {
+			function updateCount(newValue: number): void {
+				if (Internal.IMPLICIT.STORE.operation.open) {
+					const unsub =
+						Internal.IMPLICIT.STORE.subject.operationStatus.subscribe(
+							`One-Shot: enqueue update to count.txt`,
+							() => {
+								unsub()
+								const currentValue = getState(count)
+								if (currentValue instanceof Promise) {
+									currentValue.then((resolvedCurrentValue) => {
+										if (resolvedCurrentValue === newValue) {
+											return
+										}
+										setState(count, writeCount(newValue))
+									})
+									return
+								}
+								if (currentValue === newValue) {
 									return
 								}
 								setState(count, writeCount(newValue))
-							})
-							return
-						}
-						if (currentValue === newValue) {
+							},
+						)
+					return
+				}
+				const currentValue = getState(count)
+				if (currentValue instanceof Promise) {
+					currentValue.then((resolvedCurrentValue) => {
+						if (resolvedCurrentValue === newValue) {
 							return
 						}
 						setState(count, writeCount(newValue))
-					},
-				)
-				return
+					})
+					return
+				}
+				if (currentValue === newValue) {
+					return
+				}
+				setState(count, writeCount(newValue))
 			}
-			const currentValue = getState(count)
-			if (currentValue instanceof Promise) {
-				currentValue.then((resolvedCurrentValue) => {
-					if (resolvedCurrentValue === newValue) {
-						return
-					}
-					setState(count, writeCount(newValue))
-				})
-				return
-			}
-			if (currentValue === newValue) {
-				return
-			}
-			setState(count, writeCount(newValue))
-		}
 
-		expect(await getState(count)).toBe(0)
-		clearValueMap()
-		updateCount(1)
-		clearValueMap()
-		await new Promise((resolve) => setTimeout(resolve, 5))
-		expect(await getState(count)).toBe(1)
+			expect(await getState(count)).toBe(0)
+			clearValueMap()
+			updateCount(1)
+			clearValueMap()
+			await new Promise((resolve) => setTimeout(resolve, 5))
+			expect(await getState(count)).toBe(1)
+		})
 	})
 })
