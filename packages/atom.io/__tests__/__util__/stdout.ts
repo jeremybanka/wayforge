fdcac72bf84a2cf25352ec5463718ba55e2f7797
Jeremy Banka <hello@jeremybanka.com>
2023-12-01 00:26:49 -0800

    🛠️ add more stdout channels for convenience

--- packages/atom.io/__tests__/__util__/stdout.ts
+++ packages/atom.io/__tests__/__util__/stdout.ts
@@ -1,1 +1,5 @@
 export const stdout = (..._: any[]): void => undefined
+export const stdout0 = (..._: any[]): void => undefined
+export const stdout1 = (..._: any[]): void => undefined
+export const stdout2 = (..._: any[]): void => undefined
+export const stdout3 = (..._: any[]): void => undefined
