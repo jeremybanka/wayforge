c9472efcee80a04d64a26326bbe71ce6d7b584c9
Jeremy Banka <hello@jeremybanka.com>
2023-12-07 20:23:14 -0800

    💄 nicer glass elements

--- apps/atom.io.fyi/src/app/docs/nav.tsx
+++ apps/atom.io.fyi/src/app/docs/nav.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { usePathname, useRouter } from "next/navigation"
+import { usePathname } from "next/navigation"
 import * as React from "react"
 
 const SUBMODULES = [``, `react`]
