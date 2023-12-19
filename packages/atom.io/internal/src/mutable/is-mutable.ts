533cb74f99b077a78db23112a4b034d8426bec96
Jeremy Banka <hello@jeremybanka.com>
2023-12-18 18:45:12 -0800

    ♻️ simplify isMutable

--- packages/atom.io/internal/src/mutable/is-mutable.ts
+++ packages/atom.io/internal/src/mutable/is-mutable.ts
@@ -1,44 +1,17 @@
-import type {
-	AtomFamily,
-	AtomToken,
-	MutableAtomFamily,
-	MutableAtomToken,
-} from "~/packages/atom.io/src"
+import type { AtomFamily, MutableAtomFamily } from "atom.io"
+
 import type { MutableAtom } from "."
 import type { Atom } from "../atom"
-import type { Store } from "../store"
-import { withdraw } from "../store"
 
-export function isMutable(
-	token: AtomToken<any>,
-	store: Store,
-): token is MutableAtomToken<any, any>
 export function isMutable(atom: Atom<any>): atom is MutableAtom<any>
 export function isMutable(
 	family: AtomFamily<any, any>,
 ): family is MutableAtomFamily<any, any, any>
 export function isMutable(
-	atomOrTokenOrFamily: Atom<any> | AtomFamily<any, any> | AtomToken<any>,
-	store?: Store,
+	atomOrTokenOrFamily: Atom<any> | AtomFamily<any, any>,
 ): boolean {
 	if (`mutable` in atomOrTokenOrFamily) {
 		return atomOrTokenOrFamily.mutable
-	}
-	if (atomOrTokenOrFamily.type === `atom_family`) {
-		return false
-	}
-	if (`default` in atomOrTokenOrFamily) {
-		return false
-	}
-	if (!store) {
-		throw new Error(`Cannot check mutability without a store`)
-	}
-	const atom = withdraw(atomOrTokenOrFamily, store)
-	if (!atom) {
-		throw new Error(`Cannot check mutability without an atom`)
-	}
-	if (`mutable` in atom) {
-		return atom.mutable
 	}
 	return false
 }
