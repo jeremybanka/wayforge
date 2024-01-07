d5f3d555ddd6c461ab17c92756a07d0e7d378034
Jeremy Banka <hello@jeremybanka.com>
2024-01-07 13:47:43 -0800

    🚚 move graphics, delete saloon

--- apps/web/saloon/src/services/auth.ts
+++
@@ -1,8 +0,0 @@
-import * as AtomIO from "atom.io"
-import { lazyLocalStorageEffect } from "~/packages/atom.io/__unstable__/web-effects/src"
-
-export const secretState = AtomIO.atom<string>({
-	key: `secret`,
-	default: ``,
-	effects: [lazyLocalStorageEffect(`secret`)],
-})
