dbd60c06850443272ad33234437d95582de6f420
Jeremy Banka <hello@jeremybanka.com>
2024-01-15 01:39:21 -0800

    🧪 add sync state tools for client

---
+++ packages/atom.io/realtime-client/src/sync-state.ts
@@ -0,0 +1,19 @@
+import * as AtomIO from "atom.io"
+import type { Store } from "atom.io/internal"
+import type { Json } from "atom.io/json"
+import type { Socket } from "socket.io-client"
+
+export function syncState<J extends Json.Serializable>(
+	token: AtomIO.WritableToken<J>,
+	socket: Socket,
+	store: Store,
+): () => void {
+	const setServedValue = (data: J) => {
+		AtomIO.setState(token, data, store)
+	}
+	socket.on(`value:${token.key}`, setServedValue)
+	socket.emit(`get:${token.key}`)
+	return () => {
+		socket.off(`value:${token.key}`, setServedValue)
+	}
+}
