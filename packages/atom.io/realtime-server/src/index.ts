45bf394ae53f1a59c87cdae24351d54269815229
Jeremy Banka <hello@jeremybanka.com>
2024-01-15 01:35:51 -0800

    🧪 realtime state synchronizer

--- packages/atom.io/realtime-server/src/index.ts
+++ packages/atom.io/realtime-server/src/index.ts
@@ -2,6 +2,7 @@ import type { Store } from "atom.io/internal"
 import type * as SocketIO from "socket.io"
 
 export * from "./realtime-state-provider"
+export * from "./realtime-state-synchronizer"
 export * from "./realtime-family-provider"
 export * from "./realtime-mutable-provider"
 export * from "./realtime-mutable-family-provider"
