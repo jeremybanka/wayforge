7122c3a4a8f47851c6e630dd1ba8d11b774c12b3
Jeremy Banka <hello@jeremybanka.com>
2024-01-15 23:01:54 -0800

    🚚 move findInStore into internal/

--- packages/atom.io/src/find-state.ts
+++ packages/atom.io/src/find-state.ts
@@ -12,23 +12,9 @@ import type {
 	WritableSelectorToken,
 	WritableToken,
 } from "atom.io"
-import type { Store, Transceiver } from "atom.io/internal"
-import { IMPLICIT } from "atom.io/internal"
+import type { Transceiver } from "atom.io/internal"
+import { IMPLICIT, findInStore } from "atom.io/internal"
 import type { Json } from "atom.io/json"
-
-export function findInStore(
-	token: ReadableFamilyToken<any, any>,
-	key: Json.Serializable,
-	store: Store,
-): ReadableToken<any> {
-	const familyKey = token.key
-	const family = store.families.get(familyKey)
-	if (family === undefined) {
-		throw new Error(`Family ${familyKey} not found`)
-	}
-	const state = family(key)
-	return state
-}
 
 export function findState<
 	T extends Transceiver<any>,
@@ -36,26 +22,32 @@ export function findState<
 	K extends Json.Serializable,
 	Key extends K,
 >(token: MutableAtomFamilyToken<T, J, K>, key: Key): MutableAtomToken<T, J>
+
 export function findState<T, K extends Json.Serializable, Key extends K>(
 	token: RegularAtomFamilyToken<T, K>,
 	key: Key,
 ): RegularAtomToken<T>
+
 export function findState<T, K extends Json.Serializable, Key extends K>(
 	token: WritableSelectorFamilyToken<T, K>,
 	key: Key,
 ): WritableSelectorToken<T>
+
 export function findState<T, K extends Json.Serializable, Key extends K>(
 	token: ReadonlySelectorFamilyToken<T, K>,
 	key: Key,
 ): ReadonlySelectorToken<T>
+
 export function findState<T, K extends Json.Serializable, Key extends K>(
 	token: WritableFamilyToken<T, K>,
 	key: Key,
 ): WritableToken<T>
+
 export function findState<T, K extends Json.Serializable, Key extends K>(
 	token: ReadableFamilyToken<T, K>,
 	key: Key,
 ): ReadableToken<T>
+
 export function findState(
 	token: ReadableFamilyToken<any, any>,
 	key: Json.Serializable,
