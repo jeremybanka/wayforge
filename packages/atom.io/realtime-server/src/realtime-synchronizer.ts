ce53ab17156a5fa8f4fb797caaea7ad115a079d8
Jeremy Banka <hello@jeremybanka.com>
2024-01-19 16:53:08 -0800

    🚧 loop through transactions

--- packages/atom.io/realtime-server/src/realtime-synchronizer.ts
+++ packages/atom.io/realtime-server/src/realtime-synchronizer.ts
@@ -16,7 +16,7 @@ export function realtimeSynchronizer({
 	socket,
 	store = IMPLICIT.STORE,
 }: ServerConfig) {
-	return function synchronizer(syncGroup: SyncGroupToken): () => void {
+	return function synchronizer(syncGroup: SyncGroupToken): void {
 		const clientIdState = findInStore(
 			socketsOfClients.states.clientKeyOfSocket,
 			socket.id,
@@ -29,119 +29,119 @@ export function realtimeSynchronizer({
 			)
 		}
 
-		const tx = syncGroup.actions[0]
-
-		const socketUnacknowledgedUpdatesState = findInStore(
-			socketUnacknowledgedUpdatesSelectors,
-			socket.id,
-			store,
-		)
-		const socketUnacknowledgedUpdates = AtomIO.getState(
-			socketUnacknowledgedUpdatesState,
-			store,
-		)
-
-		const fillTransactionRequest = (update: AtomIO.TransactionUpdate<any>) => {
-			const performanceKey = `tx-run:${tx.key}:${update.id}`
-			const performanceKeyStart = `${performanceKey}:start`
-			const performanceKeyEnd = `${performanceKey}:end`
-			performance.mark(performanceKeyStart)
-			AtomIO.runTransaction(tx, update.id, store)(...update.params)
-			performance.mark(performanceKeyEnd)
-			const metric = performance.measure(
-				performanceKey,
-				performanceKeyStart,
-				performanceKeyEnd,
+		for (const tx of syncGroup.actions) {
+			const socketUnacknowledgedUpdatesState = findInStore(
+				socketUnacknowledgedUpdatesSelectors,
+				socket.id,
+				store,
 			)
-			store?.logger.info(`🚀`, `transaction`, tx.key, update.id, metric.duration)
-		}
-		socket.off(`tx-run:${tx.key}`, fillTransactionRequest)
-		socket.on(`tx-run:${tx.key}`, fillTransactionRequest)
+			const socketUnacknowledgedUpdates = AtomIO.getState(
+				socketUnacknowledgedUpdatesState,
+				store,
+			)
 
-		let unsubscribeFromTransaction: (() => void) | undefined
-		const fillTransactionSubscriptionRequest = () => {
-			unsubscribeFromTransaction = subscribeToTransaction(
-				tx,
-				(update) => {
-					const updateState = findInStore(completeUpdateAtoms, update.id, store)
-					AtomIO.setState(updateState, update, store)
-					const redactedUpdateKey = {
-						socketId: socket.id,
-						syncGroupKey: syncGroup.key,
-						updateId: update.id,
-					}
-					const redactedUpdateState = findInStore(
-						redactedPerspectiveUpdateSelectors,
-						redactedUpdateKey,
-						store,
-					)
-					const redactedUpdate = AtomIO.getState(redactedUpdateState, store)
+			const fillTransactionRequest = (update: AtomIO.TransactionUpdate<any>) => {
+				const performanceKey = `tx-run:${tx.key}:${update.id}`
+				const performanceKeyStart = `${performanceKey}:start`
+				const performanceKeyEnd = `${performanceKey}:end`
+				performance.mark(performanceKeyStart)
+				AtomIO.runTransaction(tx, update.id, store)(...update.params)
+				performance.mark(performanceKeyEnd)
+				const metric = performance.measure(
+					performanceKey,
+					performanceKeyStart,
+					performanceKeyEnd,
+				)
+				store?.logger.info(
+					`🚀`,
+					`transaction`,
+					tx.key,
+					update.id,
+					metric.duration,
+				)
+			}
+			socket.off(`tx-run:${tx.key}`, fillTransactionRequest)
+			socket.on(`tx-run:${tx.key}`, fillTransactionRequest)
 
+			let unsubscribeFromTransaction: (() => void) | undefined
+			const fillTransactionSubscriptionRequest = () => {
+				unsubscribeFromTransaction = subscribeToTransaction(
+					tx,
+					(update) => {
+						const updateState = findInStore(
+							completeUpdateAtoms,
+							update.id,
+							store,
+						)
+						AtomIO.setState(updateState, update, store)
+						const redactedUpdateKey = {
+							socketId: socket.id,
+							syncGroupKey: syncGroup.key,
+							updateId: update.id,
+						}
+						const redactedUpdateState = findInStore(
+							redactedPerspectiveUpdateSelectors,
+							redactedUpdateKey,
+							store,
+						)
+						const redactedUpdate = AtomIO.getState(redactedUpdateState, store)
+
+						AtomIO.setState(
+							socketUnacknowledgedUpdatesState,
+							(updates) => {
+								if (redactedUpdate) {
+									updates.push(redactedUpdate)
+									updates.sort((a, b) => a.epoch - b.epoch)
+								}
+								return updates
+							},
+							store,
+						)
+
+						socket.emit(`tx-new:${tx.key}`, redactedUpdate)
+					},
+					`tx-sub:${tx.key}:${socket.id}`,
+					store,
+				)
+				socket.on(`tx-unsub:${tx.key}`, unsubscribeFromTransaction)
+			}
+			socket.on(`tx-sub:${tx.key}`, fillTransactionSubscriptionRequest)
+
+			let i = 1
+			let next = 1
+			const retry = setInterval(() => {
+				const toEmit = socketUnacknowledgedUpdates[0]
+				console.log(clientId, socketUnacknowledgedUpdates)
+				if (toEmit && i === next) {
+					socket.emit(`tx-new:${tx.key}`, toEmit)
+					next *= 2
+				}
+
+				i++
+			}, 250)
+
+			const trackClientAcknowledgement = (epoch: number) => {
+				i = 1
+				next = 1
+				const socketEpochState = findInStore(
+					socketEpochSelectors,
+					socket.id,
+					store,
+				)
+
+				AtomIO.setState(socketEpochState, epoch, store)
+				if (socketUnacknowledgedUpdates[0]?.epoch === epoch) {
 					AtomIO.setState(
 						socketUnacknowledgedUpdatesState,
 						(updates) => {
-							if (redactedUpdate) {
-								updates.push(redactedUpdate)
-								updates.sort((a, b) => a.epoch - b.epoch)
-							}
+							updates.shift()
 							return updates
 						},
 						store,
 					)
-
-					socket.emit(`tx-new:${tx.key}`, redactedUpdate)
-				},
-				`tx-sub:${tx.key}:${socket.id}`,
-				store,
-			)
-			socket.on(`tx-unsub:${tx.key}`, unsubscribeFromTransaction)
-		}
-		socket.on(`tx-sub:${tx.key}`, fillTransactionSubscriptionRequest)
-
-		let i = 1
-		let next = 1
-		const retry = setInterval(() => {
-			const toEmit = socketUnacknowledgedUpdates[0]
-			console.log(clientId, socketUnacknowledgedUpdates)
-			if (toEmit && i === next) {
-				socket.emit(`tx-new:${tx.key}`, toEmit)
-				next *= 2
+				}
 			}
-
-			i++
-		}, 250)
-
-		const trackClientAcknowledgement = (epoch: number) => {
-			i = 1
-			next = 1
-			const socketEpochState = findInStore(
-				socketEpochSelectors,
-				socket.id,
-				store,
-			)
-
-			AtomIO.setState(socketEpochState, epoch, store)
-			if (socketUnacknowledgedUpdates[0]?.epoch === epoch) {
-				AtomIO.setState(
-					socketUnacknowledgedUpdatesState,
-					(updates) => {
-						updates.shift()
-						return updates
-					},
-					store,
-				)
-			}
-		}
-		socket.on(`tx-ack:${tx.key}`, trackClientAcknowledgement)
-
-		return () => {
-			if (unsubscribeFromTransaction) {
-				unsubscribeFromTransaction()
-				unsubscribeFromTransaction = undefined
-			}
-			clearInterval(retry)
-			socket.off(`tx-run:${tx.key}`, fillTransactionRequest)
-			socket.off(`tx-sub:${tx.key}`, fillTransactionSubscriptionRequest)
+			socket.on(`tx-ack:${tx.key}`, trackClientAcknowledgement)
 		}
 	}
 }
