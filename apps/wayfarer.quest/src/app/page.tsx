9ecabbd8a6b2e4683f4b7bcb499e7cffe9f06349
Jeremy Banka <hello@jeremybanka.com>
2024-02-10 23:15:12 -0800

    🐛 resolve SSR issue

--- apps/wayfarer.quest/src/app/page.tsx
+++ apps/wayfarer.quest/src/app/page.tsx
@@ -1,13 +1,32 @@
 "use client"
 
 import { useO } from "atom.io/react"
-import { AtomIODevtools } from "atom.io/react-devtools"
+import dynamic from "next/dynamic"
 
 import { roomViewState } from "wayfarer.quest/services/store/room-view-state"
 import Lobby from "./Lobby"
 import Room from "./Room"
 
 import scss from "./page.module.scss"
+
+// This function checks if the component should be loaded
+const shouldLoadComponent = () => {
+	// Replace `process.env.NODE_ENV !== 'production'` with any specific condition
+	// based on your environment variables or other logic to determine if this
+	// is a development or a preview environment
+	return process.env.NODE_ENV !== `production`
+}
+
+// Dynamically import the component only if shouldLoadComponent returns true
+const AtomIODevtools = dynamic(
+	() =>
+		shouldLoadComponent()
+			? import(`atom.io/react-devtools`).then((mod) => mod.AtomIODevtools)
+			: Promise.resolve(() => null),
+	{
+		ssr: false,
+	},
+)
 
 export default function SPA(): JSX.Element {
 	const roomView = useO(roomViewState)
