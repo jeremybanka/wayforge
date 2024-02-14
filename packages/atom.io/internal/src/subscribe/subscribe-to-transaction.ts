7e6c07ec7c5a6471780f1676642a543e17f535f9
Jeremy Banka <hello@jeremybanka.com>
2024-02-13 19:28:03 -0800

    🚚 atom.io/internal no longer imports from itself

--- packages/atom.io/internal/src/subscribe/subscribe-to-transaction.ts
+++ packages/atom.io/internal/src/subscribe/subscribe-to-transaction.ts
@@ -1,6 +1,6 @@
 import type { TransactionToken, TransactionUpdateHandler, ƒn } from "atom.io"
-import type { Store } from "atom.io/internal"
-import { withdraw } from "atom.io/internal"
+import type { Store } from ".."
+import { withdraw } from ".."
 
 export const subscribeToTransaction = <ƒ extends ƒn>(
 	token: TransactionToken<ƒ>,
