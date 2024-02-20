3fe0aad06d5f4a884756cda135e1b9ed1f4d1cd4
Jeremy Banka <hello@jeremybanka.com>
2024-02-20 11:11:24 -0800

    🚚 use relocated resources

--- apps/tempest.games/bun/gen-scripts.bun.ts
+++ apps/tempest.games/bun/gen-scripts.bun.ts
@@ -1,6 +1,6 @@
 async function gen() {
 	const htmxMinJS = await Bun.file(
-		`./node_modules/htmx.org/dist/htmx.min.js`,
+		`../../node_modules/htmx.org/dist/htmx.min.js`,
 	).text()
 	// const hyperScriptMinJS = await Bun.file(
 	// 	`./node_modules/hyperscript.org/dist/_hyperscript.min.js`,
