6ce6cce2308417912b3a6baa50dce00ab644688d
Jeremy Banka <hello@jeremybanka.com>
2024-03-10 14:30:03 -0700

    🛠️ everything seems to work

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,50 @@
 // @ts-check
-import eslint from '@eslint/js';
-import tslint from 'typescript-eslint';
+import eslint from 'eslint';
+import parser from '@typescript-eslint/parser';
+import plugin from '@typescript-eslint/eslint-plugin';
 
-export default tslint.config(
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-  eslint.configs.recommended,
-  ...tslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    rules: {
-      "@typescript-eslint/consistent-type-imports": [
-        `error`,
-        {
-          fixStyle: `separate-type-imports`,
-          prefer: `type-imports`, 
-        },
-      ],
-      "@typescript-eslint/explicit-member-accessibility": `error`,
-      "@typescript-eslint/explicit-module-boundary-types": `error`,
-      "@typescript-eslint/no-explicit-any": `off`,
-      "@typescript-eslint/no-unsafe-argument": `off`,
-      "@typescript-eslint/no-unsafe-assignment": `off`,
-      "@typescript-eslint/no-unsafe-member-access": `off`,
-      "@typescript-eslint/no-unsafe-return": `off`,
-      "@typescript-eslint/no-unused-vars": `off`,
-      "@typescript-eslint/prefer-enum-initializers": `error`,
-      "@typescript-eslint/prefer-literal-enum-member": `error`,
-      "@typescript-eslint/prefer-optional-chain": `error`,
-      "@typescript-eslint/sort-type-constituents": `error`,
-      "@typescript-eslint/type-annotation-spacing": `error`,
-      "@typescript-eslint/unified-signatures": `error`,
-  
-      "no-mixed-spaces-and-tabs": `off`,
-      "quotes": [`error`, `backtick`],
-      "quote-props": [`error`, `consistent`, { unnecessary: false }],
-    }
+/** @type {import("@typescript-eslint/parser").ParserOptions} */
+const parserOptions = {
+  project: `./tsconfig.json`,
+  sourceType: `module`
+}
+
+/** @type {import("eslint").Linter.FlatConfig} */
+const config = {
+  languageOptions: {
+    parser,
+    parserOptions
   },
-)
+  files: [`**/*.ts{,x}`, `eslint.config.js`],
+  ignores: [`**/dist/**/*`],
+  plugins: { "@typescript-eslint": plugin },
+  rules: {
+    "@typescript-eslint/consistent-type-imports": [
+      `error`,
+      {
+        fixStyle: `separate-type-imports`,
+        prefer: `type-imports`, 
+      },
+    ],
+    "@typescript-eslint/explicit-member-accessibility": `error`,
+    "@typescript-eslint/explicit-module-boundary-types": `error`,
+    "@typescript-eslint/no-explicit-any": `off`,
+    "@typescript-eslint/no-unsafe-argument": `off`,
+    "@typescript-eslint/no-unsafe-assignment": `off`,
+    "@typescript-eslint/no-unsafe-member-access": `off`,
+    "@typescript-eslint/no-unsafe-return": `off`,
+    "@typescript-eslint/no-unused-vars": `off`,
+    "@typescript-eslint/prefer-enum-initializers": `error`,
+    "@typescript-eslint/prefer-literal-enum-member": `error`,
+    "@typescript-eslint/prefer-optional-chain": `error`,
+    "@typescript-eslint/sort-type-constituents": `error`,
+    "@typescript-eslint/type-annotation-spacing": `error`,
+    "@typescript-eslint/unified-signatures": `error`,
+
+    "no-mixed-spaces-and-tabs": `off`,
+    "quotes": [`error`, `backtick`],
+    "quote-props": [`error`, `consistent`, { unnecessary: false }],
+  }
+}
+export default [config]
+
