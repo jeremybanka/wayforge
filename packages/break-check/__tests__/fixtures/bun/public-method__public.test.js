b663a7ed64fce7ab108c9e1cd1cc88fdfc8de032
Jeremy Banka <hello@jeremybanka.com>
2024-03-10 14:49:19 -0700

    🎨 apply rules

--- packages/break-check/__tests__/fixtures/bun/public-method__public.test.js
+++ packages/break-check/__tests__/fixtures/bun/public-method__public.test.js
@@ -1,8 +1,8 @@
 import { describe, it, expect } from "bun:test";
 import MyClass from "./src.js";
 
-describe('MyClass Public API Test', function() {
-  it('should test publicMethod', function() {
+describe('MyClass Public API Test', () => {
+  it('should test publicMethod', () => {
     const obj = new MyClass();
     expect(obj.publicMethod()).toBe("publicMethodOutput");
   });
