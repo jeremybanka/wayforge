6a24e181b1ec3ee5abcff871925bc921c3132edb
Jeremy Banka <hello@jeremybanka.com>
2024-04-07 13:06:18 -0700

    ✨ support async

--- packages/comline/src/encapsulate.ts
+++ packages/comline/src/encapsulate.ts
@@ -42,7 +42,7 @@ class FakeOut implements Pick<net.Socket, `write`> {
 	}
 }
 
-export function encapsulate<T>(fn: () => T): {
+function withCapturedOutput<T>(fn: () => T): {
 	returnValue: T
 	capturedStdout: string[]
 	capturedStderr: string[]
@@ -51,14 +51,45 @@ export function encapsulate<T>(fn: () => T): {
 	const originalStderrWrite = process.stderr.write.bind(process.stderr)
 	const fakeStdout = new FakeOut()
 	const fakeStderr = new FakeOut()
+
 	process.stdout.write = fakeStdout.write.bind(fakeStdout)
 	process.stderr.write = fakeStderr.write.bind(fakeStderr)
+
 	const returnValue = fn()
+
 	process.stdout.write = originalStdoutWrite
 	process.stderr.write = originalStderrWrite
+
 	return {
 		returnValue,
 		capturedStdout: fakeStdout.captured,
 		capturedStderr: fakeStderr.captured,
 	}
+}
+
+export function encapsulate<T>(fn: () => T): T extends Promise<infer U>
+	? Promise<{
+			returnValue: U
+			capturedStdout: string[]
+			capturedStderr: string[]
+		}>
+	: {
+			returnValue: T
+			capturedStdout: string[]
+			capturedStderr: string[]
+		} {
+	const { returnValue, capturedStdout, capturedStderr } = withCapturedOutput(fn)
+	if (returnValue instanceof Promise) {
+		const promise = returnValue.then((returnValue) => ({
+			returnValue,
+			capturedStdout,
+			capturedStderr,
+		}))
+		return promise as any
+	}
+	return {
+		returnValue,
+		capturedStdout,
+		capturedStderr,
+	} as any
 }
