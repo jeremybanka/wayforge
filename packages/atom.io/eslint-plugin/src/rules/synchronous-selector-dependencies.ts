f288c94ef90dacd8aac515284aba04271b484473
Jeremy Banka <hello@jeremybanka.com>
2024-04-15 12:24:10 -0700

    atom.io/eslint plugin.synchronous selector dependencies (#1750)
    * ✨ support non-destructured transactors
    
    * ✨ support selectorFamily
    
    * 🧹 clean up
    
    * 🦋
    
    * 🐛 fix bug where the non-destructured transactors might not be present

--- packages/atom.io/eslint-plugin/src/rules/synchronous-selector-dependencies.ts
+++ packages/atom.io/eslint-plugin/src/rules/synchronous-selector-dependencies.ts
@@ -81,17 +81,41 @@ export const synchronousSelectorDependencies = {
 	create(context) {
 		return {
 			CallExpression(node) {
-				if (`name` in node.callee && node.callee.name === `selector`) {
-					let paramName: string | null = null
-					const foundAwait = false
+				let selectorComputation: ESTree.Node | undefined
+				if (`name` in node.callee && node.callee.name === `selectorFamily`) {
 					if (node.arguments[0].type === `ObjectExpression`) {
-						const destructuredGet = node.arguments[0].properties.find(
-							(prop): prop is ESTree.AssignmentProperty =>
-								`key` in prop && `name` in prop.key && prop.key.name === `get`,
+						const selectorLookupProperty = node.arguments[0].properties.find(
+							(prop): prop is ESTree.Property => {
+								return (
+									`key` in prop && `name` in prop.key && prop.key.name === `get`
+								)
+							},
 						)
-						if (destructuredGet && `name` in destructuredGet.key) {
-							paramName = destructuredGet.key.name
+						const selectorLookup = selectorLookupProperty?.value
+						if (
+							selectorLookup?.type === `FunctionExpression` ||
+							selectorLookup?.type === `ArrowFunctionExpression`
+						) {
+							if (selectorLookup.body.type === `BlockStatement`) {
+								for (const statement of selectorLookup.body.body) {
+									if (
+										statement.type === `ReturnStatement` &&
+										statement.argument
+									) {
+										selectorComputation = statement.argument
+									}
+								}
+							} else if (
+								selectorLookup.body.type === `FunctionExpression` ||
+								selectorLookup.body.type === `ArrowFunctionExpression`
+							) {
+								selectorComputation = selectorLookup.body
+							}
 						}
+					}
+				}
+				if (`name` in node.callee && node.callee.name === `selector`) {
+					if (node.arguments[0].type === `ObjectExpression`) {
 						const selectorComputationProperty =
 							node.arguments[0].properties.find(
 								(prop): prop is ESTree.Property => {
@@ -102,41 +126,63 @@ export const synchronousSelectorDependencies = {
 									)
 								},
 							)
-						const selectorComputation = selectorComputationProperty?.value
-						if (
-							selectorComputation?.type === `FunctionExpression` ||
-							selectorComputation?.type === `ArrowFunctionExpression`
-						) {
-							let awaited: number | undefined
-							let awaitNode: ESTree.AwaitExpression | undefined
-							walk(selectorComputation, (n, depth) => {
-								if (typeof awaited === `number`) {
-									if (awaited > depth) {
-										awaited = undefined
-										awaitNode = undefined
+						selectorComputation = selectorComputationProperty?.value
+					}
+				}
+				if (
+					selectorComputation?.type === `FunctionExpression` ||
+					selectorComputation?.type === `ArrowFunctionExpression`
+				) {
+					const nonDestructuredTransactorsName =
+						selectorComputation.params[0] &&
+						`name` in selectorComputation.params[0]
+							? selectorComputation.params[0].name
+							: undefined
+					let awaited: number | undefined
+					let awaitNode: ESTree.AwaitExpression | undefined
+					walk(selectorComputation, (n, depth) => {
+						// console.log(`${`\t`.repeat(depth)}${n.type} ${n.name ?? ``}`)
+						if (typeof awaited === `number`) {
+							if (awaited > depth) {
+								awaited = undefined
+								awaitNode = undefined
+							}
+						}
+						switch (n.type) {
+							case `AwaitExpression`:
+								awaited = depth
+								awaitNode = n
+								break
+							case `CallExpression`:
+								if (awaitNode) {
+									let willReport = false
+									switch (n.callee.type) {
+										case `MemberExpression`:
+											if (
+												n.callee.object.type === `Identifier` &&
+												n.callee.object.name ===
+													nonDestructuredTransactorsName &&
+												n.callee.property.type === `Identifier` &&
+												n.callee.property.name === `get`
+											) {
+												willReport = true
+											}
+											break
+										case `Identifier`:
+											if (n.callee.name === `get`) {
+												willReport = true
+											}
+											break
+									}
+									if (willReport) {
+										context.report({
+											node: awaitNode,
+											message: `Using await before calling the 'get' transactor is not allowed.`,
+										})
 									}
 								}
-								switch (n.type) {
-									case `AwaitExpression`:
-										awaited = depth
-										awaitNode = n
-										break
-									case `CallExpression`:
-										if (
-											`name` in n.callee &&
-											n.callee.name === `get` &&
-											awaitNode
-										) {
-											context.report({
-												node: awaitNode,
-												message: `Using await before calling the 'get' transactor is not allowed.`,
-											})
-										}
-										break
-								}
-							})
 						}
-					}
+					})
 				}
 			},
 		}
