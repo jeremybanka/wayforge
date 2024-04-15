9e35ab4ebf7d7d359f9affa84df0677df57d9dc0
Jeremy Banka <hello@jeremybanka.com>
2024-04-15 00:13:28 -0700

    ✅ test atom.io/eslint-plugin

---
+++ packages/atom.io/__tests__/public/eslint-plugin/synchronous-selector-dependencies.test.ts
@@ -0,0 +1,60 @@
+import { RuleTester } from "eslint"
+
+import { Rules } from "atom.io/eslint-plugin"
+
+const ruleTester = new RuleTester()
+Object.assign(ruleTester, { describe, it })
+const rule = Rules.synchronousSelectorDependencies
+
+ruleTester.run(`synchronous-selector-dependencies`, rule, {
+	valid: [
+		{
+			code: `
+        const doubleState = selector({
+          key: "double",
+          get: (transactors) => transactors.get(countState) * 2,
+        })
+      `,
+		},
+		{
+			code: `
+        const doubleState = selector({
+          key: "double",
+          get: (transactors) => {
+            const { get } = transactors
+            const count = get(countState)
+            return count * 2
+          },
+        })
+      `,
+		},
+		{
+			code: `
+        const doubleState = selector({
+          key: "double",
+          get: function ({ get }) {
+            return get(countState) * 2
+          },
+        })
+      `,
+		},
+	],
+	invalid: [
+		{
+			code: `
+        selector({
+          get: async ({ get }) => {
+            const response = await fetch("https://api.github.com/users/jeremybanka")
+            const json = await response.json()
+            if ("key" in json) {
+              return get(myRecordState)[json.key]
+            } else {
+              return null
+            }
+          },
+        })
+      `,
+			errors: 1,
+		},
+	],
+})
