d7b708ade74d5021787a52ff75d48900f824d110
Jeremy Banka <hello@jeremybanka.com>
2024-04-15 11:51:09 -0700

    ✨ support selectorFamily

--- packages/atom.io/__tests__/public/eslint-plugin/synchronous-selector-dependencies.test.ts
+++ packages/atom.io/__tests__/public/eslint-plugin/synchronous-selector-dependencies.test.ts
@@ -153,3 +153,157 @@ ruleTester.run(`synchronous-selector-dependencies (selector)`, rule, {
 		},
 	],
 })
+ruleTester.run(`synchronous-selector-dependencies (selectorFamily)`, rule, {
+	valid: [
+		{
+			name: `arrow function without body as getter`,
+			code: `
+        const doubleState = selectorFamily({
+          key: "double",
+          get: () => (transactors) => transactors.get(countState) * 2,
+        })
+      `,
+		},
+		{
+			name: `arrow function with body as getter`,
+			code: `
+        const doubleState = selectorFamily({
+          key: "double",
+          get: () => {
+            return (transactors) => {
+              const { get } = transactors
+              const count = get(countState)
+              return count * 2
+            }
+          },
+        })
+      `,
+		},
+		{
+			name: `classic function expression as getter`,
+			code: `
+        const doubleState = selectorFamily({
+          key: "double",
+          get: function () {
+            return function ({ get }) {
+              return get(countState) * 2
+            }
+          },
+        })
+      `,
+		},
+		{
+			name: `async arrow function expression without body as getter`,
+			code: `
+        const clientNameState = selectorFamily<Loadable<string | null>>({
+          key: "clientName",
+          get: () => async ({ get }) => pipe(
+            get(clientQueryState),
+            (query) => until(query, null),
+            async (query) => await (query?.json() ?? query),
+            (json) => json.name,
+          )
+        })
+      `,
+		},
+		{
+			name: `async arrow function expression with body as getter`,
+			code: `
+        const clientNameState = selectorFamily<Loadable<string | null>>({
+          key: "clientName",
+          get: () => async ({ get }) => {
+            const query = get(clientQueryState)
+            const json = await query.json()
+            return json.name
+          }
+        })
+      `,
+		},
+		{
+			name: `async classic function expression as getter`,
+			code: `
+        const clientNameState = selectorFamily<Loadable<string | null>>({
+          key: "clientName",
+          get: function () { 
+            return async function ({ get }) {
+              const query = get(clientQueryState)
+              const json = await query.json()
+              return json.name
+            }
+          }
+        })
+      `,
+		},
+	],
+	invalid: [
+		{
+			name: `straightforward violation`,
+			code: `
+        const clientNameState = selectorFamily<Loadable<string | null>>({
+          key: "clientName",
+          get: () => async ({ find, get }) => {
+            const query = await fetch("https://api.github.com/users/jeremybanka")
+            const json = await query.json()
+            const clientState = find(clientAtoms, json.key)
+            const client = get(clientState)
+            return client?.name
+          }
+        })
+      `,
+			errors: 1,
+		},
+		{
+			name: `straightforward violation (non-destructured)`,
+			code: `
+        const clientNameState = selectorFamily<Loadable<string | null>>({
+          key: "clientName",
+          get: () => async (transactors) => {
+            const query = await fetch("https://api.github.com/users/jeremybanka")
+            const json = await query.json()
+            const clientState = transactors.find(clientAtoms, json.key)
+            const client = transactors.get(clientState)
+            return client?.name
+          }
+        })
+      `,
+			errors: 1,
+		},
+		{
+			name: `"if" conditional await-get in arrow function with body`,
+			code: `
+        selectorFamily({
+          get: () => async ({ get }) => {
+            const response = await fetch("https://api.github.com/users/jeremybanka")
+            const json = await response.json()
+            if ("key" in json) {
+              return get(myRecordState)[json.key]
+            } else {
+              return null
+            }
+          },
+        })
+      `,
+			errors: 1,
+		},
+		{
+			name: `"switch" conditional await-get in classic function expression`,
+			code: `
+        selectorFamily({
+          get: function () {
+            return async function ({ get }) {
+              const response = await fetch("https://api.github.com/users/jeremybanka")
+              const json = await response.json()
+              switch (json.type) {
+                case "user":
+                  return get(myRecordState)[json.key]
+                default:
+                  return null
+              }
+            }
+          },
+        })
+      `,
+			errors: 1,
+		},
+	],
+})
--- packages/atom.io/eslint-plugin/src/rules/synchronous-selector-dependencies.ts
+++ packages/atom.io/eslint-plugin/src/rules/synchronous-selector-dependencies.ts
@@ -81,6 +81,41 @@ export const synchronousSelectorDependencies = {
 	create(context) {
 		return {
 			CallExpression(node) {
+				let selectorComputation: ESTree.Node | undefined
+				if (`name` in node.callee && node.callee.name === `selectorFamily`) {
+					console.log(`🐛`, node.arguments[0].type)
+					if (node.arguments[0].type === `ObjectExpression`) {
+						const selectorLookupProperty = node.arguments[0].properties.find(
+							(prop): prop is ESTree.Property => {
+								return (
+									`key` in prop && `name` in prop.key && prop.key.name === `get`
+								)
+							},
+						)
+						const selectorLookup = selectorLookupProperty?.value
+						console.log(`🐛`, selectorLookup?.type)
+						if (
+							selectorLookup?.type === `FunctionExpression` ||
+							selectorLookup?.type === `ArrowFunctionExpression`
+						) {
+							if (selectorLookup.body.type === `BlockStatement`) {
+								for (const statement of selectorLookup.body.body) {
+									if (
+										statement.type === `ReturnStatement` &&
+										statement.argument
+									) {
+										selectorComputation = statement.argument
+									}
+								}
+							} else if (
+								selectorLookup.body.type === `FunctionExpression` ||
+								selectorLookup.body.type === `ArrowFunctionExpression`
+							) {
+								selectorComputation = selectorLookup.body
+							}
+						}
+					}
+				}
 				if (`name` in node.callee && node.callee.name === `selector`) {
 					if (node.arguments[0].type === `ObjectExpression`) {
 						const selectorComputationProperty =
@@ -93,62 +128,62 @@ export const synchronousSelectorDependencies = {
 									)
 								},
 							)
-						const selectorComputation = selectorComputationProperty?.value
-						if (
-							selectorComputation?.type === `FunctionExpression` ||
-							selectorComputation?.type === `ArrowFunctionExpression`
-						) {
-							const nonDestructuredTransactorsName =
-								`name` in selectorComputation.params[0]
-									? selectorComputation.params[0].name
-									: undefined
-							let awaited: number | undefined
-							let awaitNode: ESTree.AwaitExpression | undefined
-							walk(selectorComputation, (n, depth) => {
-								// console.log(`${`\t`.repeat(depth)}${n.type}`)
-								if (typeof awaited === `number`) {
-									if (awaited > depth) {
-										awaited = undefined
-										awaitNode = undefined
+						selectorComputation = selectorComputationProperty?.value
+					}
+				}
+				if (
+					selectorComputation?.type === `FunctionExpression` ||
+					selectorComputation?.type === `ArrowFunctionExpression`
+				) {
+					const nonDestructuredTransactorsName =
+						`name` in selectorComputation.params[0]
+							? selectorComputation.params[0].name
+							: undefined
+					let awaited: number | undefined
+					let awaitNode: ESTree.AwaitExpression | undefined
+					walk(selectorComputation, (n, depth) => {
+						// console.log(`${`\t`.repeat(depth)}${n.type} ${n.name ?? ``}`)
+						if (typeof awaited === `number`) {
+							if (awaited > depth) {
+								awaited = undefined
+								awaitNode = undefined
+							}
+						}
+						switch (n.type) {
+							case `AwaitExpression`:
+								awaited = depth
+								awaitNode = n
+								break
+							case `CallExpression`:
+								if (awaitNode) {
+									let willReport = false
+									switch (n.callee.type) {
+										case `MemberExpression`:
+											if (
+												n.callee.object.type === `Identifier` &&
+												n.callee.object.name ===
+													nonDestructuredTransactorsName &&
+												n.callee.property.type === `Identifier` &&
+												n.callee.property.name === `get`
+											) {
+												willReport = true
+											}
+											break
+										case `Identifier`:
+											if (n.callee.name === `get`) {
+												willReport = true
+											}
+											break
+									}
+									if (willReport) {
+										context.report({
+											node: awaitNode,
+											message: `Using await before calling the 'get' transactor is not allowed.`,
+										})
 									}
 								}
-								switch (n.type) {
-									case `AwaitExpression`:
-										awaited = depth
-										awaitNode = n
-										break
-									case `CallExpression`:
-										if (awaitNode) {
-											let willReport = false
-											switch (n.callee.type) {
-												case `MemberExpression`:
-													if (
-														n.callee.object.type === `Identifier` &&
-														n.callee.object.name ===
-															nonDestructuredTransactorsName &&
-														n.callee.property.type === `Identifier` &&
-														n.callee.property.name === `get`
-													) {
-														willReport = true
-													}
-													break
-												case `Identifier`:
-													if (n.callee.name === `get`) {
-														willReport = true
-													}
-													break
-											}
-											if (willReport) {
-												context.report({
-													node: awaitNode,
-													message: `Using await before calling the 'get' transactor is not allowed.`,
-												})
-											}
-										}
-								}
-							})
 						}
-					}
+					})
 				}
 			},
 		}
