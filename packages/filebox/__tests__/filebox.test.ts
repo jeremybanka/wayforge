ab0401ae9cd10172d46367b1cee7ea6c390c4637
Jeremy Banka <hello@jeremybanka.com>
2024-04-21 01:05:12 -0700

    ✅ more testing

--- packages/filebox/__tests__/filebox.test.ts
+++ packages/filebox/__tests__/filebox.test.ts
@@ -2,9 +2,15 @@ import * as fs from "node:fs"
 import * as http from "node:http"
 import path from "node:path"
 
+import { OpenAI } from "openai"
 import * as tmp from "tmp"
 
 import { Filebox } from "../src"
+
+const openAiClient = new OpenAI({
+	apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+	dangerouslyAllowBrowser: process.env.NODE_ENV === `test`,
+})
 
 let server: http.Server
 let tempDir: tmp.DirResult
@@ -42,7 +48,7 @@ describe(`Filebox`, () => {
 				return fetch(url).then((response) => response.text())
 			},
 		})
-		const result = await responses.get(`http://localhost:12500`)
+		const result = await responses.get(`home`, `http://localhost:12500`)
 		expect(result).toBe(`The best way to predict the future is to invent it.`)
 		expect(utils.put).toHaveBeenCalledTimes(1)
 	})
@@ -56,7 +62,7 @@ describe(`Filebox`, () => {
 		})
 		let caught: Error | undefined
 		try {
-			await get(`http://localhost:12500`)
+			await get(`home`, `http://localhost:12500`)
 		} catch (thrown) {
 			if (thrown instanceof Error) {
 				caught = thrown
@@ -66,16 +72,38 @@ describe(`Filebox`, () => {
 			expect(utils.put).toHaveBeenCalledTimes(0)
 		}
 		fs.writeFileSync(
-			path.join(tempDir.name, `hello.input.json`),
+			path.join(tempDir.name, `hello.home.input.json`),
 			`["http://localhost:12500"]`,
 		)
 		fs.writeFileSync(
-			path.join(tempDir.name, `hello.output.json`),
+			path.join(tempDir.name, `hello.home.output.json`),
 			`"The best way to predict the future is to invent it."`,
 		)
 
-		const result = await get(`http://localhost:12500`)
+		const result = await get(`home`, `http://localhost:12500`)
 		expect(result).toBe(`The best way to predict the future is to invent it.`)
 		expect(utils.put).toHaveBeenCalledTimes(0)
+	})
+	test(`mode:read-write`, async () => {
+		const filebox = new Filebox(`read-write`, tempDir.name)
+		const completions = filebox.add({
+			key: `openai`,
+			get: openAiClient.chat.completions.create.bind(
+				openAiClient.chat.completions,
+			),
+		})
+		const result = await completions.get(`french-capital`, {
+			model: `gpt-3.5-turbo`,
+			messages: [
+				{
+					role: `system`,
+					content: `You are a helpful assistant.`,
+				},
+				{
+					role: `user`,
+					content: `What is the capital of France?`,
+				},
+			],
+		})
 	})
 })
--- packages/filebox/src/filebox.ts
+++ packages/filebox/src/filebox.ts
@@ -11,11 +11,11 @@ export class Filebox {
 		public baseDir: string = path.join(process.cwd(), `.filebox`),
 	) {}
 
-	private read<I extends Json.Array, O extends Json.Serializable>(
-		key: string,
-		args: I,
-	): O {
-		const pathToInputFile = path.join(this.baseDir, `${key}.input.json`)
+	private read<I extends any[], O>(key: string, subKey: string, args: I): O {
+		const pathToInputFile = path.join(
+			this.baseDir,
+			`${key}.${subKey}.input.json`,
+		)
 		if (!fs.existsSync(pathToInputFile)) {
 			throw new Error(`Filebox: input file for key "${key}" does not exist`)
 		}
@@ -26,17 +26,27 @@ export class Filebox {
 				`Filebox: the input for "key" contained in the filebox does not match the input provided.`,
 			)
 		}
-		const pathToOutputFile = path.join(this.baseDir, `${key}.output.json`)
+		const pathToOutputFile = path.join(
+			this.baseDir,
+			`${key}.${subKey}.output.json`,
+		)
 		return JSON.parse(fs.readFileSync(pathToOutputFile, `utf-8`))
 	}
 
-	private async write<I extends Json.Array, O extends Json.Serializable>(
+	private async write<I extends any[], O>(
 		key: string,
+		subKey: string,
 		args: I,
 		get: (...args: I) => Promise<O>,
 	): Promise<O> {
-		const pathToInputFile = path.join(this.baseDir, `${key}.input.json`)
-		const pathToOutputFile = path.join(this.baseDir, `${key}.output.json`)
+		const pathToInputFile = path.join(
+			this.baseDir,
+			`${key}.${subKey}.input.json`,
+		)
+		const pathToOutputFile = path.join(
+			this.baseDir,
+			`${key}.${subKey}.output.json`,
+		)
 		const inputStringified = JSON.stringify(args)
 		if (!fs.existsSync(this.baseDir)) {
 			fs.mkdirSync(this.baseDir)
@@ -47,29 +57,29 @@ export class Filebox {
 		return output
 	}
 
-	public add<I extends Json.Array, O extends Json.Serializable>({
+	public add<I extends any[], O>({
 		key,
 		get,
 	}: { key: string; get: (...args: I) => Promise<O> }): {
-		get: (...args: I) => Promise<O>
+		get: (key: string, ...args: I) => Promise<O>
 	} {
 		return {
-			get: async (...args: I): Promise<O> => {
+			get: async (subKey: string, ...args: I): Promise<O> => {
 				switch (this.mode) {
 					case `off`:
 						return get(...args)
 					case `read`: {
-						return this.read<I, O>(key, args)
+						return this.read<I, O>(key, subKey, args)
 					}
 					case `write`: {
-						return this.write(key, args, get)
+						return this.write(key, subKey, args, get)
 					}
 					case `read-write`: {
 						try {
-							return this.read<I, O>(key, args)
+							return this.read<I, O>(key, subKey, args)
 						} catch (thrown) {
 							if (thrown instanceof Error) {
-								return this.write(key, args, get)
+								return this.write(key, subKey, args, get)
 							}
 							throw thrown
 						}
