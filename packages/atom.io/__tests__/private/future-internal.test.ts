14b80c13068d5b6c0340a0985714b11c27c6af46
Jeremy Banka <hello@jeremybanka.com>
2024-05-09 15:58:03 -0700

    ♻️ refactor away from setImmediate; suspect this may lead to flaky branching

--- packages/atom.io/__tests__/private/future-internal.test.ts
+++ packages/atom.io/__tests__/private/future-internal.test.ts
@@ -9,32 +9,34 @@ beforeEach(() => {
 
 describe(`Future`, () => {
 	it(`is a Promise whose fate can change`, async () => {
-		const future = new Future<number>((resolve) =>
-			setImmediate(() => {
-				resolve(1)
-			}),
-		)
+		const future = new Future<number>((resolve) => {
+			resolve(1)
+		})
 		future.use(
-			new Promise((resolve) =>
-				setImmediate(() => {
-					resolve(2)
-				}),
-			),
+			new Promise((resolve) => {
+				resolve(2)
+			}),
 		)
 		expect(await future.then((value) => value)).toBe(2)
 	})
 	it(`can be resolved to a value immediately`, async () => {
-		const future = new Future<number>((resolve) =>
-			setImmediate(() => {
-				resolve(1)
-			}),
-		)
+		const future = new Future<number>((resolve) => {
+			resolve(1)
+		})
 		future.use(2)
 		expect(await future.then((value) => value)).toBe(2)
 	})
 	it(`handles an initial executor that rejects`, async () => {
-		const future = new Future<number>((_, reject) =>
-			setImmediate(() => {
+		const future = new Future<number>((_, reject) => {
+			reject(new Error(`whoops`))
+		})
+		await expect(async () => future.then((value) => value)).rejects.toThrow(
+			`whoops`,
+		)
+	})
+	it(`handles an initial promise that rejects`, async () => {
+		const future = new Future<number>(
+			new Promise((_, reject) => {
 				reject(new Error(`whoops`))
 			}),
 		)
@@ -42,47 +44,27 @@ describe(`Future`, () => {
 			`whoops`,
 		)
 	})
-	it(`handles an initial promise that rejects`, async () => {
-		const future = new Future<number>(
-			new Promise((_, reject) =>
-				setImmediate(() => {
-					reject(new Error(`whoops`))
-				}),
-			),
-		)
-		await expect(async () => future.then((value) => value)).rejects.toThrow(
-			`whoops`,
-		)
-	})
 	it(`handles a new use-Promise that rejects`, async () => {
-		const future = new Future<number>((resolve) =>
-			setImmediate(() => {
-				resolve(1)
-			}),
-		)
+		const future = new Future<number>((resolve) => {
+			resolve(1)
+		})
 		future.use(
-			new Promise((_, reject) =>
-				setImmediate(() => {
-					reject(new Error(`whoops`))
-				}),
-			),
+			new Promise((_, reject) => {
+				reject(new Error(`whoops`))
+			}),
 		)
 		await expect(async () => future.then((value) => value)).rejects.toThrow(
 			`whoops`,
 		)
 	})
 	it(`does not care about a previous rejection, after a new use-static`, async () => {
-		const future = new Future<number>((resolve) =>
-			setImmediate(() => {
-				resolve(1)
-			}),
-		)
+		const future = new Future<number>((resolve) => {
+			resolve(1)
+		})
 		future.use(
-			new Promise((_, reject) =>
-				setImmediate(() => {
-					reject(new Error(`whoops`))
-				}),
-			),
+			new Promise((_, reject) => {
+				reject(new Error(`whoops`))
+			}),
 		)
 		future.use(2)
 		expect(await future.then((value) => value)).toBe(2)
