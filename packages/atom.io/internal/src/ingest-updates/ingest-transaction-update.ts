89eecdd3c250888b1551ae1423669fb3f9c3ecad
Jeremy Banka <hello@jeremybanka.com>
2024-05-21 01:17:31 -0700

    ✨ ingest disposal/creation events inside transaction updates

--- packages/atom.io/internal/src/ingest-updates/ingest-transaction-update.ts
+++ packages/atom.io/internal/src/ingest-updates/ingest-transaction-update.ts
@@ -2,6 +2,10 @@ import type { TransactionUpdate } from "atom.io"
 
 import type { Store } from "../store"
 import { ingestAtomUpdate } from "./ingest-atom-update"
+import {
+	ingestCreationEvent,
+	ingestDisposalEvent,
+} from "./ingest-creation-disposal"
 
 export function ingestTransactionUpdate(
 	applying: `newValue` | `oldValue`,
@@ -19,8 +23,14 @@ export function ingestTransactionUpdate(
 				ingestAtomUpdate(applying, updateFromTransaction, store)
 				break
 			case `state_creation`:
+				ingestCreationEvent(updateFromTransaction, applying, store)
+				break
+			case `state_disposal`:
+				ingestDisposalEvent(updateFromTransaction, applying, store)
 				break
 			case `molecule_creation`:
+				break
+			case `molecule_disposal`:
 				break
 			case `transaction_update`:
 				ingestTransactionUpdate(applying, updateFromTransaction, store)
