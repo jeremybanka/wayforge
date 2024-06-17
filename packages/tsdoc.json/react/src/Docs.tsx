62dcedd52d3775502265aaea3374bdafa3bb04d9
Jeremy Banka <hello@jeremybanka.com>
2024-06-16 22:42:55 -0700

    ✨ print all names

--- packages/tsdoc.json/react/src/Docs.tsx
+++ packages/tsdoc.json/react/src/Docs.tsx
@@ -1,3 +1,22 @@
-export function Docs(): JSX.Element {
-	return <div>Hello World</div>
+import type { TSD } from "tsdoc.json"
+
+export type DocsProps = {
+	docs: TSD.Doc[]
+	className?: string
+}
+export function Docs({ docs, className = `` }: DocsProps): JSX.Element {
+	return (
+		<div className={className}>
+			{docs.map((doc) => (
+				<Doc key={doc.name} doc={doc} />
+			))}
+		</div>
+	)
+}
+
+export type DocProps = {
+	doc: TSD.Doc
+}
+export function Doc({ doc }: DocProps): JSX.Element {
+	return <div>{doc.name}</div>
 }
