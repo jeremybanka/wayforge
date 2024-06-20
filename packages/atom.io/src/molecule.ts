2515fe36840413d30a6498c78a2f8a62943fd931
Jeremy Banka <hello@jeremybanka.com>
2024-06-20 16:13:02 -0700

    ✨ join transactor returns token

--- packages/atom.io/src/molecule.ts
+++ packages/atom.io/src/molecule.ts
@@ -39,7 +39,7 @@ export type MoleculeTransactors<K extends Json.Serializable> = Flat<
 
 		claim(below: MoleculeToken<any>, options: { exclusive: boolean })
 
-		join(joinToken: JoinToken<any, any, any, any>): void
+		join<J extends JoinToken<any, any, any, any>>(joinToken:J): J
 
 		spawn<Key extends Json.Serializable, Ctor extends MoleculeConstructor>(
 			family: MoleculeFamilyToken<Ctor>,
