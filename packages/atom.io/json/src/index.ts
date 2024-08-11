e5c62b9d89229f057475ca66972ca2492239e2c5
Jeremy Banka <hello@jeremybanka.com>
2024-08-10 20:29:49 -0700

    🏷️ Canonical

--- packages/atom.io/json/src/index.ts
+++ packages/atom.io/json/src/index.ts
@@ -1,8 +1,11 @@
+import type { Json } from "~/packages/anvl/src/json"
+import type { primitive } from "~/packages/anvl/src/primitive"
+
 export * from "./select-json"
 export * from "./select-json-family"
-export * from "~/packages/anvl/src/json"
-export * from "~/packages/anvl/src/primitive"
 
-import type { Json } from "~/packages/anvl/src/json"
+export type { Json, primitive }
+
+export type Canonical = primitive | ReadonlyArray<Canonical>
 
 export type JsonIO = (...params: Json.Serializable[]) => Json.Serializable | void
