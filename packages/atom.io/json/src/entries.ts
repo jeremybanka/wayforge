b93a14f2a61209b61cf0dde0254f1b83da5f24b4
Jeremy Banka <hello@jeremybanka.com>
2024-08-12 00:05:28 -0700

    🎨 CertainEntry -> ValueOfEntry

--- packages/atom.io/json/src/entries.ts
+++ packages/atom.io/json/src/entries.ts
@@ -6,12 +6,12 @@ export type KeyOfEntries<E extends Entries> = E extends [infer K, any][]
 	? K
 	: never
 
-export type CertainEntry<E extends Entries, K extends KeyOfEntries<E>> = {
+export type ValueOfEntry<E extends Entries, K extends KeyOfEntries<E>> = {
 	[P in Range<E[`length`]>]: E[P] extends [K, infer V] ? V : never
 }[Range<E[`length`]>]
 
 export type FromEntries<E extends Entries> = Flat<{
-	[K in KeyOfEntries<E>]: CertainEntry<E, K>
+	[K in KeyOfEntries<E>]: ValueOfEntry<E, K>
 }>
 
 export function fromEntries<E extends Entries>(entries: E): FromEntries<E> {
