8ed306d5368e8567f44d256702186c2c94c5bbe7
Jeremy Banka <hello@jeremybanka.com>
2024-08-11 22:54:35 -0700

    🧹 clean up

--- packages/atom.io/json/src/entries.ts
+++ packages/atom.io/json/src/entries.ts
@@ -1,29 +1,21 @@
 import type { Flat } from "atom.io/internal"
 
-export type Entries<K extends keyof any, V> = [key: K, value: V][]
+export type Entries<K extends keyof any = keyof any, V = any> = [K, V][]
 
-export type KeyOfEntries<E extends Entries<any, any>> = E extends [
-	key: infer K,
-	value: any,
-][]
+export type KeyOfEntries<E extends Entries> = E extends [infer K, any][]
 	? K
 	: never
 
 export type UpToTen = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
-export type CertainEntry<
-	E extends Entries<any, any>,
-	K extends KeyOfEntries<E>,
-> = {
-	[P in UpToTen]: E[P] extends [key: K, value: infer V] ? V : never
+export type CertainEntry<E extends Entries, K extends KeyOfEntries<E>> = {
+	[P in UpToTen]: E[P] extends [K, infer V] ? V : never
 }[UpToTen]
 
-export type FromEntries<E extends Entries<keyof any, any>> = Flat<{
+export type FromEntries<E extends Entries> = Flat<{
 	[K in KeyOfEntries<E>]: CertainEntry<E, K>
 }>
 
-export function fromEntries<E extends Entries<keyof any, any>>(
-	entries: E,
-): FromEntries<E> {
+export function fromEntries<E extends Entries>(entries: E): FromEntries<E> {
 	return Object.fromEntries(entries) as FromEntries<E>
 }
