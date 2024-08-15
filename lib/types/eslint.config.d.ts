74459edba75596a73d4cd2f8f86e1d59ee419286
Jeremy Banka <hello@jeremybanka.com>
2024-08-14 17:45:20 -0700

    ➕ types/eslint

--- lib/types/eslint.config.d.ts
+++ lib/types/eslint.config.d.ts
@@ -1,20 +1,22 @@
+import type { ESLint } from "eslint"
+
 // eslint-disable-next-line quotes
 declare module "atom.io/eslint-plugin" {
-	const plugin: Eslint.Plugin
+	const plugin: ESLint.Plugin
 	export default plugin
 }
 // eslint-disable-next-line quotes
 declare module "eslint-plugin-import" {
-	const plugin: Eslint.Plugin
+	const plugin: ESLint.Plugin
 	export = plugin
 }
 // eslint-disable-next-line quotes
 declare module "@next/eslint-plugin-next" {
-	const plugin: Eslint.Plugin
+	const plugin: ESLint.Plugin
 	export default plugin
 }
 // eslint-disable-next-line quotes
 declare module "@typescript-eslint/eslint-plugin" {
-	const plugin: Eslint.Plugin
+	const plugin: ESLint.Plugin
 	export = plugin
 }
