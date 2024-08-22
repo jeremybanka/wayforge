8e69912c8beeb785d897dd3c9acea5ab8626ba46
Jeremy Banka <hello@jeremybanka.com>
2024-08-22 16:13:48 -0700

    ✅ test is-json

---
+++ packages/atom.io/__tests__/public/json/is-json.test.ts
@@ -0,0 +1,18 @@
+import { isJson } from "atom.io/json"
+
+describe(`isJson`, () => {
+	it(`knows whether an unknown is deeply json`, () => {
+		expect(isJson(null)).toBe(true)
+		expect(isJson([])).toBe(true)
+		expect(isJson({})).toBe(true)
+		expect(isJson("")).toBe(true)
+		expect(isJson(true)).toBe(true)
+		expect(isJson(false)).toBe(true)
+		expect(isJson(0)).toBe(true)
+		expect(isJson(undefined)).toBe(false)
+		expect(isJson(0n)).toBe(false)
+		expect(isJson([0n])).toBe(false)
+		expect(isJson(() => {})).toBe(false)
+		expect(isJson(new Set())).toBe(false)
+	})
+})
