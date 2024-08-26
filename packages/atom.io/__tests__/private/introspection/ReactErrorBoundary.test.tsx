3dbfdc8fa2c42421787ab32a0bb2b56a4c748c55
Jeremy Banka <hello@jeremybanka.com>
2024-08-26 02:02:39 -0700

    ✅ simplify test for happy dom

--- packages/atom.io/__tests__/private/introspection/ReactErrorBoundary.test.tsx
+++ packages/atom.io/__tests__/private/introspection/ReactErrorBoundary.test.tsx
@@ -4,27 +4,7 @@ import type { FunctionComponent } from "react"
 
 const NOT_A_FUNCTION = true
 // @ts-expect-error (that's the point)
-const throwTypeError = (): never => NOT_A_FUNCTION()
-const ThrowOnRender: FunctionComponent = () => throwTypeError()
-
-let expectedErrors = 0
-let actualErrors = 0
-function onError(e) {
-	e.preventDefault()
-	actualErrors++
-}
-
-beforeEach(() => {
-	expectedErrors = 0
-	actualErrors = 0
-	window.addEventListener(`error`, onError)
-})
-
-afterEach(() => {
-	window.removeEventListener(`error`, onError)
-	expect(actualErrors).toBe(expectedErrors)
-	expectedErrors = 0
-})
+const ThrowOnRender: FunctionComponent = () => NOT_A_FUNCTION()
 
 const scenarios = {
 	componentThrowsOnRender: () => {
@@ -42,7 +22,6 @@ const scenarios = {
 }
 
 it(`renders the text of the thrown error`, () => {
-	expectedErrors = 2
 	const { errorBoundary } = scenarios.componentThrowsOnRender()
 	expect(errorBoundary.textContent).toContain(
 		`⚠️ ThrowOnRender ⚠️ TypeError: NOT_A_FUNCTION is not a function`,
