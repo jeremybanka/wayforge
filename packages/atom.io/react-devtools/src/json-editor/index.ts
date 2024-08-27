a676e1e5eedbad55dadc16e0fbac841dd56d94c9
Jeremy Banka <hello@jeremybanka.com>
2024-08-26 23:48:48 -0700

    ✅ cast-json

--- packages/atom.io/react-devtools/src/json-editor/index.ts
+++ packages/atom.io/react-devtools/src/json-editor/index.ts
@@ -13,6 +13,7 @@ import type { JsonEditorProps_INTERNAL } from "./json-editor-internal"
 
 export * from "./default-components"
 export * from "./developer-interface"
+export * from "./editors-by-type/utilities/cast-to-json"
 
 export type SetterOrUpdater<T> = <New extends T>(
 	next: New | ((old: T) => New),
