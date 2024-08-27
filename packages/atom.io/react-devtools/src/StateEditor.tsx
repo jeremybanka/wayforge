34a2bda16f9693b5d1f277656d9fb6808aacc94c
Jeremy Banka <hello@jeremybanka.com>
2024-08-27 10:19:24 -0700

    🔥 remove dead code

--- packages/atom.io/react-devtools/src/StateEditor.tsx
+++ packages/atom.io/react-devtools/src/StateEditor.tsx
@@ -1,18 +1,8 @@
 import type { ReadonlySelectorToken, WritableToken } from "atom.io"
-import { isJson } from "atom.io/json"
 import { useI, useO } from "atom.io/react"
 import type { FC } from "react"
 
-import { ElasticInput } from "./elastic-input"
 import { JsonEditor } from "./json-editor"
-
-export const fallback = <T,>(fn: () => T, fallbackValue: T): T => {
-	try {
-		return fn()
-	} catch (_) {
-		return fallbackValue
-	}
-}
 
 export const StateEditor: FC<{
 	token: WritableToken<unknown>
