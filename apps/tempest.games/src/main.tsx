34625f539b551d1b5772851cbb5018befe84c3ad
Jeremy Banka <hello@jeremybanka.com>
2024-09-14 00:49:01 -0700

    🎨 lint

--- apps/tempest.games/src/main.tsx
+++ apps/tempest.games/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
+import App from "./App.tsx"
+
+// biome-ignore lint/style/noNonNullAssertion: I believe we will find the root
+createRoot(document.getElementById(`root`)!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
 )
