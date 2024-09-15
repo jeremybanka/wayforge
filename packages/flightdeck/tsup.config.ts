5be93829d7810665137d425508d4f6e5eab225f7
Jeremy Banka <hello@jeremybanka.com>
2024-09-14 22:23:16 -0700

    🚚 adjust scripts, routes, etc

--- packages/flightdeck/tsup.config.ts
+++ packages/flightdeck/tsup.config.ts
@@ -11,7 +11,7 @@ export const OPTIONS = {
 	tsconfig: `tsconfig.json`,
 	dts: { only: true },
 	format: [`esm`],
-	entry: [`src/index.ts`],
+	entry: [`src/lib.ts`],
 	metafile: false,
 	outDir: `dist`,
 } satisfies Options
