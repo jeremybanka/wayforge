c347a94c7538ca2b3b143cf75d47576a3fa3d351
Jeremy Banka <hello@jeremybanka.com>
2024-10-01 01:26:30 -0700

    ♻️ migrate in setup

--- apps/tempest.games/__scripts__/setup.vitest.ts
+++ apps/tempest.games/__scripts__/setup.vitest.ts
@@ -1,5 +1,7 @@
 import { execSync } from "node:child_process"
 import { resolve } from "node:path"
 
-export default (): void =>
-	void execSync(resolve(__dirname, `../__scripts__/setup-db.bun.ts`))
+export default (): void => {
+	execSync(resolve(__dirname, `../__scripts__/setup-db.bun.ts`))
+	execSync(`pnpm drizzle-kit migrate`)
+}
