76f8d3cf2714cd05a97092d62447a452108fd0bf
Jeremy Banka <hello@jeremybanka.com>
2024-10-01 00:35:36 -0700

    🐛 pass process.env along to setup-db.bun.ts

--- apps/tempest.games/__scripts__/setup.vitest.ts
+++ apps/tempest.games/__scripts__/setup.vitest.ts
@@ -2,4 +2,8 @@ import { execSync } from "node:child_process"
 import { resolve } from "node:path"
 
 export default (): void =>
-	void execSync(resolve(__dirname, `../__scripts__/setup-db.bun.ts`))
+	void execSync(resolve(__dirname, `../__scripts__/setup-db.bun.ts`), {
+		env: {
+			...process.env,
+		},
+	})
