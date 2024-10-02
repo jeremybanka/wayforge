de869ba8bda76850a1704d15fc76458931180eb7
Jeremy Banka <hello@jeremybanka.com>
2024-10-01 21:14:36 -0700

    ✨ cookie auth

--- apps/tempest.games/src/backend.bun.ts
+++ apps/tempest.games/src/backend.bun.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env bun
 
+import { createHash } from "node:crypto"
 import * as http from "node:http"
 
 import { AtomIOLogger } from "atom.io"
@@ -11,6 +12,7 @@ import {
 	IMPLICIT,
 	setIntoStore,
 } from "atom.io/internal"
+import type { Json } from "atom.io/json"
 import {
 	ParentSocket,
 	realtimeContinuitySynchronizer,
@@ -19,10 +21,19 @@ import {
 	userIndex,
 	usersOfSockets,
 } from "atom.io/realtime-server"
+import { eq } from "drizzle-orm"
 import * as SocketIO from "socket.io"
+import { z } from "zod"
 
 import { worker } from "./backend.worker"
+import { DatabaseManager } from "./database/tempest-db-manager"
+import { users } from "./database/tempest-db-schema"
+import { asUUID } from "./library/as-uuid"
 import { env } from "./library/env"
+import {
+	RESPONSE_DICTIONARY,
+	responseCodeUnion,
+} from "./library/response-dictionary"
 import { countContinuity } from "./library/store"
 
 const parent = new ParentSocket()
@@ -30,7 +41,86 @@ IMPLICIT.STORE.loggers[0] = new AtomIOLogger(`warn`, undefined, parent.logger)
 
 const gameWorker = worker(parent, `backend.worker.game.bun`)
 
-const httpServer = http.createServer((_, res) => res.end(`Hello World!`))
+const credentialsSchema = z
+	.object({
+		username: z.string(),
+		password: z.string(),
+	})
+	.strict()
+
+const serverIssueSchema = z.tuple([responseCodeUnion, z.string()])
+
+const db = new DatabaseManager()
+
+const userSessionMap = new Map<string, Set<string>>()
+
+const httpServer = http.createServer((req, res) => {
+	const data: Uint8Array[] = []
+	req
+		.on(`data`, (chunk) => data.push(chunk))
+		.on(`end`, async () => {
+			const authHeader = req.headers.authorization
+			try {
+				if (typeof req.url === `undefined`) throw [400, `No URL`]
+				const url = new URL(req.url, env.VITE_BACKEND_ORIGIN)
+				switch (req.method) {
+					case `POST`:
+						switch (url.pathname) {
+							case `/login-${asUUID(`login`)}`: {
+								const text = Buffer.concat(data).toString()
+								const json: Json.Serializable = JSON.parse(text)
+								const parsed = credentialsSchema.safeParse(json)
+								if (parsed.success) {
+									const { username, password } = parsed.data
+									const [maybeUser] = await db.drizzle
+										.select({
+											hash: users.hash,
+											salt: users.salt,
+										})
+										.from(users)
+										.where(eq(users.username, username))
+										.limit(1)
+									if (maybeUser) {
+										const { hash: trueHash, salt } = maybeUser
+										const hash = createHash(`sha256`)
+											.update(password + salt)
+											.digest(`hex`)
+										if (hash === trueHash) {
+											const sessionKey = crypto.randomUUID()
+											let userSessions = userSessionMap.get(username)
+											if (!userSessions) {
+												userSessions = new Set()
+												userSessionMap.set(username, userSessions)
+											}
+											userSessions.add(sessionKey)
+											res.writeHead(204, { "Content-Type": `text/plain` })
+											res.write(
+												`Set-Cookie: username=${username}; HttpOnly; SameSite=Strict; Path=/`,
+											)
+											res.end(
+												`Set-Cookie: sessionKey=${sessionKey}; HttpOnly; SameSite=Strict; Path=/`,
+											)
+										}
+									}
+								}
+							}
+						}
+				}
+			} catch (thrown) {
+				const result = serverIssueSchema.safeParse(thrown)
+				if (result.success) {
+					const [code, message] = result.data
+					const codeMeaning = RESPONSE_DICTIONARY[code]
+					res.writeHead(code, { "Content-Type": `text/plain` })
+					res.end(`${codeMeaning}: ${message}`)
+				} else {
+					parent.logger.error(thrown)
+					res.writeHead(500, { "Content-Type": `text/plain` })
+					res.end(`Internal Server Error`)
+				}
+			}
+		})
+})
 const address = httpServer.listen(env.BACKEND_PORT).address()
 const port =
 	typeof address === `string` ? null : address === null ? null : address.port
@@ -43,8 +133,51 @@ new SocketIO.Server(httpServer, {
 	},
 })
 	.use((socket, next) => {
-		const { token, username } = socket.handshake.auth
-		if (token === `test` && socket.id) {
+		const cookieHeader = socket.handshake.headers.cookie
+		if (!cookieHeader) {
+			next(new Error(`No cookie header provided`))
+			return
+		}
+
+		const cookies = cookieHeader
+			.split(`;`)
+			.map((cookie) => cookie.trim())
+			.reduce(
+				(acc, cookie) => {
+					// Check if the cookie contains an '=' character
+					const separatorIndex = cookie.indexOf(`=`)
+					if (separatorIndex < 0) {
+						// Malformed cookie, log and skip it
+						parent.logger.warn(`Malformed cookie: ${cookie}`)
+						return acc
+					}
+
+					const key = cookie.slice(0, separatorIndex).trim()
+					const value = cookie.slice(separatorIndex + 1).trim()
+
+					// Skip if key is empty
+					if (!key) {
+						parent.logger.warn(`Cookie with empty key: ${cookie}`)
+						return acc
+					}
+
+					// Decode URI components to handle encoded characters
+					const decodedKey = decodeURIComponent(key)
+					const decodedValue = decodeURIComponent(value)
+
+					acc[decodedKey] = decodedValue
+					return acc
+				},
+				{} as Partial<Record<`sessionKey` | `username`, string>>,
+			)
+
+		const { username, sessionKey } = cookies
+		if (!(username && sessionKey)) {
+			next(new Error(`No username or session key provided`))
+			return
+		}
+		const userSessions = userSessionMap.get(username)
+		if (!userSessions?.has(sessionKey)) {
 			const socketState = findInStore(IMPLICIT.STORE, socketAtoms, socket.id)
 			setIntoStore(IMPLICIT.STORE, socketState, socket)
 			editRelationsInStore(
