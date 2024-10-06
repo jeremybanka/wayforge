c6bb03b4c13b1378539bf95357517eb4131aa89e
Jeremy Banka <hello@jeremybanka.com>
2024-10-06 00:46:00 -0700

    🔧 allow console in browser

--- eslint.config.ts
+++ eslint.config.ts
@@ -227,6 +227,7 @@ const configs = [
 			`packages/atom.io/**/src/**/*.ts{,x}`,
 			`apps/tempest.games/src/**/*.ts{,x}`,
 		],
+		ignores: [`apps/tempest.games/src/frontend/**/*.ts{,x}`],
 		rules: {
 			"no-console": ERROR,
 		},
