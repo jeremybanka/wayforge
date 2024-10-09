92231572b68e274743361677bbcdd812803b2f8f
Jeremy Banka <hello@jeremybanka.com>
2024-10-09 00:05:59 -0700

    ✨ stub admin route

---
+++ apps/tempest.games/src/frontend/views/Admin.tsx
@@ -0,0 +1,7 @@
+export function Admin(): JSX.Element {
+	return (
+		<article>
+			<h1>admin</h1>
+		</article>
+	)
+}
--- apps/tempest.games/src/frontend/views/Game.tsx
+++ apps/tempest.games/src/frontend/views/Game.tsx
@@ -3,13 +3,33 @@ import { useO } from "atom.io/react"
 import { useSyncContinuity } from "atom.io/realtime-react"
 
 import { countAtom, countContinuity, incrementTX } from "../../library/store"
+import type { Route } from "../services/router-service"
 
-export function Game(): JSX.Element {
+export type Tail<T extends any[]> = T extends [any, ...infer Rest] ? Rest : never
+
+export type GameRoute = Tail<Extract<Route, [`game`, ...any]>>
+
+export type GameIndexProps = {
+	route: GameRoute
+}
+
+export function GameIndex({ route: [gameId] }: GameIndexProps): JSX.Element {
+	return (
+		<nav>{gameId ? <Game gameId={gameId} /> : <article>no game</article>}</nav>
+	)
+}
+
+export type GameProps = {
+	gameId: string
+}
+
+export function Game({ gameId }: GameProps): JSX.Element {
+	console.log(gameId)
 	const count = useO(countAtom)
 	const increment = runTransaction(incrementTX)
 	useSyncContinuity(countContinuity)
 	return (
-		<div className="card">
+		<article>
 			<button
 				type="button"
 				onClick={() => {
@@ -19,6 +39,6 @@ export function Game(): JSX.Element {
 				count is {count}
 			</button>
 			<p>Let's see how high we can count!</p>
-		</div>
+		</article>
 	)
 }
