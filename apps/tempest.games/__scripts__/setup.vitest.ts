49c7a3f4804530c59690a878e0cab566c5c5fb47
Jeremy Banka <hello@jeremybanka.com>
2024-10-09 18:37:10 -0700

    🛠️ use new db:up script

--- apps/tempest.games/__scripts__/setup.vitest.ts
+++ apps/tempest.games/__scripts__/setup.vitest.ts
@@ -1,7 +1,5 @@
 import { execSync } from "node:child_process"
-import { resolve } from "node:path"
 
 export default (): void => {
-	execSync(resolve(__dirname, `../__scripts__/setup-db.bun.ts`))
-	execSync(`pnpm drizzle-kit migrate`)
+	execSync(`pnpm db:up`)
 }
