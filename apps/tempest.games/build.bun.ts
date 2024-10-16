b482cd4212690993e45db26285228ebddd896c26
Jeremy Banka <hello@jeremybanka.com>
2024-10-16 01:56:29 -0700

    ♻️ simplify build scripts

---
+++ apps/tempest.games/build.bun.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env bun
+
+import { build } from "bun"
+
+await build({
+	entrypoints: [
+		`./src/frontend.bun.ts`,
+		`./src/backend.bun.ts`,
+		`./src/backend.worker.game.bun.ts`,
+	],
+	outdir: `./bin`,
+	packages: `bundle`,
+	minify: true,
+	target: `bun`,
+})
+await build({
+	entrypoints: [`./__scripts__/setup-db.bun.ts`],
+	outdir: `./bin`,
+	minify: false,
+	target: `bun`,
+})
