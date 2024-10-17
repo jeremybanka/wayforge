ef3286bf132ac7cd09e12ea6b608ed78f34ac888
Jeremy Banka <hello@jeremybanka.com>
2024-10-17 00:58:39 -0700

    tempest frontend env vars (#2782)

---
+++ .changeset/chilly-laws-trade.md
@@ -0,0 +1,5 @@
+---
+"tempest.games": patch
+---
+
+🐛 Fix missing environment variables on frontend.
--- apps/tempest.games/vite.config.ts
+++ apps/tempest.games/vite.config.ts
@@ -1,22 +1,24 @@
 import react from "@vitejs/plugin-react-swc"
-import { configDotenv } from "dotenv"
+import { loadEnv } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	css: {
-		preprocessorOptions: { scss: { api: `modern-compiler` } },
-	},
-	server: {
-		port: 3333,
-	},
-	test: {
-		globals: true,
-		globalSetup: `./__scripts__/setup.vitest.ts`,
-		env: configDotenv().parsed ?? {},
-		include: [`src/**/*.test.ts`],
-	},
-	build: { outDir: `app` },
+export default defineConfig(async ({ mode }) => {
+	const { dirname } = import.meta
+	const env = loadEnv(mode, dirname, mode === `test` ? `` : undefined)
+	Object.assign(import.meta, { env })
+	await import(`./src/library/env`)
+	return {
+		plugins: [react(), tsconfigPaths()],
+		build: { outDir: `app` },
+		css: { preprocessorOptions: { scss: { api: `modern-compiler` } } },
+		server: { port: 3333 },
+		test: {
+			env,
+			globals: true,
+			globalSetup: `./__scripts__/setup.vitest.ts`,
+			include: [`src/**/*.test.ts`],
+		},
+	}
 })
