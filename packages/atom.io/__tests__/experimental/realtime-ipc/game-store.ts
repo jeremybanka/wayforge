b0f9b5b16a07c6d936b5165a0b2ee1683a99c9ac
Jeremy Banka <hello@jeremybanka.com>
2024-10-29 15:39:22 -0700

    💬 renaming

--- packages/atom.io/__tests__/experimental/realtime-ipc/game-store.ts
+++ packages/atom.io/__tests__/experimental/realtime-ipc/game-store.ts
@@ -2,17 +2,19 @@ import type { RegularAtomToken } from "atom.io"
 import { atomFamily } from "atom.io"
 import { findState } from "atom.io/ephemeral"
 import { continuity } from "atom.io/realtime"
+import type { UserKey } from "atom.io/realtime-server"
 
 export const letterAtoms = atomFamily<string | null, number>({
 	key: `letter`,
 	default: null,
 })
-export const letterIndex = atomFamily<RegularAtomToken<string | null>[], string>(
-	{
-		key: `letterIndex`,
-		default: Array.from({ length: 5 }).map((_, i) => findState(letterAtoms, i)),
-	},
-)
+export const letterIndex = atomFamily<
+	RegularAtomToken<string | null>[],
+	UserKey
+>({
+	key: `letterIndex`,
+	default: Array.from({ length: 5 }).map((_, i) => findState(letterAtoms, i)),
+})
 
 export const gameContinuity = continuity({
 	key: `game`,
