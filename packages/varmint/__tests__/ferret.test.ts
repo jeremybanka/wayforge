6e768e9809c49f042e954d62644d12f0d8595475
Jeremy Banka <hello@jeremybanka.com>
2025-01-30 18:10:38 -0800

    debug varmint cache miss (#3373)

--- packages/varmint/__tests__/ferret.test.ts
+++ packages/varmint/__tests__/ferret.test.ts
@@ -71,3 +71,51 @@ test(`ferret with openAI`, async () => {
 	}
 	expect(chunks.length).toBe(33)
 })
+
+test(`ferret with openAI (cache-miss)`, async () => {
+	let openAI: OpenAI | undefined
+	function getStreamFromOpenAi(
+		body: Omit<OpenAIResources.ChatCompletionCreateParamsStreaming, `stream`>,
+		options?: OpenAICore.RequestOptions,
+	): Promise<AsyncIterable<ChatCompletionChunk>> {
+		if (!openAI) {
+			openAI = new OpenAI({
+				apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+				dangerouslyAllowBrowser: process.env.NODE_ENV === `test`,
+			})
+		}
+
+		const createParams: OpenAIResources.ChatCompletionCreateParamsStreaming = {
+			...body,
+			stream: true,
+		}
+		console.log(`createParams`, createParams)
+		const stream = openAI.chat.completions.create(createParams, options)
+		return stream
+	}
+
+	const myFerret = new Ferret(`read`)
+
+	let caught: Error | undefined
+	try {
+		await myFerret
+			.add(`myAiService`, getStreamFromOpenAi)
+			.for(`miss`)
+			.get({
+				model: `gpt-4-turbo`,
+				messages: [
+					{ role: `user`, content: `Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!` },
+				],
+			})
+	} catch (thrown) {
+		if (thrown instanceof Error) {
+			caught = thrown
+		}
+	} finally {
+		expect(caught).toBeInstanceOf(Error)
+		expect(caught?.message).toContain(`Hello, how are you?`)
+		expect(caught?.message).toContain(`Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
+		expect(caught?.message).toContain(`YOUR INPUT DATA:`)
+		expect(caught?.message).toContain(`CACHED INPUT FILES:`)
+	}
+})
