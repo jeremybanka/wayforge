26d4756f312eec95ac3e807e44ff9f730f56f783
Jeremy Banka <hello@jeremybanka.com>
2025-02-03 13:15:08 -0800

    ✨ implement global tracking control as static methods on Squirrel

--- packages/varmint/src/squirrel.ts
+++ packages/varmint/src/squirrel.ts
@@ -186,4 +186,118 @@ export class Squirrel {
 			}
 		}
 	}
+
+	public static startGlobalTracking(): void {
+		if (storage.initialized) {
+			console.error(
+				`💥 called startGlobalTracking, but the global cache was already initialized`,
+			)
+			return
+		}
+		storage.initialize()
+	}
+	public static flushGlobal(): void {
+		if (!storage.initialized) {
+			console.error(
+				`💥 called flushGlobal, but the global cache wasn't initialized with startGlobalTracking`,
+			)
+			return
+		}
+		const dirContents = fs.readdirSync(storage.rootDir)
+		const realRoots = new Map<string, string>()
+		const roots: `root__${string}`[] = []
+		const lists: `list__${string}`[] = []
+		const files: `file__${string}`[] = []
+		for (const dirContent of dirContents) {
+			if (startsWith(`root__`, dirContent)) {
+				roots.push(dirContent)
+			} else if (startsWith(`list__`, dirContent)) {
+				lists.push(dirContent)
+			} else if (startsWith(`file__`, dirContent)) {
+				files.push(dirContent)
+			}
+		}
+		const tree: Map<string, Map<string, Set<string>>> = new Map()
+		for (const root of roots) {
+			const rootName = root.replace(`root__`, ``)
+			tree.set(rootName, new Map())
+			const rootPath = storage.getItem(root)
+			if (rootPath) {
+				realRoots.set(rootName, rootPath)
+			} else {
+				console.error(
+					`💥 Could not find folder ${rootPath} referenced in the global cache`,
+				)
+			}
+		}
+		for (const list of lists) {
+			const listPath = list.replace(`list__`, ``)
+			const [listRootName, listName] = listPath.split(`__`)
+			const listRoot = tree.get(listRootName)
+			if (listRoot) {
+				listRoot.set(listName, new Set())
+			} else {
+				console.error(
+					`💥 Could not find root ${listRootName} for list ${listName}`,
+				)
+			}
+		}
+		for (const file of files) {
+			const filePath = file.replace(`file__`, ``)
+			const [listRootName, listName, subKey] = filePath.split(`__`)
+			const listRoot = tree.get(listRootName)
+			if (listRoot) {
+				const list = listRoot.get(listName)
+				if (list) {
+					list.add(subKey)
+				} else {
+					console.error(
+						`💥 Could not find list ${listName} for file ${filePath}`,
+					)
+				}
+			} else {
+				console.error(
+					`💥 Could not find root ${listRootName} for file ${filePath}`,
+				)
+			}
+		}
+		for (const [rootName, rootMap] of tree.entries()) {
+			const realRoot = realRoots.get(rootName)
+			if (!realRoot) {
+				console.error(`💥 Could not find root ${rootName}`)
+				continue
+			}
+			const realRootContents = fs.readdirSync(realRoot)
+			for (const rootContent of realRootContents) {
+				if (!rootMap.has(rootContent)) {
+					const pathForRemoval = path.join(realRoot, rootContent)
+					console.log(`🧹 globalFlush: removing directory ${pathForRemoval}`)
+					fs.rmSync(pathForRemoval, { recursive: true })
+				}
+			}
+			for (const [listName, list] of rootMap.entries()) {
+				const realList = path.join(realRoot, listName)
+				const realListContents = fs.readdirSync(realList)
+				for (const realListContent of realListContents) {
+					const contentTrimmed = realListContent
+						.replace(`.input.json`, ``)
+						.replace(`.output.json`, ``)
+						.replace(`stream.txt`, ``)
+					if (!list.has(contentTrimmed)) {
+						const pathForRemoval = path.join(realList, realListContent)
+						console.log(`🧹 globalFlush: removing file ${pathForRemoval}`)
+						fs.rmSync(pathForRemoval)
+					}
+				}
+			}
+		}
+		fs.rmSync(storage.rootDir, { recursive: true })
+	}
+}
+
+function startsWith<T extends string>(
+	prefix: T,
+	str: string,
+): str is `${T}${string}` {
+	return str.startsWith(prefix)
 }
