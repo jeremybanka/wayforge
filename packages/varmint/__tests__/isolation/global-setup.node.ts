1a0f1aec13ceaeb05d8f854167f312b3cd1f1fa3
Jeremy Banka <hello@jeremybanka.com>
2025-02-03 16:37:37 -0800

    🦋

--- packages/varmint/__tests__/isolation/global-setup.node.ts
+++ packages/varmint/__tests__/isolation/global-setup.node.ts
@@ -1,5 +1,3 @@
-import * as fs from "node:fs"
-
 import { Squirrel } from "../../src/squirrel.ts"
 import { varmintWorkspaceManager } from "../../src/varmint-workspace-manager.ts"
 
@@ -14,15 +12,8 @@ function asyncRand() {
 }
 
 varmintWorkspaceManager.startGlobalTracking()
-console.log(varmintWorkspaceManager.storage)
-console.log(`tempdirname:`, tempdirName)
 
-console.log(`tempdir contents:`, fs.readdirSync(tempdirName))
 const squirrel = new Squirrel(`read-write`, tempdirName)
-console.log(`tempdir contents:`, fs.readdirSync(tempdirName))
 const rand = squirrel.add(`rand`, asyncRand)
-console.log(`tempdir contents:`, fs.readdirSync(tempdirName))
 const myRand = rand.for(`my-rand`)
-console.log(`tempdir contents:`, fs.readdirSync(tempdirName))
 await myRand.get()
-console.log(`tempdir contents:`, fs.readdirSync(tempdirName))
