ac6cc65d8feb85b809fc644d551f4baa03ead9b6
Jeremy Banka <hello@jeremybanka.com>
2025-02-06 14:34:52 -0800

    ✨ sanitize filename now accepts a max length (default 64)

--- packages/varmint/src/sanitize-filename.ts
+++ packages/varmint/src/sanitize-filename.ts
@@ -2,14 +2,14 @@ import { createHash } from "node:crypto"
 
 export const filenameAllowList = /[^a-zA-Z0-9\-._]/g
 
-export function sanitizeFilename(filename: string): string {
+export function sanitizeFilename(filename: string, maxLen = 64): string {
 	const onlyValidChars = filename.replace(filenameAllowList, `-`)
 
-	if (onlyValidChars.length <= 64) {
+	if (onlyValidChars.length <= maxLen) {
 		return onlyValidChars
 	}
 
 	const hash = createHash(`sha256`).update(filename).digest(`hex`)
 	// Otherwise, trim the beginning to fit within the max length
-	return onlyValidChars.slice(-64) + `+` + hash // Keep the last maxLen characters
+	return onlyValidChars.slice(-maxLen) + `+` + hash // Keep the last maxLen characters
 }
