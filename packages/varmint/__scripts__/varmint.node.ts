46d1db3d8839b763c02142933ec9349315ad3df9
Jeremy Banka <hello@jeremybanka.com>
2025-02-07 11:57:05 -0800

    ❓ log input

--- packages/varmint/__scripts__/varmint.node.ts
+++ packages/varmint/__scripts__/varmint.node.ts
@@ -4,29 +4,32 @@ import { cli, required } from "comline"
 import { varmintWorkspaceManager } from "varmint"
 import { z } from "zod"
 
-const parse = cli({
-	cliName: `varmint`,
-	routes: required({
-		track: null,
-		clean: null,
-	}),
-	routeOptions: {
-		track: { options: {}, optionsSchema: z.record(z.never()) },
-		clean: {
-			options: {
-				"ci-flag": {
-					flag: `c`,
-					required: false,
-					description: `Flag that, if present, indicates that the command is running in a CI environment`,
-					example: `--ci-flag=CI`,
+const parse = cli(
+	{
+		cliName: `varmint`,
+		routes: required({
+			track: null,
+			clean: null,
+		}),
+		routeOptions: {
+			track: { options: {}, optionsSchema: z.record(z.never()) },
+			clean: {
+				options: {
+					"ci-flag": {
+						flag: `c`,
+						required: false,
+						description: `Flag that, if present, indicates that the command is running in a CI environment`,
+						example: `--ci-flag=CI`,
+					},
 				},
+				optionsSchema: z.object({
+					"ci-flag": z.string().optional(),
+				}),
 			},
-			optionsSchema: z.object({
-				"ci-flag": z.string().optional(),
-			}),
 		},
 	},
-})
+	console,
+)
 
 const { inputs } = parse(process.argv)
 
