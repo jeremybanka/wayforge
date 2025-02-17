aa71268610face6f58506558169e3bc828815af1
Jeremy Banka <hello@jeremybanka.com>
2025-02-16 16:19:07 -0800

    Version Packages (#3489)
    * Version Packages
    
    * 🔧 organize tsup configs
    
    * ♻️ simplify lint
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/break-check/tsup.config.ts
+++ packages/break-check/tsup.config.ts
@@ -2,15 +2,15 @@ import type { Options } from "tsup"
 import { defineConfig } from "tsup"
 
 export const OPTIONS = {
-	sourcemap: true,
-	treeshake: true,
 	clean: true,
-	tsconfig: `tsconfig.json`,
 	dts: true,
-	format: [`esm`],
 	entry: [`src/break-check.ts`, `src/break-check.x.ts`],
+	format: [`esm`],
 	metafile: false,
 	outDir: `dist`,
+	sourcemap: true,
+	treeshake: true,
+	tsconfig: `tsconfig.json`,
 } satisfies Options
 
 export default defineConfig(OPTIONS)
