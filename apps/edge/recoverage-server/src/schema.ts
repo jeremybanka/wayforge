07e6e364f8f8df8ea0d98959226b9bd745fd8a01
Jeremy Banka <hello@jeremybanka.com>
2025-03-01 13:00:33 -0800

    🗃️ add projects and pave migrations

--- apps/edge/recoverage-server/src/schema.ts
+++ apps/edge/recoverage-server/src/schema.ts
@@ -2,30 +2,39 @@ import { sql } from "drizzle-orm"
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const users = sqliteTable(`users`, {
-	id: integer(`id`).primaryKey(),
-	createdAt: integer(`created_at`, { mode: `timestamp` })
+	id: integer().primaryKey(),
+	githubId: integer().unique(),
+	createdAt: integer({ mode: `timestamp` })
 		.default(sql`CURRENT_TIMESTAMP`)
 		.notNull(),
 })
 
 export const tokens = sqliteTable(`tokens`, {
-	selector: text(`selector`).primaryKey(),
-	userId: integer(`user_id`)
+	selector: text().primaryKey(),
+	userId: integer()
 		.references(() => users.id)
 		.notNull(),
-	verifierHash: text(`verifier_hash`).notNull(), // Hashed portion of the token for security
-	createdAt: integer(`created_at`, { mode: `timestamp` })
+	verifierHash: text().notNull(), // Hashed portion of the token for security
+	createdAt: integer({ mode: `timestamp` })
 		.default(sql`CURRENT_TIMESTAMP`)
 		.notNull(),
 })
 
-export const reports = sqliteTable(`reports`, {
-	id: text(`id`).primaryKey(),
-	userId: integer(`user_id`)
+export const projects = sqliteTable(`projects`, {
+	id: text().primaryKey(),
+	userId: integer()
 		.references(() => users.id)
 		.notNull(),
-	data: text(`data`).notNull(),
-	createdAt: integer(`created_at`, { mode: `timestamp` })
+	name: text().notNull(),
+})
+
+export const reports = sqliteTable(`reports`, {
+	id: text().primaryKey(),
+	projectId: text()
+		.references(() => projects.id)
+		.notNull(),
+	data: text().notNull(),
+	createdAt: integer({ mode: `timestamp` })
 		.default(sql`CURRENT_TIMESTAMP`)
 		.notNull(),
 })
