1149691aac02dd73ad7f9205e76d11a9f6ba6d9f
Jeremy Banka <hello@jeremybanka.com>
2025-04-10 18:00:38 -0700

    🔥 remove old app, 'strand'

--- apps/web/strand/src/services/auth.ts
+++
@@ -1,8 +0,0 @@
-import * as AtomIO from "atom.io"
-import { persistSync } from "atom.io/web"
-
-export const secretState = AtomIO.atom<string>({
-	key: `secret`,
-	default: ``,
-	effects: [persistSync(window.localStorage, JSON, `secret`)],
-})
