731c5afc55b6d6d72736f7d7e9a036cbc0110fc6
Jeremy Banka <hello@jeremybanka.com>
2025-04-22 13:49:18 -0700

    🔧 replace config (safedeposit)

--- packages/safedeposit/tsdown.config.ts
+++ packages/safedeposit/tsdown.config.ts
@@ -1,12 +1,11 @@
-import type { Options } from "tsup"
-import { defineConfig } from "tsup"
+import type { Options } from "tsdown"
+import { defineConfig } from "tsdown"
 
 export const OPTIONS = {
 	clean: true,
 	dts: true,
 	entry: [`src/safedeposit.ts`],
 	format: [`esm`],
-	metafile: false,
 	outDir: `dist`,
 	platform: `node`,
 	sourcemap: true,
