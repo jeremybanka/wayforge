71ab2c2bd5ddbdea2df6bf48ac8a306f9d480b46
Jeremy Banka <hello@jeremybanka.com>
2025-04-25 14:52:28 -0700

    🔧 smaller hash, use beginning and end

--- packages/varmint/src/sanitize-filename.ts
+++ packages/varmint/src/sanitize-filename.ts
@@ -3,13 +3,26 @@ import { createHash } from "node:crypto"
 export const filenameAllowList = /[^a-zA-Z0-9\-._]/g
 
 export function sanitizeFilename(filename: string, maxLen = 64): string {
+	if (maxLen % 2 === 1) {
+		maxLen += 1
+	}
 	const onlyValidChars = filename.replace(filenameAllowList, `-`)
 
 	if (onlyValidChars.length <= maxLen) {
 		return onlyValidChars
 	}
 
-	const hash = createHash(`sha256`).update(filename).digest(`hex`)
+	const hash = createHash(`sha256`)
+		.update(filename)
+		.digest(`base64`)
+		.slice(0, 8)
+		.replace(`/`, `_`)
 	// Otherwise, trim the beginning to fit within the max length
-	return onlyValidChars.slice(-maxLen) + `+` + hash // Keep the last maxLen characters
+	let baseName = onlyValidChars
+	if (baseName.length > maxLen) {
+		const beginning = baseName.slice(0, maxLen / 2)
+		const end = baseName.slice(-maxLen / 2)
+		baseName = beginning + `_` + end
+	}
+	return baseName + `+` + hash // Keep the last maxLen characters
 }
