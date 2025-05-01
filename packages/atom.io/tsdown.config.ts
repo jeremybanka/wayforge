73e9beb22daa9b3a1661d5e23a75c68316964d04
Jeremy Banka <hello@jeremybanka.com>
2025-05-01 16:01:52 -0700

    🔧 reduce tsdown config

--- packages/atom.io/tsdown.config.ts
+++ packages/atom.io/tsdown.config.ts
@@ -6,46 +6,34 @@ import { fromEntries } from "./json/src/entries.ts"
 
 const SUBMODULE_NAMES = discoverSubmodules()
 
-const SUBMODULE_NAMES_PREFIXED = [
+const EXTERNAL = [
+	/^eslint\-/,
+	/^@eslint\-/,
+	/^@typescript\-eslint\//,
 	`atom.io`,
 	...SUBMODULE_NAMES.map((submodule) => `atom.io/${submodule}`),
 ]
 
-export const BASE_OPTIONS: Options = {
-	external: SUBMODULE_NAMES_PREFIXED,
-	format: [`esm`],
-	sourcemap: false,
-	treeshake: true,
-	tsconfig: `tsconfig.json`,
-}
-
 const otherEntries = fromEntries(
 	SUBMODULE_NAMES.map(
-		(name) =>
-			[name === `react` ? `TEST` : name, `${name}/src/index.ts`] as const,
+		(name) => [`${name}/index`, `${name}/src/index.ts`] as const,
 	),
 )
 
 console.log({ SUBMODULE_NAMES, otherEntries })
 
-export const JS_OPTIONS: Options = {
-	...BASE_OPTIONS,
+const config: UserConfig = defineConfig({
+	external: EXTERNAL,
+	dts: { sourcemap: true },
+	format: [`esm`],
+	sourcemap: false,
+	treeshake: true,
+	tsconfig: `tsconfig.json`,
 	clean: true,
 	outDir: `dist`,
 	entry: {
-		atom: `src/index.ts`,
+		"main/index": `src/index.ts`,
 		...otherEntries,
 	},
-}
-
-// export const DTS_OPTIONS: Options = {
-// 	...BASE_OPTIONS,
-// 	dts: { only: true },
-// 	format: [`esm`],
-// 	entry: [`src/index.ts`],
-// 	metafile: false,
-// 	outDir: `dist`,
-// }
-
-const config: UserConfig = defineConfig(JS_OPTIONS)
+})
 export default config
