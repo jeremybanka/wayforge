d565d32bbb02f73d6dd9f711b14e94e440c5afa7
Jeremy Banka <hello@jeremybanka.com>
2025-05-02 09:38:40 -0700

    🚀 optimize safedeposit build

--- packages/safedeposit/vitest.config.ts
+++ packages/safedeposit/vitest.config.ts
@@ -1,7 +1,10 @@
+import type { UserConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
-export default defineConfig({
+const config: UserConfig = defineConfig({
 	plugins: [tsconfigPaths()],
 	test: { globals: true },
 })
+
+export default config
