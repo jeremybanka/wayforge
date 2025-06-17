1a1d5d1baf8367ca40abf44baad6a25eff1b4100
Jeremy Banka <hello@jeremybanka.com>
2025-06-17 12:03:53 -0700

    🔥 remove derelict project

--- apps/web/smithy/src/components/Card.tsx
+++
@@ -1,7 +0,0 @@
-import corners, { chamfer } from "corners"
-
-export const CARD_WIDTH = 252
-export const CARD_HEIGHT = 360
-export const CARD_PADDING = 12
-
-export const rightSlant = corners(null, chamfer)
