bc267b14640d33b0d14021dfdf3ee3eeaec6c048
Jeremy Banka <hello@jeremybanka.com>
2025-06-23 13:09:47 -0700

    🚧 REVERT: load devtools from source for testing

--- apps/atom.io.fyi/src/components/DevToolsDynamic.ts
+++ apps/atom.io.fyi/src/components/DevToolsDynamic.ts
@@ -1,5 +1,6 @@
 "use client"
 
-import "atom.io/react-devtools/css"
+// import "atom.io/react-devtools/css"
+import "../../../../packages/atom.io/src/react-devtools/devtools.css"
 
 export { AtomIODevtools } from "atom.io/react-devtools"
