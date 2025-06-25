d32b5f09377b11512af0b9d5dc815da9865ce9fb
Jeremy Banka <hello@jeremybanka.com>
2025-06-25 10:39:46 -0700

    🔧 restore production import path

--- apps/atom.io.fyi/src/components/DevToolsDynamic.ts
+++ apps/atom.io.fyi/src/components/DevToolsDynamic.ts
@@ -1,6 +1,5 @@
 "use client"
 
-// import "atom.io/react-devtools/css"
-import "../../../../packages/atom.io/src/react-devtools/devtools.css"
+import "atom.io/react-devtools/css"
 
 export { AtomIODevtools } from "atom.io/react-devtools"
