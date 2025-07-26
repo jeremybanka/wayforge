3f9f593c0d92dc7213a175d98cc1f047e3d84bba
Jeremy Banka <hello@jeremybanka.com>
2025-07-26 02:41:41 -0700

    ✨ useLoadable

--- packages/atom.io/src/react/index.ts
+++ packages/atom.io/src/react/index.ts
@@ -1,5 +1,6 @@
 export * from "./store-context"
 export * from "./use-i"
 export * from "./use-json"
+export * from "./use-loadable"
 export * from "./use-o"
 export * from "./use-tl"
---
+++ packages/atom.io/src/react/use-loadable.ts
@@ -0,0 +1,79 @@
+/** biome-ignore-all lint/correctness/useHookAtTopLevel: params are used in an invariant way */
+import type { Loadable, ReadableFamilyToken, ReadableToken } from "atom.io"
+import type { Canonical } from "atom.io/json"
+import { useO } from "atom.io/react"
+import React from "react"
+
+export function useLoadable<T>(
+	token: ReadableToken<Loadable<T>>,
+): `LOADING` | { loading: boolean; value: T }
+
+export function useLoadable<T, K extends Canonical>(
+	token: ReadableFamilyToken<Loadable<T>, K>,
+	key: K,
+): `LOADING` | { loading: boolean; value: T }
+
+export function useLoadable<T, F extends T>(
+	token: ReadableToken<Loadable<T>>,
+	fallback: F,
+): { loading: boolean; value: T }
+
+export function useLoadable<T, K extends Canonical, F extends T>(
+	token: ReadableFamilyToken<Loadable<T>, K>,
+	key: K,
+	fallback: F,
+): { loading: boolean; value: T }
+
+export function useLoadable(
+	...params:
+		| readonly [ReadableFamilyToken<any, Canonical>, Canonical, unknown]
+		| readonly [ReadableFamilyToken<any, Canonical>, Canonical]
+		| readonly [ReadableToken<any>, unknown]
+		| readonly [ReadableToken<any>]
+): `LOADING` | { loading: boolean; value: unknown } {
+	let loadable: ReadableToken<any>
+	let fallback: unknown
+
+	const [token] = params
+	let key: Canonical
+	switch (token.type) {
+		case `atom`:
+		case `mutable_atom`:
+		case `readonly_held_selector`:
+		case `readonly_pure_selector`:
+		case `writable_held_selector`:
+		case `writable_pure_selector`:
+			loadable = useO(token)
+			fallback = params[1]
+			break
+		case `atom_family`:
+		case `mutable_atom_family`:
+		case `readonly_held_selector_family`:
+		case `readonly_pure_selector_family`:
+		case `writable_held_selector_family`:
+		case `writable_pure_selector_family`:
+			key = params[1] as Canonical
+			loadable = useO(token, key)
+			fallback = params[2]
+	}
+
+	const lastLoadedRef = React.useRef(
+		fallback ?? (loadable instanceof Promise ? `LOADING` : loadable),
+	)
+	const { current: lastLoaded } = lastLoadedRef
+	if (loadable instanceof Promise) {
+		if (lastLoaded === `LOADING`) {
+			return `LOADING`
+		}
+		return {
+			loading: true,
+			value: lastLoaded,
+		}
+	}
+
+	lastLoadedRef.current = loadable
+	return {
+		loading: false,
+		value: loadable,
+	}
+}
