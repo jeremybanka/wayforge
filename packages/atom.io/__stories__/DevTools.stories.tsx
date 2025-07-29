19061d3fc6b0c24d25c1036a47244d6c9f1de77f
Jeremy Banka <hello@jeremybanka.com>
2025-07-28 17:16:12 -0700

    🔧 updat stories

--- packages/atom.io/__stories__/DevTools.stories.tsx
+++ packages/atom.io/__stories__/DevTools.stories.tsx
@@ -12,7 +12,6 @@ import {
 } from "atom.io"
 import { IMPLICIT } from "atom.io/internal"
 import { AtomIODevtools } from "atom.io/react-devtools"
-import { fn } from "storybook/test"
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof AtomIODevtools> = {
@@ -38,12 +37,6 @@ const meta: Meta<typeof AtomIODevtools> = {
 	},
 	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
 	tags: ["autodocs"],
-	// More on argTypes: https://storybook.js.org/docs/api/argtypes
-	argTypes: {
-		backgroundColor: { control: "color" },
-	},
-	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-	args: { onClick: fn() },
 }
 
 export default meta
@@ -152,7 +145,5 @@ const myTX = transaction<(param: object) => object>({
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-	args: {
-		backgroundColor: "#ae5757",
-	},
+	args: {},
 }
