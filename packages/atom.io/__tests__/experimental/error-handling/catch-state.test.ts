ff07b14c1fa09ca0d457a3fafbe942e7451d40f1
Jeremy Banka <hello@jeremybanka.com>
2025-08-28 14:23:46 -0700

    ✅ expand test plan

--- packages/atom.io/__tests__/experimental/error-handling/catch-state.test.ts
+++ packages/atom.io/__tests__/experimental/error-handling/catch-state.test.ts
@@ -15,8 +15,61 @@ beforeEach(() => {
 	vitest.spyOn(Utils, `stdout`)
 })
 
-describe(`async atom`, async () => {
-	it(`handles a rejected promise`, async () => {
+describe(`immediate states that throw`, () => {
+	it(`atom: catches a rejected promise`, () => {
+		class ClientError extends Error {}
+
+		const retrieveState = (): number => {
+			throw new ClientError(`😤`)
+		}
+
+		const count = AtomIO.atom<number, ClientError>({
+			key: `count`,
+			default: retrieveState,
+			catch: [ClientError],
+		})
+
+		const err = AtomIO.getState(count)
+
+		expect(err).toBeInstanceOf(ClientError)
+	})
+	it(`selector: catches a rejected promise`, () => {
+		class ClientError extends Error {}
+
+		const retrieveState = (): number => {
+			throw new ClientError(`😤`)
+		}
+
+		const count = AtomIO.atom<number, ClientError>({
+			key: `count`,
+			default: retrieveState,
+			catch: [ClientError],
+		})
+
+		const err = AtomIO.getState(count)
+
+		expect(err).toBeInstanceOf(ClientError)
+	})
+})
+describe(`loadable states that throw`, async () => {
+	it(`loadable atom: catches a rejected promise`, async () => {
+		class ClientError extends Error {}
+
+		const retrieveState = async (): Promise<number> => {
+			throw new ClientError(`😤`)
+		}
+
+		const count = AtomIO.atom<Loadable<number>, ClientError>({
+			key: `count`,
+			default: retrieveState,
+			catch: [ClientError],
+		})
+
+		const err = await AtomIO.getState(count)
+
+		expect(err).toBeInstanceOf(ClientError)
+	})
+	it(`loadable selector: catches a rejected promise`, async () => {
 		class ClientError extends Error {}
 
 		const retrieveState = async (): Promise<number> => {
