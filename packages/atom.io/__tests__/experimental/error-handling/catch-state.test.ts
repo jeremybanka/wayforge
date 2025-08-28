5fba939bb8d9bad36647521c271154bf676a424c
Jeremy Banka <hello@jeremybanka.com>
2025-08-28 15:06:51 -0700

    ✅ finish coverage for catching states

--- packages/atom.io/__tests__/experimental/error-handling/catch-state.test.ts
+++ packages/atom.io/__tests__/experimental/error-handling/catch-state.test.ts
@@ -16,74 +16,300 @@ beforeEach(() => {
 })
 
 describe(`immediate states that throw`, () => {
-	it(`atom: catches a rejected promise`, () => {
-		class ClientError extends Error {}
+	describe(`atom`, () => {
+		it(`(happy) catches a thrown error`, () => {
+			class ClientError extends Error {}
 
-		const retrieveState = (): number => {
-			throw new ClientError(`😤`)
-		}
+			const retrieveState = (): number => {
+				throw new ClientError(`😤`)
+			}
 
-		const count = AtomIO.atom<number, ClientError>({
-			key: `count`,
-			default: retrieveState,
-			catch: [ClientError],
+			const count = AtomIO.atom<number, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
+
+			const err = AtomIO.getState(count)
+
+			expect(err).toBeInstanceOf(ClientError)
 		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, () => {
+			class ClientError extends Error {}
 
-		const err = AtomIO.getState(count)
+			const retrieveState = (): number => {
+				throw new Error(`😤`) // not a ClientError
+			}
 
-		expect(err).toBeInstanceOf(ClientError)
+			const count = AtomIO.atom<number, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
+
+			expect(() => AtomIO.getState(count)).toThrowError(Error)
+		})
 	})
-	it(`selector: catches a rejected promise`, () => {
-		class ClientError extends Error {}
+	describe(`atom family`, () => {
+		it(`(happy) catches a thrown error`, () => {
+			class ClientError extends Error {}
 
-		const retrieveState = (): number => {
-			throw new ClientError(`😤`)
-		}
+			const retrieveState = (): number => {
+				throw new ClientError(`😤`)
+			}
 
-		const count = AtomIO.atom<number, ClientError>({
-			key: `count`,
-			default: retrieveState,
-			catch: [ClientError],
+			const count = AtomIO.atomFamily<number, string, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
+
+			const err = AtomIO.getState(count, `example`)
+
+			expect(err).toBeInstanceOf(ClientError)
 		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, () => {
+			class ClientError extends Error {}
 
-		const err = AtomIO.getState(count)
+			const retrieveState = (): number => {
+				throw new Error(`😤`) // not a ClientError
+			}
 
-		expect(err).toBeInstanceOf(ClientError)
+			const count = AtomIO.atomFamily<number, string, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
+
+			expect(() => AtomIO.getState(count, `example`)).toThrowError(Error)
+		})
+	})
+	describe(`selector`, () => {
+		it(`(happy) catches a thrown error`, () => {
+			class ClientError extends Error {}
+
+			const retrieveState = (): number => {
+				throw new ClientError(`😤`)
+			}
+
+			const count = AtomIO.selector<number, ClientError>({
+				key: `count`,
+				get: retrieveState,
+				catch: [ClientError],
+			})
+
+			const err = AtomIO.getState(count)
+
+			expect(err).toBeInstanceOf(ClientError)
+		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, () => {
+			class ClientError extends Error {}
+
+			const retrieveState = (): number => {
+				throw new Error(`😤`) // not a ClientError
+			}
+
+			const count = AtomIO.selector<number, ClientError>({
+				key: `count`,
+				get: retrieveState,
+				catch: [ClientError],
+			})
+
+			expect(() => AtomIO.getState(count)).toThrowError(Error)
+		})
+	})
+	describe(`selector family`, () => {
+		it(`(happy) catches a thrown error`, () => {
+			class ClientError extends Error {}
+
+			const retrieveState = (): number => {
+				throw new ClientError(`😤`)
+			}
+
+			const count = AtomIO.selectorFamily<number, string, ClientError>({
+				key: `count`,
+				get: () => retrieveState,
+				catch: [ClientError],
+			})
+
+			const err = AtomIO.getState(count, `example`)
+
+			expect(err).toBeInstanceOf(ClientError)
+		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, () => {
+			class ClientError extends Error {}
+
+			const retrieveState = (): number => {
+				throw new Error(`😤`) // not a ClientError
+			}
+
+			const count = AtomIO.selectorFamily<number, string, ClientError>({
+				key: `count`,
+				get: () => retrieveState,
+				catch: [ClientError],
+			})
+
+			expect(() => AtomIO.getState(count, `example`)).toThrowError(Error)
+		})
 	})
 })
-describe(`loadable states that throw`, async () => {
-	it(`loadable atom: catches a rejected promise`, async () => {
-		class ClientError extends Error {}
 
-		const retrieveState = async (): Promise<number> => {
-			throw new ClientError(`😤`)
-		}
+describe(`loadable states that reject`, async () => {
+	describe(`loadable atom`, () => {
+		it(`catches a rejected promise`, async () => {
+			class ClientError extends Error {}
 
-		const count = AtomIO.atom<Loadable<number>, ClientError>({
-			key: `count`,
-			default: retrieveState,
-			catch: [ClientError],
+			const retrieveState = async (): Promise<number> => {
+				throw new ClientError(`😤`)
+			}
+
+			const count = AtomIO.atom<Loadable<number>, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
+
+			const err = await AtomIO.getState(count)
+
+			expect(err).toBeInstanceOf(ClientError)
 		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, async () => {
+			class ClientError extends Error {}
+			const retrieveState = async (): Promise<number> => {
+				throw new Error(`😤`) // not a ClientError
+			}
+			const count = AtomIO.atom<Loadable<number>, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
 
-		const err = await AtomIO.getState(count)
-
-		expect(err).toBeInstanceOf(ClientError)
+			let err: any
+			try {
+				await AtomIO.getState(count)
+			} catch (e) {
+				err = e
+			}
+			expect(err).toBeInstanceOf(Error)
+		})
 	})
-	it(`loadable selector: catches a rejected promise`, async () => {
-		class ClientError extends Error {}
+	describe(`loadable atom family`, () => {
+		it(`(happy) catches a thrown error`, async () => {
+			class ClientError extends Error {}
 
-		const retrieveState = async (): Promise<number> => {
-			throw new ClientError(`😤`)
-		}
+			const retrieveState = async (): Promise<number> => {
+				throw new ClientError(`😤`)
+			}
 
-		const count = AtomIO.atom<Loadable<number>, ClientError>({
-			key: `count`,
-			default: retrieveState,
-			catch: [ClientError],
+			const count = AtomIO.atomFamily<Loadable<number>, string, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
+
+			const err = await AtomIO.getState(count, `example`)
+
+			expect(err).toBeInstanceOf(ClientError)
 		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, async () => {
+			class ClientError extends Error {}
+			const retrieveState = async (): Promise<number> => {
+				throw new Error(`😤`) // not a ClientError
+			}
+			const count = AtomIO.atomFamily<Loadable<number>, string, ClientError>({
+				key: `count`,
+				default: retrieveState,
+				catch: [ClientError],
+			})
+			let err: any
+			try {
+				await AtomIO.getState(count, `example`)
+			} catch (e) {
+				err = e
+			}
+			expect(err).toBeInstanceOf(Error)
+		})
+	})
+	describe(`loadable selector`, async () => {
+		it(`(happy) catches a rejected promise`, async () => {
+			class ClientError extends Error {}
 
-		const err = await AtomIO.getState(count)
+			const retrieveState = async (): Promise<number> => {
+				throw new ClientError(`😤`)
+			}
 
-		expect(err).toBeInstanceOf(ClientError)
+			const count = AtomIO.selector<Loadable<number>, ClientError>({
+				key: `count`,
+				get: retrieveState,
+				catch: [ClientError],
+			})
+
+			const err = await AtomIO.getState(count)
+
+			expect(err).toBeInstanceOf(ClientError)
+		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, async () => {
+			class ClientError extends Error {}
+
+			const retrieveState = async (): Promise<number> => {
+				throw new Error(`😤`) // not a ClientError
+			}
+
+			const count = AtomIO.selector<Loadable<number>, ClientError>({
+				key: `count`,
+				get: retrieveState,
+				catch: [ClientError],
+			})
+
+			let err: any
+			try {
+				await AtomIO.getState(count)
+			} catch (e) {
+				err = e
+			}
+			expect(err).toBeInstanceOf(Error)
+		})
+	})
+	describe(`loadable selector family`, async () => {
+		it(`(happy) catches a thrown error`, async () => {
+			class ClientError extends Error {}
+
+			const retrieveState = async (): Promise<number> => {
+				throw new ClientError(`😤`)
+			}
+
+			const count = AtomIO.selectorFamily<Loadable<number>, string, ClientError>(
+				{
+					key: `count`,
+					get: () => retrieveState,
+					catch: [ClientError],
+				},
+			)
+
+			const err = await AtomIO.getState(count, `example`)
+
+			expect(err).toBeInstanceOf(ClientError)
+		})
+		it(`(sad) doesn't catch an error category it doesn't know about`, async () => {
+			class ClientError extends Error {}
+			const retrieveState = async (): Promise<number> => {
+				throw new Error(`😤`) // not a ClientError
+			}
+			const count = AtomIO.selectorFamily<Loadable<number>, string, ClientError>(
+				{
+					key: `count`,
+					get: () => retrieveState,
+					set: () => () => {},
+					catch: [ClientError],
+				},
+			)
+			let err: any
+			try {
+				await AtomIO.getState(count, `example`)
+			} catch (e) {
+				err = e
+			}
+			expect(err).toBeInstanceOf(Error)
+		})
 	})
 })
