c466e8123e581dcc470099cba79fbae364f6b353
Jeremy Banka <hello@jeremybanka.com>
2025-08-31 16:40:41 -0700

    ✅ unit test for ipc

--- packages/atom.io/__tests__/experimental/realtime/ipc-socket.test.tsx
+++ packages/atom.io/__tests__/experimental/realtime/ipc-socket.test.tsx
@@ -10,28 +10,59 @@ beforeEach(() => {
 	vitest.spyOn(logger, `info`)
 })
 
-test(`inter-process communication`, () => {
-	const parentToChild = new PassThrough()
-	const childToParent = new PassThrough()
-	const childLogs = new PassThrough()
-	const parentLogs = new PassThrough()
-
+test(`inter-process communication`, async () => {
+	const stdin = new PassThrough()
+	const stdout = new PassThrough()
+	const stderr = new PassThrough()
 	const pid = 1
 
-	const child = new ChildSocket(
+	const parentsInterfaceToChild = new ChildSocket(
 		{
-			stdin: parentToChild,
-			stdout: childToParent,
-			stderr: childLogs,
+			stdin,
+			stdout,
+			stderr,
 			pid,
 			kill: () => true,
 		},
 		`child-socket`,
 	)
-	const parent = new ParentSocket({
-		stdin: childToParent,
-		stdout: parentToChild,
-		stderr: parentLogs,
+
+	const childsInterfaceToParent = new ParentSocket({
+		stdin,
+		stdout,
+		stderr,
 		pid,
+		exit: () => {
+			console.log(`exited`)
+		},
 	})
+
+	const gotPing = new Promise<[string]>((resolve) => {
+		childsInterfaceToParent.on(`ping`, (msg: string) => {
+			resolve([msg])
+		})
+	})
+
+	parentsInterfaceToChild.emit(`ping`, `hello from parent`)
+
+	const [ping] = await gotPing
+
+	console.log(`⛔⛔⛔⛔⛔ gotPing`, ping)
+
+	expect(ping).toBe(`hello from parent`)
+
+	// and now test the reverse
+	const gotPong = new Promise<[string]>((resolve) => {
+		parentsInterfaceToChild.on(`pong`, (msg: string) => {
+			resolve([msg])
+		})
+	})
+
+	childsInterfaceToParent.emit(`pong`, `hello from child`)
+
+	const [pong] = await gotPong
+
+	console.log(`⛔⛔⛔⛔⛔ gotPing`, pong)
+
+	expect(pong).toBe(`hello from child`)
 })
