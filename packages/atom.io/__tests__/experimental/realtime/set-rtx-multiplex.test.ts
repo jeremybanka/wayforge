e6cf60c11f0bb498c4b9baf1bddf87584af882f2
Jeremy Banka <hello@jeremybanka.com>
2025-09-12 08:00:56 -0700

    🚧 build out transaction test

--- packages/atom.io/__tests__/experimental/realtime/set-rtx-multiplex.test.ts
+++ packages/atom.io/__tests__/experimental/realtime/set-rtx-multiplex.test.ts
@@ -1,3 +1,6 @@
+import { inspect } from "node:util"
+
+import type { Logger, TransactionOutcomeEvent } from "atom.io"
 import {
 	editRelations,
 	findRelations,
@@ -7,12 +10,32 @@ import {
 	join,
 	mutableAtom,
 	mutableAtomFamily,
+	runTransaction,
 	selectorFamily,
 	setState,
 	subscribe,
+	transaction,
 } from "atom.io"
 import { IMPLICIT } from "atom.io/internal"
+import * as Internal from "atom.io/internal"
 import { SetRTX } from "atom.io/transceivers/set-rtx"
+
+import * as Utils from "../../__util__"
+
+const LOG_LEVELS = [null, `error`, `warn`, `info`] as const
+const CHOOSE = 3
+
+let logger: Logger
+
+beforeEach(() => {
+	Internal.clearStore(Internal.IMPLICIT.STORE)
+	Internal.IMPLICIT.STORE.loggers[0].logLevel = LOG_LEVELS[CHOOSE]
+	logger = Internal.IMPLICIT.STORE.logger //= Utils.createNullLogger()
+	vitest.spyOn(logger, `error`)
+	vitest.spyOn(logger, `warn`)
+	vitest.spyOn(logger, `info`)
+	vitest.spyOn(Utils, `stdout`)
+})
 
 describe(`join in perspective`, () => {
 	test(`card game: players cannot see the values of cards private to other players or hidden from view`, () => {
@@ -246,5 +269,130 @@ describe(`join in perspective`, () => {
 		expect(
 			getState(cardFacesPlayerViewAtoms, [`player::alice`, `face::hearts-5`]),
 		).toEqual(new SetRTX([]))
+	})
+
+	test.only(`translating a transaction update`, () => {
+		const isCardKey = (key: string): key is `card::${string}` =>
+			key.startsWith(`card::`)
+		const isFaceKey = (key: string): key is `face::${string}` =>
+			key.startsWith(`face::`)
+		const isStackKey = (key: string): key is StackKey => key.startsWith(`stack-`)
+		const isPlayerKey = (key: string): key is `player::${string}` =>
+			key.startsWith(`player::`)
+
+		const facesOfCards = join({
+			key: `facesOfCards`,
+			between: [`face`, `card`],
+			cardinality: `1:n`,
+			isAType: isFaceKey,
+			isBType: isCardKey,
+		})
+		const stacksOfCards = join({
+			key: `stacksOfCards`,
+			between: [`stack`, `card`],
+			cardinality: `1:n`,
+			isAType: isStackKey,
+			isBType: isCardKey,
+		})
+		const playersOwningStacks = join({
+			key: `playersOwningStacks`,
+			between: [`owner`, `stack`],
+			cardinality: `1:n`,
+			isAType: isPlayerKey,
+			isBType: isStackKey,
+		})
+
+		type StackType = `deck` | `hand` | `pile` | `trick`
+		type StackKey = `stack-${StackType}::${string}`
+		const stackType = (stackKey: StackKey): `stack-${StackType}` =>
+			stackKey.split(`::`)[0] as any
+
+		const playerKeysAtom = mutableAtom<SetRTX<`player::${string}`>>({
+			key: `playerKeys`,
+			class: SetRTX,
+		})
+		const stackKeysAtom = mutableAtom<SetRTX<`stack-${StackType}::${string}`>>({
+			key: `stackKeys`,
+			class: SetRTX,
+		})
+		const faceKeysAtom = mutableAtom<SetRTX<`face::${string}`>>({
+			key: `faceKeys`,
+			class: SetRTX,
+		})
+		const cardKeysAtom = mutableAtom<SetRTX<`card::${string}`>>({
+			key: `cardKeys`,
+			class: SetRTX,
+		})
+
+		const visibleCardKeys = selectorFamily<
+			`card::${string}`[],
+			`player::${string}`
+		>({
+			key: `visibleCardKeys`,
+			get:
+				(playerKey) =>
+				({ get }) => {
+					const visible: `card::${string}`[] = []
+					const cardKeys = get(cardKeysAtom)
+					for (const cardKey of cardKeys) {
+						const stackKey = get(
+							findRelations(stacksOfCards, cardKey).stackKeyOfCard,
+						)
+						if (!stackKey) continue
+						switch (stackType(stackKey)) {
+							case `stack-deck`:
+								break
+							case `stack-hand`:
+								{
+									const ownerKey = get(
+										findRelations(playersOwningStacks, stackKey).ownerKeyOfStack,
+									)
+									if (ownerKey === playerKey) {
+										visible.push(cardKey)
+									}
+								}
+								break
+							case `stack-pile`:
+								visible.push(cardKey)
+								break
+							case `stack-trick`:
+								visible.push(cardKey)
+						}
+					}
+					return visible
+				},
+		})
+
+		const transferCardTx = transaction<
+			(cardKey: `card::${string}`, to: StackKey) => void
+		>({
+			key: `transferCard`,
+			do: (_, cardKey, to) => {
+				editRelations(stacksOfCards, (relations) => {
+					relations.set(to, cardKey)
+				})
+			},
+		})
+
+		setState(cardKeysAtom, (keys) => keys.add(`card::44`))
+		setState(stackKeysAtom, (keys) =>
+			keys.add(`stack-pile::1`).add(`stack-deck::1`),
+		)
+		setState(faceKeysAtom, (keys) => keys.add(`face::hearts-5`))
+		editRelations(stacksOfCards, (relations) => {
+			relations.set(`stack-pile::1`, `card::44`)
+		})
+		editRelations(facesOfCards, (relations) => {
+			relations.set(`face::hearts-5`, `card::44`)
+		})
+		let outcome: TransactionOutcomeEvent<typeof transferCardTx> | undefined
+
+		subscribe(transferCardTx, (event) => {
+			outcome = event
+		})
+
+		runTransaction(transferCardTx)(`card::44`, `stack-deck::1`)
+
+		console.log(inspect(outcome, false, 10, true))
 	})
 })
