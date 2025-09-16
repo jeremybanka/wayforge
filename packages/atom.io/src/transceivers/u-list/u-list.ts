b9dc731096e70cad3cb09b89399be8c0be2c948d
Jeremy Banka <hello@jeremybanka.com>
2025-09-16 00:54:49 -0700

    🧪 optimal string rpc

--- packages/atom.io/src/transceivers/u-list/u-list.ts
+++ packages/atom.io/src/transceivers/u-list/u-list.ts
@@ -2,6 +2,18 @@ import type { Fn, Transceiver, TransceiverMode } from "atom.io/internal"
 import { Subject } from "atom.io/internal"
 import type { primitive } from "atom.io/json"
 
+type Enumeration<T extends string> = Record<T, number> & Record<number, T>
+function enumeration<T extends string>(...values: T[]): Enumeration<T> {
+	const result: Record<any, any> = {}
+	let i = 0
+	for (const value of values) {
+		result[value] = i
+		result[i] = value
+		++i
+	}
+	return result
+}
+
 export type SetMutations = Exclude<
 	keyof Set<any>,
 	symbol | keyof ReadonlySet<any>
@@ -19,6 +31,91 @@ export type UListUpdateType = SetUpdate<any>[`type`]
 true satisfies SetMutations extends UListUpdateType
 	? true
 	: Exclude<SetMutations, UListUpdateType>
+
+export type PackedSetUpdate<P extends primitive> = string & {
+	update?: SetUpdate<P>
+}
+
+const BOOL = "\u0001"
+const NULL = "\u0002"
+const STRING = "\u0003"
+const NUMBER = "\u0004"
+
+export const SET_UPDATE_ENUM: Enumeration<UListUpdateType> = enumeration(
+	`add`,
+	`delete`,
+	`clear`,
+)
+
+export function packSetUpdate<P extends primitive>(
+	update: SetUpdate<P>,
+): PackedSetUpdate<P> {
+	const head = SET_UPDATE_ENUM[update.type] + `\u001F`
+	if (update.type === `clear`) {
+		return (
+			head +
+			update.values
+				.map((value) => {
+					switch (typeof value) {
+						case `string`:
+							return STRING + value
+						case `number`:
+							return NUMBER + value
+						case `boolean`:
+							return BOOL + +value
+						case `object`:
+							return NULL
+					}
+				})
+				.join(`\u001E`)
+		)
+	}
+	switch (typeof update.value) {
+		case `string`:
+			return head + STRING + update.value
+		case `number`:
+			return head + NUMBER + update.value
+		case `boolean`:
+			return head + BOOL + +update.value
+		case `object`:
+			return head + NULL
+	}
+}
+export function unpackSetUpdate<P extends primitive>(
+	packed: PackedSetUpdate<P>,
+): SetUpdate<P> {
+	const [type, tail] = packed.split(`\u001F`) as [number, string]
+	const head = SET_UPDATE_ENUM[type]
+	if (head === `clear`) {
+		const values = tail.split(`\u001E`).map((value) => {
+			switch (value[0]) {
+				case STRING:
+					return value.slice(1)
+				case NUMBER:
+					return +value.slice(1)
+				case BOOL:
+					return value.slice(1) === `1`
+				case NULL:
+					return null
+			}
+		}) as P[]
+		return { type: `clear`, values }
+	}
+	switch (head) {
+		case `add`:
+		case `delete`:
+			switch (tail[0] as `\u0001` | `\u0002` | `\u0003` | `\u0004`) {
+				case STRING:
+					return { type: head, value: tail.slice(1) as P }
+				case NUMBER:
+					return { type: head, value: +tail.slice(1) as P }
+				case BOOL:
+					return { type: head, value: (tail.slice(1) === `1`) as P }
+				case NULL:
+					return { type: head, value: null as P }
+			}
+	}
+}
 
 export type SetMutationHandler = { [K in UListUpdateType]: Fn }
 
