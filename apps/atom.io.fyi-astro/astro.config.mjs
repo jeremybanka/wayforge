401221ebde929b7946f8b62f2e2ac5873a0206a2
Jeremy Banka <hello@jeremybanka.com>
2025-10-01 12:39:01 -0700

    ➕ mdx

--- apps/atom.io.fyi-astro/astro.config.mjs
+++ apps/atom.io.fyi-astro/astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), mdx()]
 });
