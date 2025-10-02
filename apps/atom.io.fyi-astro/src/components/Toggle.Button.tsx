3e310d1a80562e27c9cdc8629599a672418ea1b6
Jeremy Banka <hello@jeremybanka.com>
2025-10-01 20:10:59 -0700

    ✨ port components (almost no work)

---
+++ apps/atom.io.fyi-astro/src/components/Toggle.Button.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import type { ToggleProps } from "./Toggle"
+import label from "./Toggle.Button.module.scss"
+import type { VNode } from "preact"
+
+export const setCssVars = (
+	vars: Record<`--${string}`, number | string>,
+): Partial<React.CSSProperties> => vars as any
+
+export function Button({ children, checked, onChange }: ToggleProps): VNode {
+	return (
+		<label
+			className={label[`class`]}
+			style={setCssVars({
+				"--width": `40px`,
+				"--height": `40px`,
+			})}
+		>
+			<input type="checkbox" checked={checked} onChange={onChange} />
+			<div />
+			<span>{children}</span>
+		</label>
+	)
+}
