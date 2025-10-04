11c021f59a9d1880c82508ae173b376770779bf5
Jeremy Banka <hello@jeremybanka.com>
2025-10-03 19:41:39 -0700

    🔧 cloudflare adapter

--- apps/atom.io.fyi-astro/astro.config.ts
+++ apps/atom.io.fyi-astro/astro.config.ts
@@ -1,3 +1,4 @@
+import cloudflare from "@astrojs/cloudflare"
 import mdx from "@astrojs/mdx"
 import preact from "@astrojs/preact"
 import { defineConfig } from "astro/config"
@@ -5,4 +6,5 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
 	integrations: [preact({ compat: true }), mdx()],
+	adapter: cloudflare(),
 })
