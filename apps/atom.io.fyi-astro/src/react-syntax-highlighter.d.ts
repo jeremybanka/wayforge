0392a6cdb06a0dcfbba9b9ebd6e80127f42f7e8f
Jeremy Banka <hello@jeremybanka.com>
2025-10-10 10:21:31 -0700

    🏷️ patch in preact-jsx-compatible type for react-syntax-highlighter

---
+++ apps/atom.io.fyi-astro/src/react-syntax-highlighter.d.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line quotes
+declare module "react-syntax-highlighter/dist/esm/prism" {
+	import type { SyntaxHighlighterProps } from "react-syntax-highlighter"
+
+	export default function (props: SyntaxHighlighterProps): JSX.Element
+}
