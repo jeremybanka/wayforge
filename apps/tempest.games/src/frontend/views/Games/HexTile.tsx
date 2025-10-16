4079c321d5ed8957f4720f9a8497118307e69ee7
Jeremy Banka <hello@jeremybanka.com>
2025-10-16 10:25:45 -0700

    🔧 render coordinates

--- apps/tempest.games/src/frontend/views/Games/HexTile.tsx
+++ apps/tempest.games/src/frontend/views/Games/HexTile.tsx
@@ -58,3 +58,48 @@ export function HexTile({
 		</mesh>
 	)
 }
+
+export function GameTile({
+	coordinates,
+}: {
+	coordinates: { x: number; y: number; z: number }
+}) {
+	const { x: boardA, y: boardB, z: boardC } = coordinates
+	// if (boardX + boardY + boardZ !== 0) {
+	// 	console.error(`GameTile: bad coordinates did not add to zero`, coordinates)
+	// 	return null
+	// }
+
+	const unit = Math.sqrt(3)
+	const a120 = (Math.PI * 2) / 3
+	const a240 = a120 * 2
+	const sin120 = Math.sin(a120)
+	const cos120 = Math.cos(a120)
+	const sin240 = Math.sin(a240)
+	const cos240 = Math.cos(a240)
+
+	const uA = unit * boardA
+	const uB = unit * boardB
+	const uC = unit * boardC
+
+	const sin120UA = uA * sin120
+	const cos120UA = uA * cos120
+	// const sin120UB = uB * sin120
+	// const cos120UB = uB * cos120
+	// const sin120UC = uC * sin120
+	// const cos120UC = uC * cos120
+
+	// const sin240UA = uA * sin240
+	// const cos240UA = uA * cos240
+	const sin240UB = uB * sin240
+	const cos240UB = uB * cos240
+	// const sin240UC = uC * sin240
+	// const cos240UC = uC * cos240
+
+	const x = sin120UA + sin240UB
+	const z = cos120UA + cos240UB + uC
+
+	// return <HexTile position={[0, 0, uC]} /> // works with z1
+	// return <HexTile position={[sin120UA, 0, cos120UA]} /> // works with x1
+	return <HexTile position={[x, 0, z]} /> // works with y1
+}
