f5b4772b35e3fe8ed0765211587a540e812d2e6b
Jeremy Banka <hello@jeremybanka.com>
2025-10-18 19:37:03 -0700

    🔥 remove next implementation of atom.io.fyi

--- apps/atom.io.fyi/src/app/docs/Toggle.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "./Toggle.Button"
-
-export type ToggleProps = {
-	children: string
-	checked: boolean
-	onChange: () => void
-}
-
-export const Toggle = { Button }
--- apps/atom.io.fyi/src/components/DevtoolsDynamic.tsx
+++
@@ -1,5 +0,0 @@
-"use client"
-
-import "atom.io/react-devtools/css"
-
-export { AtomIODevtools } from "atom.io/react-devtools"
