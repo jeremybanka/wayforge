ab19e43feb39d4434e1426b32beb5c90c3df8629
Jeremy Banka <hello@jeremybanka.com>
2025-10-18 22:30:01 -0700

    🧹 clean up

--- packages/hamr/__scripts__/manifest-test.node.ts
+++ packages/hamr/__scripts__/manifest-test.node.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import defineIntegrationScope from "./define-integration-scope.node"
-import definePackageExports from "./define-package-exports.node"
+import defineIntegrationScope from "./define-integration-scope.node.ts"
+import definePackageExports from "./define-package-exports.node.ts"
 
 definePackageExports(`test`)
 defineIntegrationScope(`test`)
