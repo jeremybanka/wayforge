c8360aba719a0ccbc0fe811a61cb4de8033912b1
Jeremy Banka <hello@jeremybanka.com>
2025-10-18 19:36:34 -0700

    🚚 move some future plans over to astro

---
+++ apps/atom.io.fyi-astro/scripts/tsdoc.bun.worker.ts
@@ -0,0 +1,27 @@
+import path from "node:path"
+
+import * as Bun from "bun"
+import { compileDocs } from "tsdoc.json"
+
+import {
+	ATOM_IO_ROOT,
+	ATOM_IO_SRC,
+} from "../../../packages/atom.io/__scripts__/constants"
+import { ATOM_IO_FYI_ROOT } from "./constants"
+
+declare const self: Worker
+self.onmessage = async function tsDocWorkerJob({
+	data: subPackageName,
+}: {
+	data: string
+}): Promise<void> {
+	console.log(`📝 Extracting ${subPackageName}`)
+	const entrypoint = path.join(ATOM_IO_SRC, subPackageName, `index.ts`)
+	const tsconfigPath = path.join(ATOM_IO_ROOT, `tsconfig.json`)
+	const doc = compileDocs({ entrypoint, tsconfigPath })
+	await Bun.write(
+		path.join(ATOM_IO_FYI_ROOT, `gen`, `${subPackageName}.tsdoc.json`),
+		JSON.stringify(doc, null, `\t`),
+	)
+	self.postMessage(`Done`)
+}
---
+++ apps/atom.io.fyi-astro/src/components/ModuleDocs.tsx
@@ -0,0 +1,29 @@
+import "./ModuleDocs.scss"
+
+import fs from "node:fs"
+import path from "node:path"
+
+import type { VNode } from "preact"
+import type { TSD } from "tsdoc.json"
+import { Mod } from "tsdoc.json/react"
+
+import { ATOM_IO_FYI_ROOT } from "../../scripts/constants"
+
+export function ModuleDocs({ module }: { module: string }): VNode {
+	const docsText = fs.readFileSync(
+		path.join(ATOM_IO_FYI_ROOT, `gen`, `${module}.tsdoc.json`),
+		`utf-8`,
+	)
+	const docs: TSD.Doc[] = JSON.parse(docsText)
+	return (
+		<Mod
+			className="tsdoc-styles"
+			docs={docs.filter(
+				(doc) =>
+					// doc.name === `setState` /* ❗ */ &&
+					doc.type === `function` ||
+					(doc.type === `composite` && doc.kind === `class`),
+			)}
+		/>
+	)
+}
