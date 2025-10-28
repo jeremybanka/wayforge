2c7941cc2cd68525da863be3b67d73514833dccf
Jeremy Banka <hello@jeremybanka.com>
2025-10-27 17:59:01 -0700

    ✨ render everything

--- packages/create-atom/templates/base/src/BezierPlayground.tsx
+++ packages/create-atom/templates/base/src/BezierPlayground.tsx
@@ -80,9 +80,9 @@ const subpathKeysAtoms = atomFamily<string[], string>({
 	key: "subpathKeys",
 	default: [],
 })
-const nodeAtoms = atomFamily<PointXY, string>({
+const nodeAtoms = atomFamily<PointXY | null, string>({
 	key: "nodeAtoms",
-	default: { x: 0, y: 0 },
+	default: null,
 })
 const edgeAtoms = atomFamily<null | { c?: PointXY; s: PointXY }, string>({
 	key: "edgeAtoms",
@@ -94,13 +94,13 @@ function clamp(n: number, min: number, max: number) {
 }
 
 function InteractiveNode({ subpathKey }: { subpathKey: string }) {
-	const { x, y } = useO(nodeAtoms, subpathKey)
+	const node = useO(nodeAtoms, subpathKey)
 	const edge = useO(edgeAtoms, subpathKey)
-	return edge === null ? (
+	return node === null ? null : edge === null ? (
 		<rect
 			key={subpathKey}
-			x={x - 5}
-			y={y - 5}
+			x={node.x - 5}
+			y={node.y - 5}
 			width={10}
 			height={10}
 			fill="white"
@@ -113,8 +113,8 @@ function InteractiveNode({ subpathKey }: { subpathKey: string }) {
 	) : (
 		<circle
 			key={subpathKey}
-			cx={x}
-			cy={y}
+			cx={node.x}
+			cy={node.y}
 			r={5}
 			fill="red"
 			onPointerDown={(evt) => {
@@ -125,19 +125,30 @@ function InteractiveNode({ subpathKey }: { subpathKey: string }) {
 	)
 }
 
-function Subpath({ subpathKey, idx }: { subpathKey: string; idx?: number }) {
+function Subpath({
+	subpathKey,
+	prev,
+	idx,
+}: {
+	subpathKey: string
+	prev: string
+	idx: number
+}) {
 	const node = useO(nodeAtoms, subpathKey)
 	const edge = useO(edgeAtoms, subpathKey)
-	if (idx === 0) {
+	if (node === null) {
+		return "Z"
+	}
+	if (idx === 0 || prev === null) {
 		return `M ${node.x} ${node.y}`
 	}
 	if (edge === null) {
 		return `L ${node.x} ${node.y}`
 	}
 	if (`c` in edge) {
-		return `C ${edge.c.x} ${edge.c.y} ${edge.s.x} ${edge.s.y} ${node.x} ${node.y} `
+		return `C ${edge.c.x} ${edge.c.y} ${edge.s.x} ${edge.s.y} ${node.x} ${node.y}`
 	}
-	return `S ${edge.s.x} ${edge.s.y} ${node.x} ${node.y} `
+	return `S ${edge.s.x} ${edge.s.y} ${node.x} ${node.y}`
 }
 
 function Path({ pathKey }: { pathKey: string }) {
@@ -147,7 +158,7 @@ function Path({ pathKey }: { pathKey: string }) {
 	return (
 		<>
 			<path
-				d={`${subpathKeys.map((spk, idx) => Subpath({ subpathKey: spk, idx })).join(" ")} Z`}
+				d={`${subpathKeys.map((spk, idx) => Subpath({ subpathKey: spk, prev: subpathKeys[idx - 1], idx })).join(" ")} Z`}
 				stroke="red"
 				fill="none"
 			/>
@@ -212,14 +223,11 @@ const reset = () => {
 			}
 			resetState(pathKeysAtom)
 
-			const [hexagon, northeastOval, northwestOval, circle] = text
+			const shapes = text
 				.split("\n")
 				.filter((l) => l.startsWith("\t<path"))
 				.map((l) => l.split(`d="`)[1].slice(0, -9))
 				.map((raw, idx) => {
-					if (idx === 1 || idx === 2) {
-						return []
-					}
 					const CODES = [
 						`m`,
 						`M`,
@@ -232,7 +240,7 @@ const reset = () => {
 						`z`,
 						`Z`,
 					] as const
-					type Letter = Exclude<(typeof CODES)[number], `z` | `Z`>
+					type Letter = (typeof CODES)[number]
 					let letter: Letter | undefined
 					let number = ``
 					let numbers: number[] = []
@@ -264,20 +272,22 @@ const reset = () => {
 						}
 
 						number += c
-						console.log(number)
 					}
 					console.log(raw)
-					// console.log("👺", JSON.stringify(instructions, null, 2))
+					console.log("👺", JSON.stringify(instructions, null, 2))
 					// throw new Error("stop")
 					let prev: PointXY = { x: 0, y: 0 }
 					const edgeNodes = instructions.map<{
+						node: null | PointXY
 						edge: null | { c?: PointXY; s: PointXY }
-						node: PointXY
 					}>(({ letter, numbers }) => {
-						let node: PointXY
+						let node: null | PointXY
 						let edge: null | { c?: PointXY; s: PointXY }
 						switch (letter) {
 							case `m`:
+								node = { x: prev.x + numbers[0], y: prev.y + numbers[1] }
+								edge = null
+								break
 							case `M`:
 								node = { x: numbers[0], y: numbers[1] }
 								edge = null
@@ -311,11 +321,18 @@ const reset = () => {
 							case `V`:
 								node = { x: prev.x, y: numbers[0] }
 								edge = null
+								break
+							case `z`:
+							case `Z`:
+								node = null
+								edge = null
 						}
-						prev = node
-						console.log(
-							`letter: ${letter}, numbers: ${JSON.stringify(numbers)}, node: [${node.x},${node.y}], c:[${edge?.c?.x},${edge?.c?.y}], s:[${edge?.s?.x},${edge?.s?.y}], prev: [${prev.x},y:${prev.y}]`,
-						)
+						if (node) {
+							prev = node
+						}
+						// console.log(
+						// 	`letter: ${letter}, numbers: ${JSON.stringify(numbers)}, node: [${node.x},${node.y}], c:[${edge?.c?.x},${edge?.c?.y}], s:[${edge?.s?.x},${edge?.s?.y}], prev: [${prev.x},y:${prev.y}]`,
+						// )
 						return { node, edge }
 					})
 
@@ -326,7 +343,7 @@ const reset = () => {
 
 			let i = 0
 			let j = 0
-			for (const shape of [circle, hexagon]) {
+			for (const shape of shapes) {
 				const jj = j
 				for (const { node, edge } of shape) {
 					if (edge) {
@@ -349,10 +366,3 @@ const reset = () => {
 			}
 		})
 }
-
-const preact = [
-	"m128 0l128 73.9v147.8l-128 73.9L0 221.7V73.9z",
-	"M34.865 220.478c17.016 21.78 71.095 5.185 122.15-34.704c51.055-39.888 80.24-88.345 63.224-110.126c-17.017-21.78-71.095-5.184-122.15 34.704c-51.055 39.89-80.24 88.346-63.224 110.126Zm7.27-5.68c-5.644-7.222-3.178-21.402 7.573-39.253c11.322-18.797 30.541-39.548 54.06-57.923c23.52-18.375 48.303-32.004 69.281-38.442c19.922-6.113 34.277-5.075 39.92 2.148c5.644 7.223 3.178 21.403-7.573 39.254c-11.322 18.797-30.541 39.547-54.06 57.923c-23.52 18.375-48.304 32.004-69.281 38.441c-19.922 6.114-34.277 5.076-39.92-2.147Z",
-	"M220.239 220.478c17.017-21.78-12.169-70.237-63.224-110.126C105.96 70.464 51.88 53.868 34.865 75.648c-17.017 21.78 12.169 70.238 63.224 110.126c51.055 39.889 105.133 56.485 122.15 34.704Zm-7.27-5.68c-5.643 7.224-19.998 8.262-39.92 2.148c-20.978-6.437-45.761-20.066-69.28-38.441c-23.52-18.376-42.74-39.126-54.06-57.923c-10.752-17.851-13.218-32.03-7.575-39.254c5.644-7.223 19.999-8.261 39.92-2.148c20.978 6.438 45.762 20.067 69.281 38.442c23.52 18.375 42.739 39.126 54.06 57.923c10.752 17.85 13.218 32.03 7.574 39.254Z",
-	"M127.552 167.667c10.827 0 19.603-8.777 19.603-19.604c0-10.826-8.776-19.603-19.603-19.603c-10.827 0-19.604 8.777-19.604 19.603c0 10.827 8.777 19.604 19.604 19.604Z",
-]
