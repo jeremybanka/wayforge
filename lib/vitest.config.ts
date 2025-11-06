9ae086a524376a400fc686264949216eff6aa343
Jeremy Banka <hello@jeremybanka.com>
2025-11-06 00:17:39 -0800

    drop extraneous dependencies (#5287)
    * ➖ drop extra eslint/ deps
    
    * 🔧 bit of cleanup in hamr's build
    
    * ➖ c8
    
    * ➖ vite-stconfig-paths
    
    * ➖ types/bun
    
    * ➖ nyc
    
    * ➖ typescript
    
    * 🔧 remove tsconfig paths plugin from most configs

--- lib/vitest.config.ts
+++ lib/vitest.config.ts
@@ -1,9 +1,3 @@
-import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
-export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: {
-		globals: true,
-	},
-})
+export default defineConfig({ test: { globals: true } })
