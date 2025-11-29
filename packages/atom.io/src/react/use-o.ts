6d1594fe18365e02e555c1f2e689df38c5961eff
Jeremy Banka <hello@jeremybanka.com>
2025-11-29 11:53:03 -0800

    🐛 trap value in useO to satisfy react

--- packages/atom.io/src/react/use-o.ts
+++ packages/atom.io/src/react/use-o.ts
@@ -1,17 +1,27 @@
-import type { ReadableFamilyToken, ReadableToken } from "atom.io"
+import type { ReadableFamilyToken, ReadableToken, ViewOf } from "atom.io"
 import { getFromStore, subscribeToState } from "atom.io/internal"
 import type { Canonical } from "atom.io/json"
-import { useContext, useId, useSyncExternalStore } from "react"
+import {
+	useCallback,
+	useContext,
+	useId,
+	useRef,
+	useSyncExternalStore,
+} from "react"
 
 import { parseStateOverloads } from "./parse-state-overloads"
 import { StoreContext } from "./store-context"
 
-export function useO<T, E = never>(token: ReadableToken<T, any, E>): E | T
+const UNSET = Symbol(`UNSET`)
+
+export function useO<T, E = never>(
+	token: ReadableToken<T, any, E>,
+): ViewOf<E | T>
 
 export function useO<T, K extends Canonical, E = never>(
 	token: ReadableFamilyToken<T, K, E>,
 	key: NoInfer<K>,
-): E | T
+): ViewOf<E | T>
 
 export function useO<T, K extends Canonical, E = never>(
 	...params:
@@ -21,9 +31,28 @@ export function useO<T, K extends Canonical, E = never>(
 	const store = useContext(StoreContext)
 	const token = parseStateOverloads(store, ...params)
 	const id = useId()
-	return useSyncExternalStore<E | T>(
-		(dispatch) => subscribeToState(store, token, `use-o:${id}`, dispatch),
-		() => getFromStore(store, token),
-		() => getFromStore(store, token),
+	const ref = useRef<ViewOf<E | T> | typeof UNSET>(UNSET)
+	const sub = useCallback(
+		(dispatch: () => void) =>
+			subscribeToState<T, E>(
+				store,
+				token,
+				`use-o:${id}`,
+				function trapValue({ newValue }) {
+					ref.current = newValue
+					dispatch()
+				},
+			),
+		[token],
 	)
+	const get = useCallback(() => {
+		let value: ViewOf<E | T>
+		if (ref.current === UNSET) {
+			value = ref.current = getFromStore(store, token)
+		} else {
+			value = ref.current
+		}
+		return value
+	}, [token])
+	return useSyncExternalStore<ViewOf<E | T>>(sub, get, get)
 }
