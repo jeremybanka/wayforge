3e7151bd3d3fc49261f19e906303a8672e6d0522
Jeremy Banka <hello@jeremybanka.com>
2025-11-29 18:22:40 -0800

    document reset state (#5485)
    * 💄 style uncaught syntax
    
    * 📝 add reset state summary
    
    * 📝 document example of reset state
    
    * 📝 document useLoadable
    
    * ✏️ fix typo

---
+++ apps/atom.io.fyi/src/exhibits/react/use-loadable-fallback.tsx
@@ -0,0 +1,24 @@
+import { atom, type Loadable } from "atom.io"
+import { useLoadable } from "atom.io/react"
+
+const myApiDataAtom = atom<Loadable<{ name: string }>, Error>({
+	key: `myApiData`,
+	default: async () => {
+		const response = await fetch(`https://api.github.com/users/jeremybanka`)
+		return response.json()
+	},
+	catch: [Error],
+})
+
+export function UrlDisplay(): React.JSX.Element {
+	const myApiData = useLoadable(myApiDataAtom, { name: `Jeremy Banka` })
+	return (
+		<div>
+			<h1>
+				{myApiData.value.name}
+				{myApiData.loading ? ` ⌛` : ``}
+			</h1>
+			{myApiData.error ? <p>{myApiData.error.message}</p> : null}
+		</div>
+	)
+}
