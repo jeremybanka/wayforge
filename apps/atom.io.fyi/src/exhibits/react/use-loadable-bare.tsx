e22814e28d91e6a8ca931dac7a6912efc0ec01e7
Jeremy Banka <hello@jeremybanka.com>
2025-11-29 18:15:14 -0800

    📝 document useLoadable

---
+++ apps/atom.io.fyi/src/exhibits/react/use-loadable-bare.tsx
@@ -0,0 +1,25 @@
+import { atom, type Loadable } from "atom.io"
+import { useLoadable } from "atom.io/react"
+
+const myApiDataAtom = atom<Loadable<{ name: string }>>({
+	key: `myApiData`,
+	default: async () => {
+		const response = await fetch(`https://api.github.com/users/jeremybanka`)
+		return response.json()
+	},
+})
+
+export function UrlDisplay(): React.JSX.Element {
+	const myApiData = useLoadable(myApiDataAtom)
+	if (myApiData === `LOADING`) {
+		return <p>Loading...</p>
+	}
+	return (
+		<div>
+			<h1>
+				{myApiData.value.name}
+				{myApiData.loading ? ` ⌛` : ``}
+			</h1>
+		</div>
+	)
+}
