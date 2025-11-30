c0dc934d46917facbac5cec9e06d17e54c1cf267
Jeremy Banka <hello@jeremybanka.com>
2025-11-30 02:19:50 -0800

    🦋

--- packages/break-check/src/break-check.x.ts
+++ packages/break-check/src/break-check.x.ts
@@ -37,7 +37,7 @@ const BREAK_CHECK_MANUAL = options(
 		tagPattern: {
 			flag: `v`,
 			required: false,
-			description: `String which, if found in a git tag, will be considered a release tag for your library.`,
+			description: `RegExp which, if found matched to a git tag, will be considered a release tag for your library.`,
 			example: `--tagPattern="my-library"`,
 		},
 		testPattern: {
