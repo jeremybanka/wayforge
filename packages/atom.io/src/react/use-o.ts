f72b0f336253001eca3d654b339d1b52beba49c0
Jeremy Banka <hello@jeremybanka.com>
2025-12-01 09:42:05 -0800

    🎨 lint

--- packages/atom.io/src/react/use-o.ts
+++ packages/atom.io/src/react/use-o.ts
@@ -2,7 +2,6 @@
 import type { ReadableFamilyToken, ReadableToken, ViewOf } from "atom.io"
 import { getFromStore, subscribeToState } from "atom.io/internal"
 import type { Canonical } from "atom.io/json"
-import { useSingleEffect } from "./use-single-effect"
 import {
 	useCallback,
 	useContext,
@@ -14,6 +13,7 @@ import {
 
 import { parseStateOverloads } from "./parse-state-overloads"
 import { StoreContext } from "./store-context"
+import { useSingleEffect } from "./use-single-effect"
 
 export function useO<T, E = never>(
 	token: ReadableToken<T, any, E>,
