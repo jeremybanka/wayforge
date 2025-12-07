afb9ec30c0358bc907799c92d45fd9171be6dd70
Jeremy Banka <hello@jeremybanka.com>
2025-12-07 15:52:45 -0800

    ♻️ create the token for myUserKey inline

--- packages/atom.io/src/realtime-server/realtime-server-stores/provide-identity.ts
+++ packages/atom.io/src/realtime-server/realtime-server-stores/provide-identity.ts
@@ -1,5 +1,4 @@
 import { IMPLICIT } from "atom.io/internal"
-import { myUserKeyAtom } from "atom.io/realtime-client"
 
 import { realtimeStateProvider } from "../realtime-state-provider"
 import type { ServerConfig } from "../server-config"
@@ -11,7 +10,7 @@ export function provideIdentity({
 }: ServerConfig): void {
 	const provideState = realtimeStateProvider({ socket, store, userKey })
 
-	const unsub = provideState(myUserKeyAtom, userKey)
+	const unsub = provideState({ key: `myUserKey`, type: `atom` }, userKey)
 
 	socket.on(`disconnect`, () => {
 		unsub()
