7e12b1dade1541f3af4798229f22b9511e1a024d
Jeremy Banka <hello@jeremybanka.com>
2025-12-07 14:58:42 -0800

    ♻️ use userKey directly

--- packages/atom.io/src/realtime-server/realtime-server-stores/provide-identity.ts
+++ packages/atom.io/src/realtime-server/realtime-server-stores/provide-identity.ts
@@ -1,21 +1,14 @@
-import { findRelationsInStore, getFromStore, IMPLICIT } from "atom.io/internal"
-import type { SocketKey } from "atom.io/realtime"
+import { IMPLICIT } from "atom.io/internal"
 import { myUserKeyAtom } from "atom.io/realtime-client"
 
 import { realtimeStateProvider } from "../realtime-state-provider"
 import type { ServerConfig } from "../server-config"
-import { usersOfSockets } from "./server-user-store"
 
 export function provideIdentity({
 	store = IMPLICIT.STORE,
 	socket,
+	userKey,
 }: ServerConfig): void {
-	const socketKey = `socket::${socket.id}` satisfies SocketKey
-	const userKey = getFromStore(
-		store,
-		findRelationsInStore(store, usersOfSockets, socketKey).userKeyOfSocket,
-	)!
-
 	const provideState = realtimeStateProvider({ socket, store, userKey })
 
 	const unsub = provideState(myUserKeyAtom, userKey)
