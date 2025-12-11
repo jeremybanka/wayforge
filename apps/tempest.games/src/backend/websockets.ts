527ac77325421a0d5cda148d8cee4f5fcf789585
Jeremy Banka <hello@jeremybanka.com>
2025-12-11 07:14:19 -0800

    ♻️ greatly simplify socket interface

--- apps/tempest.games/src/backend/websockets.ts
+++ apps/tempest.games/src/backend/websockets.ts
@@ -1,126 +1,94 @@
-import { findState, getInternalRelations } from "atom.io"
+import { type } from "arktype"
 import { IMPLICIT } from "atom.io/internal"
 import type { UserKey } from "atom.io/realtime"
-import {
-	roomKeysAtom,
-	usersInRooms,
-	visibleUsersInRoomsSelector,
-} from "atom.io/realtime"
 import type { Handshake, ServerConfig } from "atom.io/realtime-server"
-import {
-	provideRooms,
-	realtimeMutableFamilyProvider,
-	realtimeMutableProvider,
-	realtimeStateProvider,
-} from "atom.io/realtime-server"
+import { provideRooms, realtimeStateProvider } from "atom.io/realtime-server"
 import { CookieMap } from "bun"
-import { eq } from "drizzle-orm"
-import type { Socket } from "socket.io"
+import { and, eq } from "drizzle-orm"
 
 import { resolveRoomScript, workerNames } from "../backend.worker"
 import { users, userSessions } from "../database/tempest-db-schema"
-import type {
-	TempestSocketDown,
-	TempestSocketServerSide,
-	TempestSocketUp,
-} from "../library/socket-interface"
+// import { userSessions } from "./user-sessions"
 import { cpuCountAtom } from "../library/store"
 import { db } from "./db"
 import { logger } from "./logger"
-// import { userSessions } from "./user-sessions"
 
-export type TempestServerSocket = Socket<
-	TempestSocketUp,
-	TempestSocketDown,
-	TempestSocketServerSide
->
-
-export interface EventsMap {
-	[event: string]: any
-}
+const handshakeSchema = type({
+	auth: type({ username: `string` }),
+	headers: type({ cookie: `string` }),
+})
 
 export const sessionMiddleware = async (
 	handshake: Handshake,
 ): Promise<Error | UserKey> => {
-	const { username } = handshake.auth as { username: string }
-	if (!username) {
-		return new Error(`No auth header provided`)
+	const handshakeResult = handshakeSchema(handshake)
+	if (handshakeResult instanceof type.errors) {
+		return new Error(`Handshake failed validation: ${handshakeResult.summary}`)
 	}
-	const cookies = new CookieMap(handshake.headers.cookie ?? ``)
+	const {
+		auth: { username },
+		headers: { cookie },
+	} = handshakeResult
+	const cookies = new CookieMap(cookie)
 	const sessionKey = cookies.get(`sessionKey`)
-	if (!sessionKey) {
-		return new Error(`No session key provided`)
+	if (sessionKey === null) {
+		return new Error(`No sessionKey cookie was provided`)
 	}
 	const user = await db.drizzle.query.users.findFirst({
 		where: eq(users.username, username),
 	})
-	if (!user?.emailVerified) {
-		return new Error(`Email not verified`)
+	if (user === undefined) {
+		return new Error(`User not found`)
 	}
-	const userKey = `user::${user.id}` satisfies UserKey
-	const socketKey = `socket::${socket.id}` satisfies SocketKey
 	const session = await db.drizzle.query.userSessions.findFirst({
-		where: eq(userSessions.sessionKey, sessionKey),
+		where: and(
+			eq(userSessions.userId, user.id),
+			eq(userSessions.sessionKey, sessionKey),
+		),
 	})
-
-	if (session) {
-		const socketState = findInStore(IMPLICIT.STORE, socketAtoms, socketKey)
-		setIntoStore(IMPLICIT.STORE, socketState, socket)
-		editRelationsInStore(IMPLICIT.STORE, usersOfSockets, (relations) => {
-			relations.set(userKey, socketKey)
-		})
-		setIntoStore(IMPLICIT.STORE, onlineUsersAtom, (index) => index.add(userKey))
-		setIntoStore(IMPLICIT.STORE, socketKeysAtom, (index) => index.add(socketKey))
-		logger.info(`${username} connected on ${socket.id}`)
-		return `user::${user.id}` satisfies UserKey
+	if (session === undefined) {
+		return new Error(`User passed an invalid sessionKey`)
 	}
-	logger.info(`${username} couldn't authenticate`)
-	return new Error(`Authentication error`)
+	if (user.emailVerified === null) {
+		return new Error(`User does not have a verified email`)
+	}
+	return `user::${user.id}` satisfies UserKey
 }
 
 export const serveSocket = (config: ServerConfig): (() => void) => {
 	const { socket, userKey } = config
-	const rawUserId = userKey?.replace(/^user::/, ``)
-	const myRoomAtoms = getInternalRelations(usersInRooms)
-
-	const selfListSelector = findState(visibleUsersInRoomsSelector, userKey)
-	const provideFamily = realtimeMutableFamilyProvider({ socket, userKey })
-
 	socket.onAny((event, ...args) => {
 		logger.info(`🛰️ << 📡`, { event, args })
 	})
 	socket.onAnyOutgoing((event, ...args) => {
 		logger.info(`🛰️ >> 📡`, { event, args })
 	})
+	const provideState = realtimeStateProvider(config)
 
-	const unsubs = [
-		...[cpuCountAtom].map((token) =>
-			realtimeStateProvider({ socket, userKey })(token),
-		),
-		...[roomKeysAtom].map(realtimeMutableProvider({ socket, userKey })),
-		...[myRoomAtoms].map((atoms) => provideFamily(atoms, selfListSelector)),
+	const unsubFunctions = [
+		provideState(cpuCountAtom),
+		provideRooms({
+			socket,
+			userKey,
+			store: IMPLICIT.STORE,
+			roomNames: workerNames,
+			resolveRoomScript,
+		}),
 	]
 
-	provideRooms({
-		socket,
-		userKey,
-		store: IMPLICIT.STORE,
-		roomNames: workerNames,
-		resolveRoomScript,
-	})
-
-	socket.on(`changeUsername`, async (newUsername) => {
-		logger.info(`changing username to`, newUsername)
-		if (rawUserId && typeof newUsername === `string`) {
+	const rawUserId = userKey.replace(/^user::/, ``)
+	socket.on(`changeUsername`, async (username) => {
+		logger.info(`changing username to`, username)
+		if (typeof username === `string` && username.length < 20) {
 			await db.drizzle
 				.update(users)
-				.set({ username: newUsername })
+				.set({ username })
 				.where(eq(users.id, rawUserId))
-			socket.emit(`usernameChanged`, newUsername)
+			socket.emit(`usernameChanged`, username)
 		}
 	})
 
 	return () => {
-		for (const unsub of unsubs) unsub()
+		for (const unsub of unsubFunctions) unsub()
 	}
 }
--- apps/tempest.games/src/library/socket-interface.ts
+++ apps/tempest.games/src/library/socket-interface.ts
@@ -4,10 +4,6 @@ export interface TempestSocketServerSide {
 
 export interface TempestSocketUp {
 	changeUsername: (username: string) => void
-	createRoom: (roomId: string) => void
-	joinRoom: (roomId: string) => void
-	[leaveRoom: `leaveRoom:${string}`]: () => void
-	[deleteRoom: `deleteRoom:${string}`]: () => void
 }
 
 export interface TempestSocketDown {
