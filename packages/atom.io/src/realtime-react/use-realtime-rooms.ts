c91f3a14c2930791b90045d0c289255be334850f
Jeremy Banka <hello@jeremybanka.com>
2025-12-15 00:24:19 -0800

    ✨ pull rooms state from useRealtimeRooms

--- packages/atom.io/src/realtime-react/use-realtime-rooms.ts
+++ packages/atom.io/src/realtime-react/use-realtime-rooms.ts
@@ -1,13 +1,58 @@
-import type { RoomSocketInterface } from "atom.io/realtime"
+import type { MutableAtomToken } from "atom.io"
+import { findInStore, getInternalRelationsFromStore } from "atom.io/internal"
+import { StoreContext, useJSON, useO } from "atom.io/react"
+import type { RoomKey, RoomSocketInterface, UserKey } from "atom.io/realtime"
+import { ownersOfRooms, roomKeysAtom, usersInRooms } from "atom.io/realtime"
+import type { UList } from "atom.io/transceivers/u-list"
 import * as React from "react"
 import type { Socket } from "socket.io-client"
 
 import { RealtimeContext } from "./realtime-context"
+import { usePullMutable } from "./use-pull-mutable-atom"
+import { usePullMutableAtomFamilyMember } from "./use-pull-mutable-family-member"
 
-export function useRealtimeRooms<RoomNames extends string>(): Socket<
-	{},
-	RoomSocketInterface<RoomNames>
-> {
+export type RealtimeRoomsTools = {
+	socket: Socket<{}, RoomSocketInterface<string>>
+	currentRoomKey: RoomKey | null
+	allRoomKeysAtom: MutableAtomToken<UList<RoomKey>>
+	mutualsAtom: MutableAtomToken<UList<UserKey>>
+	ownedRoomsAtom: MutableAtomToken<UList<RoomKey>>
+}
+export function useRealtimeRooms<RoomNames extends string>(
+	userKey: UserKey,
+): RealtimeRoomsTools {
+	const store = React.useContext(StoreContext)
 	const { socket } = React.useContext(RealtimeContext)
-	return socket as Socket<{}, RoomSocketInterface<RoomNames>>
+	usePullMutable(roomKeysAtom)
+
+	const [userKeysFamily, roomKeysFamily] = getInternalRelationsFromStore(
+		store,
+		usersInRooms,
+		`split`,
+	)
+
+	usePullMutableAtomFamilyMember(roomKeysFamily, userKey)
+	const myJoinedRoomKeys = useO(roomKeysFamily, userKey)
+	let myRoomKey: RoomKey = `room::$_NONE_$`
+	for (const roomKey of myJoinedRoomKeys) {
+		myRoomKey = roomKey
+		break
+	}
+	usePullMutableAtomFamilyMember(userKeysFamily, myRoomKey)
+
+	const [ownedRoomsFamily] = getInternalRelationsFromStore(
+		store,
+		ownersOfRooms,
+		`split`,
+	)
+	const ownedRoomsToken = findInStore(store, ownedRoomsFamily, userKey)
+	usePullMutableAtomFamilyMember(ownedRoomsFamily, userKey)
+
+	return {
+		socket: socket as Socket<{}, RoomSocketInterface<RoomNames>>,
+		currentRoomKey: myRoomKey ?? null,
+		allRoomKeysAtom: roomKeysAtom,
+		mutualsAtom: findInStore(store, userKeysFamily, myRoomKey),
+		ownedRoomsAtom: ownedRoomsToken,
+	}
 }
