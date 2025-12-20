dd6478d76f85278db56c35b5b6f195f733913ec9
Jeremy Banka <hello@jeremybanka.com>
2025-12-20 12:25:29 -0800

    ✨ use subscriber for push game state also

--- packages/atom.io/src/realtime-client/push-state.ts
+++ packages/atom.io/src/realtime-client/push-state.ts
@@ -5,43 +5,32 @@ import type { Json } from "atom.io/json"
 import type { Socket } from "atom.io/realtime"
 import { employSocket, mutexAtoms } from "atom.io/realtime"
 
+import { createSubscriber } from "./create-subscriber"
+
 export function pushState<J extends Json.Serializable>(
 	store: Store,
 	socket: Socket,
 	token: WritableToken<J>,
 ): () => void {
-	const publish = (newValue: J) => {
-		socket.emit(`pub:${token.key}`, newValue)
-	}
-
-	const subscriptions = new Set<() => void>()
-	const clearSubscriptions = () => {
-		for (const unsub of subscriptions) unsub()
-		subscriptions.clear()
-	}
-
-	const init = () => {
-		subscriptions.add(
-			employSocket(socket, `claim-result:${token.key}`, (success: boolean) => {
+	return createSubscriber(socket, `push:${token.key}`, () => {
+		let stopWatching = employSocket(
+			socket,
+			`claim-result:${token.key}`,
+			(success: boolean) => {
 				if (!success) return
-
-				clearSubscriptions()
+				stopWatching()
 				setIntoStore(store, mutexAtoms, token.key, true)
-				subscriptions.add(
-					subscribeToState(store, token, `push`, ({ newValue }) => {
-						publish(newValue)
-					}),
-				)
-			}),
+				stopWatching = subscribeToState(store, token, `push`, ({ newValue }) => {
+					socket.emit(`pub:${token.key}`, newValue)
+				})
+			},
 		)
 
 		socket.emit(`claim:${token.key}`)
-	}
 
-	init()
-
-	return () => {
-		clearSubscriptions()
-		socket.emit(`unclaim:${token.key}`)
-	}
+		return () => {
+			socket.emit(`unclaim:${token.key}`)
+			stopWatching()
+		}
+	})
 }
