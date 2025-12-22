a1cd0358292075d4dfeaff941b7350b104d62038
Jeremy Banka <hello@jeremybanka.com>
2025-12-21 19:12:52 -0800

    ✨ allow TypedSocket for employSocket

--- packages/atom.io/src/realtime/employ-socket.ts
+++ packages/atom.io/src/realtime/employ-socket.ts
@@ -1,8 +1,18 @@
-import type { Socket } from "atom.io/realtime"
+import type { EventsMap, Socket, TypedSocket } from "atom.io/realtime"
 import type { Events } from "atom.io/realtime-server"
 
+export function employSocket<I extends EventsMap, K extends string & keyof I>(
+	socket: TypedSocket<I, any>,
+	event: K,
+	handleEvent: (...data: Parameters<I[K]>) => void,
+): () => void
 export function employSocket<I extends Events, K extends string & keyof I>(
 	socket: Socket,
+	event: K,
+	handleEvent: (...data: I[K]) => void,
+): () => void
+export function employSocket<I extends Events, K extends string & keyof I>(
+	socket: Socket | TypedSocket<any, any>,
 	event: K,
 	handleEvent: (...data: I[K]) => void,
 ): () => void {
