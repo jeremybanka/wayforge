cccac690b42be27a575f69a8ef79b70ffb40088d
Jeremy Banka <hello@jeremybanka.com>
2025-12-29 21:15:51 -0800

    🔧 rework dev cert (#5690)

--- apps/tempest.games/dev/https-dev.ts
+++ apps/tempest.games/dev/https-dev.ts
@@ -2,6 +2,7 @@ import { readFileSync } from "node:fs"
 import type { RequestListener, Server } from "node:http"
 import { createServer as createHttpServer } from "node:http"
 import { createServer as createSecureServer } from "node:https"
+import { hostname } from "node:os"
 import { resolve } from "node:path"
 
 import { env } from "../src/library/env"
@@ -10,8 +11,8 @@ const devDir = resolve(import.meta.dirname, `../dev`)
 
 export const httpsDev = env.VITE_DEV_HTTPS
 	? {
-			cert: readFileSync(resolve(devDir, `./cert.pem`), `utf-8`),
-			key: readFileSync(resolve(devDir, `./key.pem`), `utf-8`),
+			cert: readFileSync(resolve(devDir, `${hostname()}.pem`), `utf-8`),
+			key: readFileSync(resolve(devDir, `${hostname()}-key.pem`), `utf-8`),
 		}
 	: undefined
 
