c5817687f6745c12fcf8964e6af9e43cc5859f00
Jeremy Banka <hello@jeremybanka.com>
2025-12-30 17:33:54 -0800

    🔧 acquire hostname correctly

--- apps/tempest.games/dev/https-dev.ts
+++ apps/tempest.games/dev/https-dev.ts
@@ -1,18 +1,27 @@
+import { exec } from "node:child_process"
 import { readFileSync } from "node:fs"
 import type { RequestListener, Server } from "node:http"
 import { createServer as createHttpServer } from "node:http"
 import { createServer as createSecureServer } from "node:https"
-import { hostname } from "node:os"
 import { resolve } from "node:path"
+import { promisify } from "node:util"
+
+const execAsync = promisify(exec)
 
 import { env } from "../src/library/env"
 
 const devDir = resolve(import.meta.dirname, `../dev`)
 
+const localHostName = (
+	await execAsync(`scutil --get LocalHostName`)
+).stdout.trim()
+
+const bonjourName = `${localHostName}.local`
+
 export const httpsDev = env.VITE_DEV_HTTPS
 	? {
-			cert: readFileSync(resolve(devDir, `${hostname()}.pem`), `utf-8`),
-			key: readFileSync(resolve(devDir, `${hostname()}-key.pem`), `utf-8`),
+			cert: readFileSync(resolve(devDir, `${bonjourName}.pem`), `utf-8`),
+			key: readFileSync(resolve(devDir, `${bonjourName}-key.pem`), `utf-8`),
 		}
 	: undefined
 
