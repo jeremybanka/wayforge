44e146deda0db6b7aa972b2b9954b0cc952a6563
Jeremy Banka <hello@jeremybanka.com>
2026-01-03 19:39:57 -0800

    🧹 clean up logs in test

--- packages/atom.io/__tests__/experimental/realtime/ipc-socket.test.tsx
+++ packages/atom.io/__tests__/experimental/realtime/ipc-socket.test.tsx
@@ -6,10 +6,10 @@ import { UList } from "atom.io/transceivers/u-list"
 
 import * as Utils from "../../__util__"
 
-// console.log = () => undefined
-// console.info = () => undefined
-// console.warn = () => undefined
-// console.error = () => undefined
+console.log = () => undefined
+console.info = () => undefined
+console.warn = () => undefined
+console.error = () => undefined
 const logger: Pick<Console, `error` | `info` | `warn`> = console
 
 interface VirtualProcess {
