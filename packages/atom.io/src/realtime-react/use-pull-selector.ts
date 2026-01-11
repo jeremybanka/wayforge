4e9feb60066be983b9314a205f35407c6cf763d7
Jeremy Banka <hello@jeremybanka.com>
2026-01-10 18:59:52 -0800

    🏷️ don't require json type for pull selector

--- packages/atom.io/src/realtime-react/use-pull-selector.ts
+++ packages/atom.io/src/realtime-react/use-pull-selector.ts
@@ -1,14 +1,13 @@
 import type * as AtomIO from "atom.io"
-import type { Json } from "atom.io/json"
 import { StoreContext, useO } from "atom.io/react"
 import * as RTC from "atom.io/realtime-client"
 import * as React from "react"
 
 import { useRealtimeService } from "./use-realtime-service"
 
-export function usePullSelector<J extends Json.Serializable>(
-	token: AtomIO.SelectorToken<J>,
-): AtomIO.ViewOf<J> {
+export function usePullSelector<T>(
+	token: AtomIO.SelectorToken<T>,
+): AtomIO.ViewOf<T> {
 	const store = React.useContext(StoreContext)
 	useRealtimeService(`pull:${token.key}`, (socket) =>
 		RTC.pullSelector(store, socket, token),
