4d781452229703b0bafe25587e01a2282dbd8179
Jeremy Banka <hello@jeremybanka.com>
2026-03-21 17:23:34 -0700

    🛸 update to new standards for isolatedTypeDeclarations [atom.io]

--- packages/atom.io/__tests__/__util__/summarize.ts
+++ packages/atom.io/__tests__/__util__/summarize.ts
@@ -2,7 +2,7 @@ import { prettyDOM } from "@testing-library/react"
 import type { Store } from "atom.io/internal"
 import { IMPLICIT } from "atom.io/internal"
 
-export const summarize = (store: Store = IMPLICIT.STORE): void => {
+export function summarize(store: Store = IMPLICIT.STORE): void {
 	console.log(prettyDOM(document), {
 		atoms: [store.atoms.size],
 	})
