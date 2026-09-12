bdfa6142cbe5e9d66d82ad49eb03390af94afbae
Jeremy Banka <hello@jeremybanka.com>
2026-09-11 17:44:56 -0700

    test(comline): enforce coverage with recoverage

--- packages/comline/vitest.config.ts
+++ packages/comline/vitest.config.ts
@@ -2,7 +2,14 @@ import type { UserConfig } from "vite"
 import { defineConfig } from "vitest/config"
 
 const config: UserConfig = defineConfig({
-	test: { globals: true },
+	test: {
+		globals: true,
+		coverage: {
+			provider: `v8`,
+			reporter: [`text`, `lcov`, `json`],
+			include: [`src/**/*.ts`],
+		},
+	},
 })
 
 export default config
